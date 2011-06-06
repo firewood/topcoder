db674c00b54e854cfdcce3b61fb8b33acb0150e8
firewood <karamaki@gmail.com>
2011-06-06 23:21:30 +0900

    add non-bound cases

--- srm_507/CubeRoll.cpp
+++ srm_507/CubeRoll.cpp
@@ -91,34 +91,60 @@ typedef vector<int> IntVector;
 class CubeRoll {
 	public:
 	int getMinimumSteps(int initPos, int goalPos, vector <int> holePos) {
-		int s = min(initPos, goalPos);
-		int e = max(initPos, goalPos);
+		int start = min(initPos, goalPos);
+		int end = max(initPos, goalPos);
 		sort(holePos.begin(), holePos.end());
 		size_t i;
-		int range = e - s;
-		int jump_range = min(1000000, 100 * range);
-		int lb = *holePos.begin() - jump_range;
-		int ub = *holePos.rbegin() + jump_range;
+		bool has_lower_bound = false;
+		bool has_upper_bound = false;
+		int lower_bound, upper_bound;
 		for (i = 0; i < holePos.size(); ++i) {
 			int p = holePos[i];
-			if (p < s) {
-				lb = p;
+			if (p < start) {
+				has_lower_bound = true;
+				lower_bound = p;
 				continue;
 			}
-			if (p < e) {
+			if (p < end) {
 				return -1;
 			}
-			if (p > e) {
-				ub = p;
-				break;
-			}
+			has_upper_bound = true;
+			upper_bound = p;
+			break;
 		}
-//		int distance = (int)sqrt((double)range) + 1;
-		int distance = jump_range / 10;
+
+		int range = end - start;
+
+		if (!has_lower_bound || !has_upper_bound) {
+			int sq = (int)sqrt((double)range);
+			int remain = range - sq * sq;
+			if (remain == 0) {
+				return 1;
+			}
+
+
+			//??
+			sq = (int)sqrt((double)remain);
+			if (remain == sq * sq) {
+				return 2;
+			}
+
+
+			if ((range % 2) == 1) {
+				// (x + 1)^2 - x^2 = 2x + 1
+				return 2;
+			}
+			if ((range % 4) == 0) {
+				// (x + 2)^2 - x-2 = 4x + 4
+				return 2;
+			}
+			return 3;
+		}
 
 		IntSet f;
 		IntVector current;
-		current.push_back(s);
+		f.insert(start);
+		current.push_back(start);
 		int turns;
 		for (turns = 1; turns < range; ++turns) {
 			IntVector next;
@@ -126,31 +152,29 @@ class CubeRoll {
 				int pos = *current.rbegin();
 				current.pop_back();
 				int d;
-				for (d = 1; d <= distance; ++d) {
-					int d2 = d * d;
-					int x = pos - d2;
-					if (x <= lb) {
+				for (d = 1; ; ++d) {
+					int x = pos - d * d;
+					if (x <= lower_bound) {
 						break;
 					}
 					if (f.find(x) != f.end()) {
 						continue;
 					}
-					if (x == e) {
+					if (x == end) {
 						return turns;
 					}
 					f.insert(x);
 					next.push_back(x);
 				}
-				for (d = 1; d <= distance; ++d) {
-					int d2 = d * d;
-					int x = pos + d2;
-					if (x >= ub) {
+				for (d = 1; ; ++d) {
+					int x = pos + d * d;
+					if (x >= upper_bound) {
 						break;
 					}
 					if (f.find(x) != f.end()) {
 						continue;
 					}
-					if (x == e) {
+					if (x == end) {
 						return turns;
 					}
 					f.insert(x);
@@ -159,8 +183,7 @@ class CubeRoll {
 			}
 			current = next;
 		}
-
-		return e - s;
+		return range;
 	}
 };
 
@@ -196,6 +219,8 @@ static void Test(int s, int e, const char *seq, int expected)
 }
 
 int main() {
+//	Test(0, 9, "9999", 1);
+	Test(0, 10, "9999", 2);
 //	Test(20, 25, "10,30,50,100", 5);
 //	Test(20, 40, "10,30,50,100", -1);
 //	Test(1, 3, "0,5,6,7", 2);
@@ -204,6 +229,6 @@ int main() {
 //	Test(10, 21, "38,45", 2);
 //	Test(2,99998, "1", 2);
 //	Test(42807, 44571, "39017, 20306, 29032, 25977, 18312, 42424, 27909, 22813, 42264, 33996, 14393, 12311, 33005, 16254, 20880, 126, 19777, 31443, 9321, 5760, 1160, 20301, 34356, 32737, 20907, 32171, 36059, 25373, 11937, 16767, 30015, 33421, 3569, 1574, 29407, 22544, 35042, 19455, 40334, 14547, 7328, 27007, 10953, 32956, 39191, 29707, 5139, 41106, 35028, 5366", 1);
-	Test(81247, 62954, "30171, 9572, 20677, 29005, 29157, 18097, 3863, 7601, 38755, 20001, 7099, 49274, 48284, 7155, 1814, 9093, 25130, 27403, 25182, 615, 38597, 6692, 35207, 36018, 30998, 33632, 58506, 16757, 48164, 33934, 29840, 46312, 19493, 41646, 22701, 33757, 55569, 62953, 36871, 28113, 43610, 10937, 1547, 52665, 17212, 24107, 43945, 27055, 43030, 5682", 2);
+//	Test(81247, 62954, "30171, 9572, 20677, 29005, 29157, 18097, 3863, 7601, 38755, 20001, 7099, 49274, 48284, 7155, 1814, 9093, 25130, 27403, 25182, 615, 38597, 6692, 35207, 36018, 30998, 33632, 58506, 16757, 48164, 33934, 29840, 46312, 19493, 41646, 22701, 33757, 55569, 62953, 36871, 28113, 43610, 10937, 1547, 52665, 17212, 24107, 43945, 27055, 43030, 5682", 2);
 }
 // END CUT HERE
