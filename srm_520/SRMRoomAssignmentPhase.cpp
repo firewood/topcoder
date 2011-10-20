3b5092957ee14b13d31ff2d76b07027b6ce856ef
firewood <karamaki@gmail.com>
2011-10-20 12:41:30 +0900

    translate

--- srm_520/SRMRoomAssignmentPhase.cpp
+++ srm_520/SRMRoomAssignmentPhase.cpp
@@ -1,110 +1,12 @@
 // BEGIN CUT HERE
 /*
 // SRM 520 Div2 Easy (250)
-// PROBLEM STATEMENT
-// Mr. Dengklek introduced you to an online programming contest called SRM (Special Round Match)!
 
-There are N coders in the contest, including you. Each coder has a skill measurement called rating.
-This information is given in ratings, where ratings[0] is your rating and
-ratings[1] through ratings[N-1] are the ratings of the other coders.
-Ratings of all coders are pairwise distinct.
+問題
 
-You are now in the room assignment phase of the contest. There are K available rooms.
-Each coder will be assigned to exactly one room in the following way:
-the coders, in descending order of their ratings, are assigned one-by-one
-to rooms 1, 2, ..., K, 1, 2, ..., K, ... and so on in that order.
-See the examples for further clarification.
-
-You wish to win your room, so you wonder how many stronger competitors you have.
-Return the number of coders in your room who have higher ratings than yours.
-
-DEFINITION
-Class:SRMRoomAssignmentPhase
-Method:countCompetitors
-Parameters:vector <int>, int
-Returns:int
-Method signature:int countCompetitors(vector <int> ratings, int K)
-
-
-CONSTRAINTS
--ratings will contain between 2 and 50 elements, inclusive.
--Each element of ratings will be between 0 and 1199, inclusive.
--All elements of ratings will be distinct.
--K will be between 1 and the number of elements in ratings, inclusive.
-
-
-EXAMPLES
-
-0)
-{491, 981, 1199, 763, 994, 879, 888}
-3
-
-Returns: 2
-
-Here is the room assignment for all coders.
-
-Room 1 coders' ratings:
-
-1199
-888
-491
-
-
-Room 2 coders' ratings:
-
-994
-879
-
-
-Room 3 coders' ratings:
-
-981
-763
-
-
-You are in Room 1, and there are 2 coders with greater ratings than yours.
-
-
-
-1)
-{1024, 1000, 600}
-1
-
-Returns: 0
-
-You are the highest rated coder in Room 1.
-
-2)
-{505, 679, 900, 1022}
-2
-
-Returns: 1
-
-You are the lowest rated coder in Room 2.
-
-3)
-{716, 58, 1000, 1004, 912, 822, 453, 1100, 558}
-3
-
-Returns: 1
-
-You are the second highest rated coder in Room 3.
-
-4)
-{422, 623, 1023, 941, 882, 776, 852, 495, 803, 622, 618, 532, 751, 500}
-4
-
-Returns: 3
-
-
-
-5)
-{1197, 1198, 1196, 1195, 1199}
-1
-
-Returns: 2
-
-
+SRMというプログラミングコンテストにN人が参加している。
+K個の部屋があり、レートの高い順に割り当てられる。
+部屋内で自分よりレートが高い人数を求める。
 
 #line 102 "SRMRoomAssignmentPhase.cpp"
 */
@@ -170,12 +72,24 @@ static void Test(const char *seq, int K, int expected)
 }
 
 int main() {
+	// example 0
 	Test("491, 981, 1199, 763, 994, 879, 888", 3, 2);
+
+	// example 1
 	Test("1024, 1000, 600", 1, 0);
+
+	// example 2
 	Test("505, 679, 900, 1022", 2, 1);
+
+	// example 3
 	Test("716, 58, 1000, 1004, 912, 822, 453, 1100, 558", 3, 1);
+
+	// example 4
 	Test("422, 623, 1023, 941, 882, 776, 852, 495, 803, 622, 618, 532, 751, 500", 4, 3);
+
+	// example 5
 	Test("1197, 1198, 1196, 1195, 1199", 1, 2);
+
 	return 0;
 }
 // END CUT HERE
