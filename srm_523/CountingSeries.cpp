5e09ab071294ca916d4d4fd68d5e1260316af224
firewood <karamaki@gmail.com>
2011-11-15 02:30:59 +0900

    add upperBound check

--- srm_523/CountingSeries.cpp
+++ srm_523/CountingSeries.cpp
@@ -1,85 +1,11 @@
 // BEGIN CUT HERE
 /*
 // SRM 523 Div2 Medium (500)
-// PROBLEM STATEMENT
-// You are given long longs a, b, c and d. The numbers a and b define an arithmetic progression that consists of all numbers of the form a + b*x, where x is a nonnegative integer. Likewise, c and d define a geometric progression that consists of all the numbers that are equal to c * d^y, where y is a nonnegative integer. You are also given a long long upperBound. Return the total number of integers between 1 and upperBound, inclusive, that belong to the arithmetic progression, the geometric progression or both.
 
-DEFINITION
-Class:CountingSeries
-Method:countThem
-Parameters:long long, long long, long long, long long, long long
-Returns:long long
-Method signature:long long countThem(long long a, long long b, long long c, long long d, long long upperBound)
+ñ‚ëË
 
-
-NOTES
--The ^ operator in this statement denotes the exponentiation operation. For example, 3^0 = 1 and 2^4 = 2*2*2*2 = 16.
-
-
-CONSTRAINTS
--a, b, c and upperBound will each be between 1 and 1000000000000 (10^12), inclusive.
--d will be between 1 and 100000 (10^5), inclusive.
-
-
-EXAMPLES
-
-0)
-1
-1
-1
-2
-1000
-
-Returns: 1000
-
-The arithmetic progression is: 1, 2, 3, 4, ... .
-The geometric progression is: 1, 2, 4, 8, 16, ... . 
-Each positive integer is contained in at least one of the progressions.
-
-1)
-3
-3
-1
-2
-1000
-
-Returns: 343
-
-This time, the arithmetic progression is: 3, 6, 9, 12, ... .
-The geometric progression is still: 1, 2, 4, 8, 16, ....
-There are 333 multiples of 3 between 1 and 1000, inclusive, and there are 10 powers of 2, 512 being the highest. As these two progressions do not have any common elements, the total result is 343.
-
-2)
-40
-77
-40
-100000
-40
-
-Returns: 1
-
-
-
-3)
-452
-24
-4
-5
-600
-
-Returns: 10
-
-The 10 numbers are: 4, 20, 100, 452, 476, 500, 524, 548, 572 and 596.
-
-4)
-234
-24
-377
-1
-10000
-
-Returns: 408
-
+1à»è„ÇÃêîa,b,c,d,upperBoundÇ™ó^Ç¶ÇÁÇÍÇÈÅB
+1Ç©ÇÁupperBoundÇ‹Ç≈ÇÃä‘Ç≈a+b*xÇ∆c*d^yÇÃÇ¢Ç∏ÇÍÇ©ÇñûÇΩÇ∑êîÇÃå¬êîÇãÅÇﬂÇÈÅB
 
 
 #line 86 "CountingSeries.cpp"
@@ -105,9 +31,12 @@ class CountingSeries {
 
 public:
 	long long countThem(long long a, long long b, long long c, long long d, long long upperBound) {
-		LL Min = a;
-		LL Max = upperBound - ((upperBound - a) % b);
-		LL result = (Max - Min) / b + 1;
+		LL result = 0;
+		if (a <= upperBound) {
+			LL Min = a;
+			LL Max = upperBound - ((upperBound - a) % b);
+			result += (Max - Min) / b + 1;
+		}
 
 		LL x = c;
 		while (x <= upperBound) {
@@ -146,7 +75,6 @@ public:
 // BEGIN CUT HERE
 int main() {
 	CountingSeries ___test;
-	___test.run_test(0);
 	___test.run_test(-1);
 	return 0;
 }
