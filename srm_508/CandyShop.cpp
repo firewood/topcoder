7d7f3bfb58e9394fc888de757e70ea0a198133f6
firewood <karamaki@gmail.com>
2011-11-15 10:49:35 +0900

    translate

--- srm_508/CandyShop.cpp
+++ srm_508/CandyShop.cpp
@@ -1,84 +1,11 @@
 // BEGIN CUT HERE
 /*
 // SRM 508 Div2 Easy (250)
-// PROBLEM STATEMENT
-// Manao has been to a tremendous candy shop several times. He has forgotten its exact placement, but remembers some information about each time he visited the shop.
 
-The city Manao lives in can be represented as an infinite two-dimensional plane with a Cartesian coordinate system. From point (x, y), Manao can move to any of the points (x - 1, y), (x, y - 1), (x + 1, y), or (x, y + 1). In order to perform each of these 4 moves, he needs to walk 1 unit of length. Manao recalls that the candy shop was located at a point with integer coordinates. Also, he remembers that the i-th time he visited the candy shop, he went there from point (X[i], Y[i]) and walked at most R[i] units of length.
+問題
 
-Since Manao's visits to the shop span a long period of time, he may have misremembered some details. If no intersection complies with his reminiscence, return 0. Otherwise return the number of different intersections where Manao's candy shop could be, assuming Manao remembers everything correctly.
-
-DEFINITION
-Class:CandyShop
-Method:countProbablePlaces
-Parameters:vector <int>, vector <int>, vector <int>
-Returns:int
-Method signature:int countProbablePlaces(vector <int> X, vector <int> Y, vector <int> R)
-
-
-CONSTRAINTS
--X will contain between 1 and 50 elements, inclusive.
--Y will contain the same number of elements as X.
--R will contain the same number of elements as X.
--Each element of X will be between -100 and 100, inclusive.
--Each element of Y will be between -100 and 100, inclusive.
--Each element of R will be between 1 and 100, inclusive.
-
-
-EXAMPLES
-
-0)
-{0}
-{0}
-{1}
-
-Returns: 5
-
-Manao walked at most 1 unit of distance. Therefore, the shop could be at (0,0) if Manao didn't walk at all and at (1,0), (0,1), (-1,0) or (0,-1) if he walked.
-
-1)
-{0}
-{0}
-{2}
-
-Returns: 13
-
-8 new locations are introduced if Manao walks another unit of distance.
-
-2)
-{2,3}
-{1,-1}
-{2,2}
-
-Returns: 4
-
-The possible places for the shop are intersections (2,0), (2,-1), (3,0) and (3,1).
-
-3)
-{2,3,5}
-{1,-1,0}
-{2,2,3}
-
-Returns: 3
-
-This is the same situation as in the previous example, but Manao has been to the shop once more. After this visit, (2,-1) is also ruled out from the possibilities list.
-
-4)
-{-100,-100,-100,0,0,0,100,100,100}
-{-100,0,100,-100,0,100,-100,0,100}
-{1,1,1,1,1,1,1,1,1}
-
-Returns: 0
-
-Manao is surely mistaken in his reminiscence.
-
-5)
-{-3,3,5,5}
-{4,5,-2,0}
-{10,11,8,6}
-
-Returns: 33
-
+キャンディー屋の候補が座標で与えられる
+可能性のある座標の個数を求める
 
 
 #line 85 "CandyShop.cpp"
@@ -151,11 +78,22 @@ static void Test(const char *x, const char *y, const char *r, int expected)
 }
 
 int main() {
+	// example 0
 	Test("0", "0", "1", 5);
-//	Test("0", "0", "2", 13);
+
+	// example 1
+	Test("0", "0", "2", 13);
+
+	// example 2
 	Test("2,3", "1,-1", "2,2", 4);
+
+	// example 3
 	Test("2,3,5", "1,-1,0", "2,2,3", 3);
+
+	// example 4
 	Test("-100,-100,-100,0,0,0,100,100,100","-100,0,100,-100,0,100,-100,0,100","1,1,1,1,1,1,1,1,1", 0);
+
+	// example 5
 	Test("-3,3,5,5","4,5,-2,0","10,11,8,6", 33);
 
 	return 0;
