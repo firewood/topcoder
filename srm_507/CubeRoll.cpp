e3794747c79128cb7e4a9a17f25e7493e3fa80a9
firewood <karamaki@gmail.com>
2011-11-15 10:38:13 +0900

    translate

--- srm_507/CubeRoll.cpp
+++ srm_507/CubeRoll.cpp
@@ -1,77 +1,11 @@
 // BEGIN CUT HERE
 /*
 // SRM 507 Div2 Hard (1000)
-// PROBLEM STATEMENT
-// NOTE: This problem statement contains images that may not display properly if viewed outside of the applet.
-Fox Ciel is competing against Fox Jiro in a game called cube roll.
 
-Cube roll is played on a horizontal row of cells.  Cells are numbered consecutively, increasing from left to right.  The row is infinitely long, so all integers, including negative ones, are valid cell numbers.  Certain cells contain holes, and they are given in the vector <int> holePos.
-Initially, a cube is placed at cell initPos.  The objective of the game is to move the cube to cell goalPos using the least possible number of turns.  On each turn, the player will choose a direction (left or right) and roll the cube in that direction by x number of cells, where x is a perfect square (1, 4, 9, 16, ...).  If at any time during the roll, the cube lands on a cell containing a hole, the cube will fall into the hole.  Once the cube falls into a hole, it can never be moved again.
+問題
 
-
-Return the minimum number of turns required to move the cube to cell goalPos.  If it is impossible, return -1 instead.
-
-
-DEFINITION
-Class:CubeRoll
-Method:getMinimumSteps
-Parameters:int, int, vector <int>
-Returns:int
-Method signature:int getMinimumSteps(int initPos, int goalPos, vector <int> holePos)
-
-
-CONSTRAINTS
--initPos and goalPos will each be between 1 and 100,000, inclusive.
--holePos will contain between 1 and 50 elements, inclusive.
--Each element of holePos be between 1 and 100,000, inclusive.
--initPos and goalPos and all elements of holePos will be distinct.
-
-
-EXAMPLES
-
-0)
-5
-1
-{3}
-
-Returns: -1
-
-There is a hole between initPos and goalPos, so the cube cannot be moved from initPos to goalPos.
-
-1)
-36
-72
-{300, 100, 200, 400}
-
-Returns: 1
-
-The distance between initPos and goalPos is a perfect square, and there are no holes between them, so the cube can be moved in a single turn.
-
-2)
-10
-21
-{38,45}
-
-Returns: 2
-
-One of the optimal solutions is to move the cube to 10 -> -15 -> 21. (Note that the coordinate of the cube can be a negative integer.)
-
-3)
-98765
-4963
-{10,20,40,30}
-
-Returns: 2
-
-
-
-4)
-68332
-825
-{99726,371,67,89210}
-
-Returns: 2
-
+n^2マス単位で動かすことができるすごろくがある。
+ゴールまでの最小ターン数を答える。
 
 
 #line 78 "CubeRoll.cpp"
@@ -216,6 +150,22 @@ static void Test(int s, int e, const char *seq, int expected)
 }
 
 int main() {
+	// example 0
+	Test(5, 1, "3", -1);
+
+	// example 1
+	Test(36, 72, "300,100,200,400", 1);
+
+	// example 2
+	Test(10, 21, "38,45", 2);
+
+	// example 3
+	Test(98765, 4963, "10,20,40,30", 2);
+
+	// example 4
+	Test(68332, 825, "99726,371,67,89210", 2);
+
+
 //	Test(0, 9, "9999", 1);
 	Test(0, 10, "9999", 2);
 //	Test(20, 25, "10,30,50,100", 5);
@@ -227,5 +177,6 @@ int main() {
 //	Test(2,99998, "1", 2);
 //	Test(42807, 44571, "39017, 20306, 29032, 25977, 18312, 42424, 27909, 22813, 42264, 33996, 14393, 12311, 33005, 16254, 20880, 126, 19777, 31443, 9321, 5760, 1160, 20301, 34356, 32737, 20907, 32171, 36059, 25373, 11937, 16767, 30015, 33421, 3569, 1574, 29407, 22544, 35042, 19455, 40334, 14547, 7328, 27007, 10953, 32956, 39191, 29707, 5139, 41106, 35028, 5366", 1);
 //	Test(81247, 62954, "30171, 9572, 20677, 29005, 29157, 18097, 3863, 7601, 38755, 20001, 7099, 49274, 48284, 7155, 1814, 9093, 25130, 27403, 25182, 615, 38597, 6692, 35207, 36018, 30998, 33632, 58506, 16757, 48164, 33934, 29840, 46312, 19493, 41646, 22701, 33757, 55569, 62953, 36871, 28113, 43610, 10937, 1547, 52665, 17212, 24107, 43945, 27055, 43030, 5682", 2);
+
 }
 // END CUT HERE
