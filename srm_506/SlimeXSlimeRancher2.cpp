55e7f7d9f02928d6aa7603b708a1cb26e6d84338
firewood <karamaki@gmail.com>
2011-11-15 09:50:48 +0900

    translate

--- srm_506/SlimeXSlimeRancher2.cpp
+++ srm_506/SlimeXSlimeRancher2.cpp
@@ -1,49 +1,11 @@
 // BEGIN CUT HERE
 /*
 // SRM 506 Div2 Easy (250)
-// PROBLEM STATEMENT
-// You are playing a game titled Slime Rancher 2. You will be training slimes in this game.
 
+問題
 
-You have a slime-in-training. Associated with the slime are N attributes, numbered 0 through N-1, each represented by a positive integer. You are given vector <int> attributes containing N integers : the i-th integer is the initial value of the i-th attribute for the slime. After the training is complete, each of the slime's attributes will either stay the same or increase to some positive integer less than or equal to 999. None of the attributes will decrease in value. The weight of the training is defined as the sum of the differences between the final and initial values of all the attributes for the slime.
-
-
-You are a master slime breeder, and you're able to obtain any possible final values for a slime's attributes. This time, you would like to create a well-balanced slime. A slime is well-balanced if all of its attributes have equal values. What is the minimum possible weight of the training?
-
-DEFINITION
-Class:SlimeXSlimeRancher2
-Method:train
-Parameters:vector <int>
-Returns:int
-Method signature:int train(vector <int> attributes)
-
-
-CONSTRAINTS
--attributes will contain between 2 and 50 elements, inclusive.
--Each element of attributes will be between 1 and 999, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,2,3}
-
-Returns: 3
-
-Train the slime such that all of its attributes are equal to 3. The total weight of the training is |3 - 1| + |3 - 2| + |3 - 3| = 3.
-
-1)
-{5,5}
-
-Returns: 0
-
-This slime is already well-balanced.
-
-2)
-{900,500,100}
-
-Returns: 1200
-
+N個の数値がある。
+最大値に合わせるために、加算する総和を求める。
 
 
 */
@@ -104,7 +66,15 @@ static void Test(const char *seq, int expected)
 }
 
 int main() {
+	// example 0
+	Test("1,2,3", 3);
+
+	// example 1
+	Test("5,5", 0);
+
+	// example 2
 	Test("900,500,100", 1200);
+
 	return 0;
 }
 // END CUT HERE
