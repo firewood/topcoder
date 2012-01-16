ea945c1ee915cf2e41e498a6fd533ae8f3815a55
firewood <karamaki@gmail.com>
2012-01-17 02:20:16 +0900

    translate

--- srm_529/MinskyMysteryDiv2.cpp
+++ srm_529/MinskyMysteryDiv2.cpp
@@ -1,94 +1,12 @@
 // BEGIN CUT HERE
 /*
 // SRM 529 Div2 Hard (1000)
-// PROBLEM STATEMENT
-// 
-Marvin plays a simple game. The game is played with an infinite supply of marbles and five bags,
-labeled "bag 0" through "bag 4".
 
-At the beginning, Marvin takes N marbles (where N is a nonnegative integer) and places them into bag 0.
-The remaining four bags are left empty.
-Marvin then follows this simple algorithm:
+問題
 
-    Add a marble into bag 1.
-    Repeat forever:
-        Add a marble into bag 1.
-        Empty bag 4.
-        While there are marbles in bag 0:
-            While there are marbles both in bag 0 and in bag 1:
-                Remove a marble from bag 0.
-                Remove a marble from bag 1.
-                Add a marble into bag 2.
-                Add a marble into bag 3.
-            End While
-            Add a marble into bag 4.
-            If bags 0 and 1 are both empty:
-                Move all marbles from bag 3 to bag 4.
-                TERMINATE THE GAME
-            End If
-            Move all marbles from bag 3 to bag 1.
-        End While
-        Move all marbles from bag 2 to bag 0.
-    End Repeat
+あるルールに基づいて、5つの袋からビー玉を足したり引いたりする
+0から10^12まで処理できるようにせよ
 
-You are given a long long N. Return a long long containing the number of marbles that will be
-in bag 4 at the end of Marvin's game. If Marvin's game does not terminate for the given N,
-return -1 instead.
-
-
-DEFINITION
-Class:MinskyMysteryDiv2
-Method:computeAnswer
-Parameters:long long
-Returns:long long
-Method signature:long long computeAnswer(long long N)
-
-
-NOTES
--You may assume that the answer always fits into a long long.
--Note that N is allowed to be zero.
-
-
-CONSTRAINTS
--N will be between 0 and 10^12, inclusive.
-
-
-EXAMPLES
-
-0)
-2
-
-Returns: 3
-
-
-
-1)
-3
-
-Returns: 4
-
-
-
-2)
-4
-
-Returns: 4
-
-
-
-3)
-15
-
-Returns: 8
-
-
-
-4)
-24
-
-Returns: 14
-
-#line 94 "MinskyMysteryDiv2.cpp"
 */
 // END CUT HERE
 #include <cmath>
