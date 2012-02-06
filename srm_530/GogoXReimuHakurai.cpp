1524edec50ac8ddd59d8f1dc1030431edb72a467
firewood <karamaki@gmail.com>
2012-02-07 01:01:18 +0900

    translate

--- srm_530/GogoXReimuHakurai.cpp
+++ srm_530/GogoXReimuHakurai.cpp
@@ -2,83 +2,11 @@
 /*
 // SRM 530 Div2 Hard (1000)
 
-// PROBLEM STATEMENT
-// Like all other software engineers, Gogo likes to "read" virtual novels.
-In particular, he's currently "reading" a novel titled Touhou, with Reimu Hakurai
-as its main protagonist. There are N stages in Touhou, numbered 0 through N-1.
-A playthrough of the novel always starts at the stage 0. The playthrough
-then may visit some other stages, based on the player's choices. The sequence
-of visited stages is always strictly ascending. A playthrough is successful
-if it terminates by reaching the stage N-1.
-
-In each stage, Gogo can either finish the playthrough or choose one of the
-available options that advance Reimu to other stages. You are given
-a vector <string> choices. If there is a choice that advances Reimu
-from stage i to stage j, choices[i][j] will be 'Y', otherwise it will be 'N'.
-For each pair of stages i, j such that i < j the game contains at most one such choice.
-
-Gogo wants to make as many successful playthroughs as possible, one after another.
-However, there is an additional constraint: Each playthrough must contain
-at least one choice Gogo never made in any of the previous playthroughs.
-
-Return the maximum number of successful playthroughs that Gogo can play.
-If there are no such playthrough, return 0.
-
-DEFINITION
-Class:GogoXReimuHakurai
-Method:solve
-Parameters:vector <string>
-Returns:int
-Method signature:int solve(vector <string> choices)
-
-
-CONSTRAINTS
--choices will contain between 2 and 50 elements, inclusive.
--Each element of choices will contain N characters, where N is the number of elements in choices.
--Each character in choices will be either 'Y', or 'N'.
--If i >= j then choices[i][j] will be 'N'.
-
-
-EXAMPLES
-
-0)
-{
-"NYY",
-"NNY",
-"NNN"}
-
-Returns: 2
-
-The two possible successful playthroughs:
-0 -> 1 -> 2
-0 -> 2
-Gogo may play them both, in any order.
-
-1)
-{
-"NYNY",
-"NNYY",
-"NNNY",
-"NNNN"}
-
-Returns: 3
-
-
-
-2)
-{"NNY"
-,"NNY"
-,"NNN"}
-
-Returns: 1
-
-
-
-3)
-{"NN"
-,"NN"}
-
-Returns: 0
+問題
+小説が0から(N-1)までの全Nステージある
+あるステージAから次のステージBへのルートがあるかどうかのフラグが与えられる
+前のステージへは戻れない
+それまでに通っていないルートを1通りとして何通りのルートがあるか求める
 
 */
 // END CUT HERE
