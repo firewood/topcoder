bebeffd53a139430673d485f8699354648456e10
firewood <karamaki@gmail.com>
2012-02-25 15:32:10 +0900

    translate

--- srm_534/EllysCheckers.cpp
+++ srm_534/EllysCheckers.cpp
@@ -1,85 +1,16 @@
 // BEGIN CUT HERE
 /*
-// SRM 500 Div2 Easy Medium Hard (250 500 1000)
+// SRM 534 Div2 Medium (500)
 
-// PROBLEM STATEMENT
-// Elly and Kriss play a game. The game is played on a single row
-that consists of N cells;
-we will call it the board. The cells of the board are numbered 0 through N-1,
-from the left to the right. Each cell of the board is either empty or occupied
-by a single checker. The girls take alternating turns, until one of them
-cannot make a move. The girl who is unable to make a move loses the game.
+問題
 
-In each move the current player selects a cell containing a checker and
-performs one of the following two types of moves:
-
-A step, in which the checker is moved one cell to the right. The step can
-only be made if the target cell is empty.
-A jump, in which the checker jumps three cells to the right. The jump can
-only be made if the target cell is empty and the cells it jumped over contain
-two other checkers.
-
-Once a checker reaches the rightmost cell, it disappears immediately and
-no longer plays any role in the game.
-
-The initial layout of the board will be given as a string board. The i-th
-character of board will be '.' (a period) if the i-th cell is empty at the
-beginning, and it will be 'o' (lowercase letter o) if the i-th cell initially
-contains a checker. Assume that both girls play optimally. Return "YES"
-(quotes for clarity) if the first player wins the game and "NO" otherwise.
-
-DEFINITION
-Class:EllysCheckers
-Method:getWinner
-Parameters:string
-Returns:string
-Method signature:string getWinner(string board)
-
-
-NOTES
--If there is a checker on the rightmost cell in the beginning of the game,
- it disappears instantly (before the first move is made), as if it were never there.
--The rules of the game ensure that each cell contains at most one checker
- at any time, and that no checker can jump beyond the last cell.
-
-
-CONSTRAINTS
--board will contain between 1 and 20 characters, inclusive.
--Each character of board will be either '.' or 'o'.
-
-
-EXAMPLES
-
-0)
-".o..."
-
-Returns: "YES"
-
-With only one checker it is pretty obvious who will win.
-
-1)
-"..o..o"
-
-Returns: "YES"
-
-Don't forget to ignore checkers on the rightmost cell.
-
-2)
-".o...ooo..oo.."
-
-Returns: "NO"
-
-Here one can jump the checker from cell 5 to cell 8.
-
-3)
-"......o.ooo.o......"
-
-Returns: "YES"
-
-4)
-".o..o...o....o.....o"
-
-Returns: "NO"
+N個のマスからなる盤面がある。
+マスは空または駒が置かれている。
+駒の右が空いていれば駒を一つ右に移動できる。
+駒の右とその右が駒でその右が空いていれば、駒を三つ移動できる。
+最も右の駒は直ちに取り除かれる。
+二人のプレーヤーで交互に移動操作をするとき、動かせなかったら負けとするとき
+最初のプレーヤーが勝つかどうかを求める。
 
 */
 // END CUT HERE
--- srm_534/EllysDirectoryListing.cpp
+++ srm_534/EllysDirectoryListing.cpp
@@ -2,91 +2,12 @@
 /*
 // SRM 534 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// Elly wants to write a program that lists all the files in a given directory.
-She already has the list of all the files.
-You will be given this list as a vector <string> files.
-
-In addition to the names of files, the variable files will contain exactly two additional elements:
-the current directory (the string "."), and the parent directory (the string "..").
-These two elements may be anywhere in files.
-However, Elly wants them to be the last two elements.
-
-In order to move the two directories to the last two positions in files, she wants you to
-perform the following steps:
-
-
-If "." and ".." are the last two elements of files (in any order), you are done.
-Find the first element of files that is either "." or "..". Swap it with the last element of files.
-If "." and ".." are now the last two elements of files (in any order), you are done.
-Find the first element of files that is either "." or "..". Swap it with the element of files that is one position before the last one.
-
-
-Your method must perform the above steps and return a vector <string> containing the modified
-order of elements in files.
-
-DEFINITION
-Class:EllysDirectoryListing
-Method:getFiles
-Parameters:vector <string>
-Returns:vector <string>
-Method signature:vector <string> getFiles(vector <string> files)
-
-
-NOTES
--Swapping elements at positions i and j means that the element at position i is moved to the position j and vice versa.
-
-
-CONSTRAINTS
--files will contain between 2 and 50 elements, inclusive.
--All elements of files will be distinct.
--Exactly one element of files will be ".".
--Exactly one element of files will be "..".
--Each element of files will contain between 1 and 20 characters, inclusive.
--Each element of files will contain only uppercase and lowercase letters and dots ('A'-'Z', 'a'-'z', '.').
-
-
-EXAMPLES
-
-0)
-{"ContestApplet.jnlp", ".", "Image.jpg", "..", "Book.pdf", "Movie.avi"}
-
-Returns: {"ContestApplet.jnlp", "Movie.avi", "Image.jpg", "Book.pdf", "..", "." }
-
-The directories are not the last two elements, so we search for the first one (in this case "."), and swap it with the last element (in this case "Movie.avi"). As "." and ".." are still not the last two elements, we search for the second one (in  this case "..") and swap it with "Book.pdf".
-
-1)
-{"Image.jpg", "..", "."}
-
-Returns: {"Image.jpg", "..", "." }
-
-In this case the directories are already the last two elements, so we do nothing.
-
-2)
-{"..", ".", "Image.jpg"}
-
-Returns: {"Image.jpg", ".", ".." }
-
-After swapping ".." with "Image.jpg", the directories are already the last two elements, so we are done.
-
-3)
-{"No", "..", "Zaphod", ".", "Just", "very", "very...", "Improbable"}
-
-Returns: {"No", "Improbable", "Zaphod", "very...", "Just", "very", ".", ".." }
-
-
-
-4)
-{"www.topcoder.com", "Ever.tried", ".", "Ever.failed", "..", "No", "Matter.", "Try", "Again.", "Fail", "Again..", "Fail.Better"}
-
-Returns: {"www.topcoder.com", "Ever.tried", "Fail.Better", "Ever.failed", "Again..", "No", "Matter.", "Try", "Again.", "Fail", "..", "." }
-
-5)
-{"This", ".", "is", "tricky", "test", ".."}
-
-Returns: {"This", "test", "is", "tricky", "..", "." }
-
-The first swap moves ".." to position 1, so we need to move it again to index 4 (indexed from zero).
+問題
+ディレクトリのファイルのリストがある。
+リストの最後がカレントディレクトリとルートディレクトリでないとき、
+最初に見つかったディレクトリを末尾と交換する。
+それでもリストの最後がカレントディレクトリとルートディレクトリでないとき、
+最初に見つかったディレクトリを末尾のひとつ前と交換する。
 
 */
 // END CUT HERE
