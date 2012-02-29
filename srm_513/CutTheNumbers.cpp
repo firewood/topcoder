4ca4a4822298818d865ecf8f3dfff8aaf2d5d151
firewood <karamaki@gmail.com>
2012-02-29 16:11:15 +0900

    use intrinsic functions

--- srm_513/CutTheNumbers.cpp
+++ srm_513/CutTheNumbers.cpp
@@ -1,4 +1,4 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 513 Div2 Hard (1000)
 
@@ -16,59 +16,64 @@
 #include <string>
 #include <vector>
 #include <set>
+#if _MSC_VER >= 1400
+#include <intrin.h>
+#endif
 
 using namespace std;
 
 typedef vector<string> StrVec;
 
 class CutTheNumbers {
+	static int find_first_set_bit(unsigned int b) {
+#ifdef __GNUC__
+		return __builtin_ctz(b);	// undefined for zero
+#elif _MSC_VER >= 1400
+		unsigned long r; _BitScanForward(&r, b); return r;
+#else
+		unsigned int i = 0; while ((1<<i) & b) ++i; return i;
+#endif
+	}
+
 	int w;
 	int h;
 	int n[4][4];
 	int memo[65536];
 
-	int _rec(int x, int y, int b) {
-		int res = 0;
-		int filled = b;
-		int m = 1 << (y*4+x);
-		int sum = 0;
-		int i;
-		for (i = x; i < w; ++i) {
-			if (b & m) {
-				break;
-			}
-			sum = sum * 10 + n[y][i];
-			filled |= m;
-			m <<= 1;
-			res = max(res, sum + rec(filled));
-		}
-		filled = b;
-		m = 1 << (y*4+x);
-		sum = 0;
-		for (i = y; i < h; ++i) {
-			if (b & m) {
-				break;
-			}
-			sum = sum * 10 + n[i][x];
-			filled |= m;
-			m <<= 4;
-			res = max(res, sum + rec(filled));
-		}
-		return res;
-	}
-
 	int rec(int b) {
 		int &res = memo[b];
 		if (res < 0) {
 			res = 0;
-			int x, y;
-			for (y = 0; y < h; ++y) {
-				for (x = 0; x < w; ++x) {
-					int m = 1 << (y*4+x);
-					if ((b & m) == 0) {
-						return res = _rec(x, y, b);
-					}
+
+			// find position
+			int pos = find_first_set_bit(b);
+			int x = pos & 3;
+			int y = pos / 4;
+
+			int m = 1 << pos;
+			int pat = b;
+			int sum = 0;
+			int i;
+			for (i = x; i < w; ++i) {
+				if (!(b & m)) {
+					break;
 				}
+				sum = sum * 10 + n[y][i];
+				pat ^= m;		// pat &= ~m;
+				m <<= 1;
+				res = max(res, sum + rec(pat));
+			}
+			m = 1 << pos;
+			pat = b;
+			sum = 0;
+			for (i = y; i < h; ++i) {
+				if (!(b & m)) {
+					break;
+				}
+				sum = sum * 10 + n[i][x];
+				pat ^= m;		// pat &= ~m;
+				m <<= 4;
+				res = max(res, sum + rec(pat));
 			}
 		}
 		return res;
@@ -76,17 +81,19 @@ class CutTheNumbers {
 
 public:
 	int maximumSum(vector <string> board) {
-		memset(n, 0, sizeof(n));
-		memset(memo, -1, sizeof(memo));
 		w = board[0].length();
 		h = board.size();
+		unsigned int mask = 0;
 		int i, j;
 		for (i = 0; i < h; ++i) {
 			for (j = 0; j < w; ++j) {
+				mask |= (1 << (i*4+j));
 				n[i][j] = board[i][j] - '0';
 			}
 		}
-		return rec(0);
+		memset(memo, -1, sizeof(memo));
+		memo[0] = 0;
+		return rec(mask);
 	}
 };
 
