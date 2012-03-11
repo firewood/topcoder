b715f06624c0b8128e6d3d4781d1493c0e832e78
firewood <karamaki@gmail.com>
2012-03-11 22:04:32 +0900

    translate

--- srm_536/BinaryPolynomialDivTwo.cpp
+++ srm_536/BinaryPolynomialDivTwo.cpp
@@ -1,93 +1,14 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 536 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// Warning: This problem statement contains superscripts and/or subscripts.
-It may not display properly outside of the applet.
-
-In this problem we will consider binary polynomials. A binary polynomial
-can be written in the following form:
-
-P(x) = a[0] * x0 + a[1] * x1 + ... + a[n] * xn
-
-
-The values a[i] are integer constants (called coefficients). For a binary
-polynomial we must have a[n] = 1 and each other a[i] must be either 0 or 1.
-The number n is called the degree of the polynomial.
-
-
-Several examples:
-
- P(x) = 1 * x0 + 0 * x1 + 1 * x2 is a binary polynomial of degree 2. 
- P(x) = 0 * x0 + 1 * x1 + 0 * x2 + 1 * x3 is a binary polynomial of degree 3. 
- P(x) = 1 * x0 is a binary polynomial of degree 0. 
- P(x) = 0 * x0 + 3 * x1 - 1 * x2 is not a binary polynomial, because each coefficient must be a 0 or a 1. 
- P(x) = 0 * x0 is not a valid binary polynomial, because the last of the values a[i] must be 1. 
-
-We can evaluate a binary polynomial for the inputs x = 0 and x = 1.
-In order to do so, we just have to substitute 0 or 1 for x in the above expression,
-compute the value of the expression and take the remainder it gives when
-divided by two. For example, if P(x) = 1 * x0 + 0 * x1 + 1 * x2,
-then P(0) = 1 * 00 + 0 * 01 + 1 * 02 = 1 and P(1) = 1 * 10 + 0 * 11 + 1 * 12 = 0
-(modulo 2). Note that in this problem we assume that x0 = 1 for all x.
-In particular, also 00 = 1.
-
-
-We call an integer x (where x is 0 or 1) a root of the binary polynomial P if P(x) = 0.
-You are given a binary polynomial P as the array a of its coefficients.
-Return the number of roots of that binary polynomial.
-
-DEFINITION
-Class:BinaryPolynomialDivTwo
-Method:countRoots
-Parameters:vector <int>
-Returns:int
-Method signature:int countRoots(vector <int> a)
-
-
-CONSTRAINTS
--The degree of P will be between 0 and 49, inclusive.
--a will contain exactly n+1 elements, where n is the degree of P.
--Each element of a will be either 0 (zero) or 1 (one).
--a[n] will be equal to 1 (one).
-
-
-EXAMPLES
-
-0)
-{1, 0, 1}
-
-Returns: 1
-
-The example from the problem statement. The only root of this binary polynomial is 1.
-
-1)
-{0, 1, 0, 1}
-
-Returns: 2
-
-This is the maximum possible answer; both 0 and 1 are roots of this binary polynomial.
-
-2)
-{1}
-
-Returns: 0
-
-This binary polynomial has no roots - it always evaluates to 1.
-
-3)
-{1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1}
-
-Returns: 0
-
-
-
-4)
-{1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0,
- 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1}
-
-Returns: 1
+問題
+P(x) = a[0] * x^0 + a[1] * x^1 + ... + a[n] * x^nという多項式がある
+a[i]は0か1のいずれかである
+xとして0か1のいずれかを与える
+a[n]は必ず1が与えられる
+x^0は1である
+P(x) mod 2が0の場合がいくつかを求める
 
 */
 // END CUT HERE
@@ -115,8 +36,8 @@ class BinaryPolynomialDivTwo {
 
 public:
 	int countRoots(vector <int> a) {
-		int result = accumulate(a.begin(), a.end(), 0) % 2 == 0;
-		result += a[0] == 0;
+		int result = a[0] == 0;
+		result += accumulate(a.begin(), a.end(), 0) % 2 == 0;
 		return result;
 	}
 
@@ -190,7 +111,6 @@ public:
 // BEGIN CUT HERE
 int main() {
 	BinaryPolynomialDivTwo ___test;
-	___test.run_test(0);
 	___test.run_test(-1);
 	return 0;
 }
--- srm_536/RollingDiceDivTwo.cpp
+++ srm_536/RollingDiceDivTwo.cpp
@@ -1,94 +1,12 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 536 Div2 Medium (500)
 
-// PROBLEM STATEMENT
-// Byteasar is playing a tabletop role-playing game with his friends.
-To determine the effectiveness of their heroes' actions the players
-use a rather unique set of dice which may have nonequal number of faces.
-Each die has between 1 and 9 faces, inclusive. If a die has m faces,
-they contain precisely all the values between 1 and m, inclusive.
-More precisely, for each k between 1 and m, inclusive, there is one face
-that shows exactly k pips. When a die is cast, every face has equal
-probability to come out on top.
-
-Every time all the dice were thrown at once, Byteasar wrote down the numbers
-of pips visible on each of the topmost faces (in any order). The results
-of the i-th throw are given in throws[i]; the length of throws[i] is
-equal to the number of dice and each character of throws[i] denotes the
-number of pips visible on one of the topmost faces. For example,
-if throws[3][0]='7', this means that in throw 3 (0-based index) one of the
-dice showed exactly 7 pips on the top. Please note that the ordering of
-dice may be different for different throws.
-
-Given the vector <string> throws containing Byteasar's notes, return the
-minimum possible total number of faces of all dice.
-
-DEFINITION
-Class:RollingDiceDivTwo
-Method:minimumFaces
-Parameters:vector <string>
-Returns:int
-Method signature:int minimumFaces(vector <string> rolls)
-
-
-NOTES
--Please note that a die can have as few as one or two faces.
-
-
-CONSTRAINTS
--rolls will contain between 1 and 50 elements, inclusive.
--rolls[0] will contain between 1 and 50 characters, inclusive.
--All elements of rolls will contain the same number of characters.
--Each character in each element of rolls will be one of '1'-'9'.
-
-
-EXAMPLES
-
-0)
-{"137", "364", "115", "724"}
-
-Returns: 14
-
-In the first throw the numbers of pips on the topmost faces of the dice
-were 1, 3 and 7; in the second throw they were 3, 6 and 4, in the third
-they were 1, 1 and 5 and in the fourth roll they were 7, 2 and 4.
-The players may have used dice with 3, 4 and 7 faces, giving a total of
-14 faces. No other possible set of dice has less faces in total.
-
-1)
-{"1112", "1111", "1211", "1111"}
-
-Returns: 5
-
-The players could have used three dice with one face each and one die with two faces.
-
-2)
-{"24412", "56316", "66666", "45625"}
-
-Returns: 30
-
-The players could have used five dice with six faces each.
-
-3)
-{"931", "821", "156", "512", "129", "358", "555"}
-
-Returns: 19
-
-
-
-4)
-{"3", "7", "4", "2", "4"}
-
-Returns: 7
-
-
-
-5)
-{"281868247265686571829977999522", "611464285871136563343229916655", "716739845311113736768779647392", "779122814312329463718383927626",
-"571573431548647653632439431183", "547362375338962625957869719518", "539263489892486347713288936885", "417131347396232733384379841536"}
-
-Returns: 176
+問題
+面の数が不明なサイコロがx個ある
+それぞれの面の数は1から9のいずれかであり、出目の確率は等しい
+n回サイコロを振った出目が与えられる
+面の数の合計の最小値を求める
 
 */
 // END CUT HERE
@@ -107,10 +25,6 @@ Returns: 176
 #include <sstream>
 
 using namespace std;
-
-typedef long long LL;
-typedef vector<int> IntVec;
-typedef vector<string> StrVec;
 
 class RollingDiceDivTwo {
 
