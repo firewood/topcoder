fb654dc3f8f9899be4578e22682de4375e765c36
firewood <karamaki@gmail.com>
2012-03-20 01:48:51 +0900

    translate

--- srm_508/YetAnotherORProblem2.cpp
+++ srm_508/YetAnotherORProblem2.cpp
@@ -1,72 +1,9 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 508 Div2 Hard (1000)
 
-// PROBLEM STATEMENT
-// NOTE: This problem statement contains subscripts
-that may not display properly if viewed outside of the applet.
-
-You're given ints N and R. Count the number of sequences A0, A1, ..., AN-1
-such that each Ai is an integer satisfying 0 <= Ai <= R and
-A0 + A1 + ... + AN-1 = A0 | A1 | ... | AN-1.
-The '|' symbol stands for bitwise OR of the operands.
-Return the number of such sequences modulo 1,000,000,009.
-
-DEFINITION
-Class:YetAnotherORProblem2
-Method:countSequences
-Parameters:int, int
-Returns:int
-Method signature:int countSequences(int N, int R)
-
-
-NOTES
--If a and b are single bits then a | b is defined as max(a, b).
-For two integers, A and B, in order to calculate A | B,
-they need to be represented in binary: A = (an...a1)2, B = (bn...b1)2
-(if the lengths of their representations are different,
-the shorter one is prepended with the necessary number of leading zeroes).
-Then A | B = C = (cn...c1)2, where ci = ai | bi.
-For example, 10 | 3 = (1010)2 | (0011)2 = (1011)2 = 11.
-
-
-CONSTRAINTS
--N will be between 2 and 10, inclusive.
--R will be between 1 and 15000, inclusive.
-
-
-EXAMPLES
-
-0)
-2
-2
-
-Returns: 7
-
-The possible sequences are: {0,0}, {0,1}, {0,2}, {1,0}, {1,2}, {2,0}, {2,1}.
-
-
-1)
-2
-3
-
-Returns: 9
-
-Now we can also have {0,3} and {3,0}.
-
-
-2)
-3
-3
-
-Returns: 16
-
-
-3)
-7
-1023
-
-Returns: 73741815
+問題
+R以下のN個の数値において、和と論理和が等しい組み合わせの数を求める。
 
 */
 // END CUT HERE
