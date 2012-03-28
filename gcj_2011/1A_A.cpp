6b512fdd52f574fd2fd40bcdd01348593e452dfe
firewood <karamaki@gmail.com>
2012-03-29 02:12:42 +0900

    simplified

--- gcj_2011/1A_A.cpp
+++ gcj_2011/1A_A.cpp
@@ -1,136 +1,109 @@
 /*
 
-Round 1A
+Google Code Jam 2011 Round 1A
+
 Problem A. FreeCell Statistics
+
+
+Problem
+
+I played D (D > 0) games of FreeCell today. Each game of FreeCell ends
+in one of two ways -- I either win, or I lose. I've been playing for
+many years, and have so far played G games in total (obviously, G >= D).
+
+At the end of the day, I look at the game statistics to see how well I have
+played. It turns out that I have won exactly PD percent of the D games today,
+and exactly PG percent of G total games I had ever played. Miraculously,
+there is no rounding necessary -- both percentages are exact! Unfortunately,
+I don't remember the exact number of games that I have played today (D),
+or the exact number of games that I have played in total (G). I do know that
+I could not have played more than N games today (D <= N).
+
+Are the percentages displayed possible, or is the game statistics calculator broken?
+
+
+Input
+
+The first line of the input gives the number of test cases, T. T lines follow.
+Each line contains 3 integers -- N, PD and PG.
+
+
+Output
+
+For each test case, output one line containing "Case #x: y", where x is the
+case number (starting from 1) and y is either "Possible" or "Broken".
+
+
+Limits
+
+0 <= PD <= 100;
+0 <= PG <= 100.
+
+Small dataset
+
+1 <= T <= 100;
+1 <= N <= 10.
+
+Large dataset
+
+1 <= T <= 2000;
+1 <= N <= 10^15.
+
+
+Sample
+
+Input
+
+3
+1 100 50
+10 10 100
+9 80 56
+
+Output
+
+Case #1: Possible
+Case #2: Broken
+Case #3: Possible
+
+In Case #3, I could have played 5 games today (D = 5) and 25 games in total
+(G = 25), and won 4 games today (80% of 5) and 14 games in total (56% of 25).
 
 */
 
-
-#include <stdio.h>
-#include <string.h>
-#include <list>
-#include <vector>
-#include <map>
-#include <set>
+#include <iostream>
 
 using namespace std;
 
-typedef __int64 __ll;
+typedef __int64 LL;
 
-class GCJ {
-	static const size_t BufferSize = 1024000;
-	FILE *m_Source;
-	size_t m_BufferSize;
-	char *m_Buffer;
+LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
 
-public:
-	GCJ(FILE *source, size_t buffer_size = BufferSize) {
-		m_Source = source;
-		m_BufferSize = buffer_size;
-		m_Buffer = new char[m_BufferSize];
+static bool solve(LL n, LL pd, LL pg) {
+	if (pg == 0 || pg == 100) {
+		return pd == pg;
 	}
-
-	~GCJ() {
-		delete [] m_Buffer;
-	}
-
-	bool solve(void) {
-		int cases;
-		if (!getInt(cases) || cases < 1) {
-			return false;
-		}
-
-		int solved = 0;
-		int i;
-		for (i = 0; i < cases; ++i) {
-			if (solve(i)) {
-				++solved;
-			}
-		}
-		return solved >= cases;
-	}
-
-private:
-	int gcd(int m, int n) {
-		if (n == 0) {
-			return m;
-		}
-		if ((m % n) == 0) {
-			return n;
-		}
-		return gcd(n, m % n);
-	}
-
-	int lcm(int m, int n) {
-		int g = gcd(m, n);
-		return m * n / g;
-	}
-
-	bool solve(__ll N, int PD, int PG) {
-		if (PG == 0 || PG == 100) {
-			return PD == PG;
-		}
-		if (PD == 0 || PD == 100) {
-			return true;
-		}
-		int g = gcd(100, PD);
-		__ll a = 100 / g;
-		return a <= N;
-	}
-
-	bool solve(int case_index) {
-		char *p = getSingleLine();
-		if (!p) return false;
-
-		__ll N;
-		int PD, PG;
-		if (sscanf(p, "%lld %d %d", &N, &PD, &PG) != 3) {
-			return false;
-		}
-
-		// N: known
-		// G: unknown
-		// D: unknown
-		// 1 <= D <= N
-		// 1 <= D <= G
-
-		bool result = solve(N, PD, PG);
-		printf("Case #%d: %s\n", case_index + 1, result ? "Possible" : "Broken");
+	if (pd == 0 || pd == 100) {
 		return true;
 	}
-
-	int getCases(void) {
-		int cases;
-		char *p = getSingleLine();
-		if (p != NULL && sscanf(p, "%d", &cases) >= 1) {
-			return cases;
-		}
-		return -1;
-	}
-
-	bool getInt(int &i) {
-		char *p = getSingleLine();
-		return p != NULL && sscanf(p, "%d", &i) >= 1;
-	}
-
-	char *getSingleLine(void) {
-		char *p = fgets(m_Buffer, m_BufferSize, m_Source);
-		if (!p) {
-			return NULL;
-		}
-		char *term = p + strlen(p) - 1;
-		if (term > p && *term == '\n') {
-			*term = 0;
-		}
-		return p;
-	}
-
-};
-
+	LL g = gcd(100, pd);
+	LL a = 100 / g;
+	return a <= n;
+}
 
 int main()
 {
-	GCJ gcj(stdin);
-	return gcj.solve() ? 0 : -1;
+	int T;
+	cin >> T;
+	int i;
+	for (i = 0; i < T; ++i) {
+		// n: max games
+		// pd: won
+		// pg: played
+		LL n, pd, pg;
+		cin >> n >> pd >> pg;
+		cout << "Case #" << (i+1) << ": " << (solve(n, pd, pg) ? "Possible" : "Broken") << endl;
+	}
+
+	return 0;
 }
 
