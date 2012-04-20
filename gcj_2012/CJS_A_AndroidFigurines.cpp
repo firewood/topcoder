6165667c140d982a32b62a7aea99b10ae15ec2c7
firewood <karamaki@gmail.com>
2012-04-20 13:49:34 +0900

    add statement

--- gcj_2012/CJS_A_AndroidFigurines.cpp
+++ gcj_2012/CJS_A_AndroidFigurines.cpp
@@ -1,6 +1,71 @@
+/*
 
-// Code Jam Sprint
-// A. Android Figurines
+Code Jam Sprint
+Problem A. Android Figurines
+
+
+Problem
+
+A new series of Android mini figurines is on sale at the Google Store.
+The impulse to collect them and gift them to your friends is too strong to
+resist. You want to buy at least K1, K2, ... KM of each of the M new designs.
+The friendly store staff reveal that each design has exactly L packages
+in stock, with one figurine per package, but all the packages look the same
+with no identifiable labels on the outside, and you can't open them before
+the purchase. What is the minimum number of packages you need to buy to
+achieve your shopping goal in the worst case scenario?
+
+
+Input
+
+The first line of the input gives the number of test cases, N. N test cases
+follow. Each case is a line formatted as
+
+L M K1 K2 ... KM
+
+
+Output
+
+For each test case, output one line containing "Case #x: y", where x is the
+case number (starting from 1) and y is the minimum number of packages you
+need to buy in the worst case scenario (or -1 if the goal cannot be achieved).
+
+
+Limits
+
+1 <= N <= 100.
+
+
+Small dataset
+
+0 <= L <= 3.
+1 <= M <= 3.
+0 <= Kj <= 10, for 1 <= j <= M.
+
+
+Large dataset
+
+0 <= L <= 100.
+1 <= M <= 100.
+0 <= Kj <= 100, for 1 <= j <= M.
+
+
+Sample
+
+
+Input
+
+2
+2 3 1 1 1
+3 3 2 3 4
+
+
+Output
+
+Case #1: 5
+Case #2: -1
+
+*/
 
 #include <algorithm>
 #include <vector>
