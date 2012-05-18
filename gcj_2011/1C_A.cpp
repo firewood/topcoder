279e233c99ac2d15c626276dc945016e9b33e018
firewood <karamaki@gmail.com>
2012-05-19 02:00:46 +0900

    translate

--- gcj_2011/1C_A.cpp
+++ gcj_2011/1C_A.cpp
@@ -1,108 +1,14 @@
-/*
+﻿/*
 
 Google Code Jam 2011 Round 1C
 
 Problem A. Square Tiles
 
 
-Problem
-
-You are selling beautiful geometric pictures. Each one consists of 1x1 square
-tiles arranged into a non-overlapping grid. For example:
-
-    .##..
-    .####
-    .####
-    .##..
-Blue tiles are represented by '#' characters, and white tiles are represented
-by '.' characters. You do not use other colors.
-Not everybody likes blue though, and some customers want you to replace all
-the blue tiles in your picture with red tiles. Unfortunately, red tiles only
-come in the larger 2x2 size, which makes this tricky.
-
-You can cover any 2x2 square of blue tiles with a single red tile, and then
-repeat until finished. A red tile cannot overlap another red tile, it cannot
-cover white tiles, and it cannot go outside the picture. For example, you
-could add red tiles to the previous picture as follows:
-
-    ./\..
-    .\//\
-    ./\\/
-    .\/..
-Each red tile is represented here by a pair of '/' characters in the top-left
-and bottom-right corners, and a pair of '\' characters in the other two corners.
-Given a blue and white picture, can you transform it into a red and white
-picture in this way?
-
-
-Input
-
-The first line of the input gives the number of test cases, T. T test cases follow.
-
-Each test case begins with a line containing R and C, the number of rows and
-columns in a picture. The next R lines each contain exactly C characters,
-describing the picture. As above, '#' characters represent blue tiles, and '.'
-characters represent white tiles.
-
-
-Output
-
-For each test case, first output one line containing "Case #x:" where x is the
-case number (starting from 1).
-
-If it is possible to cover the blue tiles with non-overlapping red tiles,
-output R lines each containing C characters, describing the resulting red and
-white picture. As above, red tiles should be represented by '/' and '\' characters,
-while white tiles are represented by '.' characters. If multiple solutions are
-possible, you may output any of them.
-
-If the task is impossible, output a single line containing the text "Impossible" instead.
-
-
-Limits
-
-Small dataset
-
-1 <= T <= 20.
-1 <= R <= 6.
-1 <= C <= 6.
-
-Large dataset
-
-1 <= T <= 50.
-1 <= R <= 50.
-1 <= C <= 50.
-
-
-Sample
-
-
-Input
-
-3
-2 3
-###
-###
-1 1
-.
-4 5
-.##..
-.####
-.####
-.##..
-
-
-Output
-
-Case #1:
-Impossible
-Case #2:
-.
-Case #3:
-./\..
-.\//\
-./\\/
-.\/..
+問題
+グリッドに1x1の正方形のタイルが敷いてある。
+全てのタイルが敷いてある場所を2x2のタイルで置き換えたい。
+可能かどうか、可能な場合は敷き方を求める。
 
 */
 
--- gcj_2011/1C_C.cpp
+++ gcj_2011/1C_C.cpp
@@ -5,74 +5,10 @@ Google Code Jam 2011 Round 1C
 Problem C. Perfect Harmony
 
 
-Problem
-
-Jeff is a part of the great Atlantean orchestra. Each player of the orchestra
-has already decided what sound will he play (for the sake of simplicity we
-assume each player plays only one sound). We say two sounds are in harmony
-if the frequency of any one of them divides the frequency of the other
-(that's a pretty restrictive idea of harmony, but the Atlanteans are known
-to be very conservative in music). Jeff knows that the notes played by other
-players are not necessarily in harmony with each other. He wants his own note
-to improve the symphony, so he wants to choose his note so that it is in
-harmony with the notes all the other players play.
-
-Now, this sounds simple (as all the frequencies are positive integers, it
-would be enough for Jeff to play the note with frequency 1, or, from the other
-side, the Least Common Multiple of all the other notes), but unfortunately
-Jeff's instrument has only a limited range of notes available. Help Jeff
-find out if playing a note harmonious with all others is possible.
-
-
-Input
-
-The first line of the input gives the number of test cases, T. T test cases
-follow. Each test case is described by two lines. The first contains three
-numbers: N, L and H, denoting the number of other players, the lowest and
-the highest note Jeff's instrument can play. The second line contains N
-integers denoting the frequencies of notes played by the other players.
-
-
-Output
-
-For each test case, output one line containing "Case #x: y", where x is the
-case number (starting from 1) and y is either the string "NO" (if Jeff cannot
-play an appropriate note), or a possible frequency. If there are multiple
-frequencies Jeff could play, output the lowest one.
-
-
-Limits
-
-1 <= T <= 40.
-
-Small dataset
-
-1 <= N <= 100.
-1 <= L <= H <= 10000.
-All the frequencies are no larger than 10000.
-
-Large dataset
-
-1 <= N <= 104.
-1 <= L <= H <= 10^16
-All the frequencies are no larger than 10^16
-
-
-Sample
-
-Input
-
-2
-3 2 100
-3 5 7
-4 8 16
-1 20 5 2
-
-
-Output
-
-Case #1: NO
-Case #2: 10
+問題
+ある二つの数があり、片方がもう片方で割り切れるとき、ハーモニーと呼ぶことにする。
+数値の配列Aが与えられる。
+L以上H以下でAの全てとハーモニーが成り立つ数を求める。
 
 */
 
@@ -99,9 +35,6 @@ LL solve(int N, LL L, LL H, IntVec &v) {
 	for (x = L; x <= H; ++x) {
 		LLSet::const_iterator it;
 		for (it = m.begin(); it != m.end(); ++it) {
-			if (*it == x) {
-				break;
-			}
 			if ((*it % x) != 0 && (x % *it) != 0) {
 				break;
 			}
