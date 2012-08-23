d9b1ef5926b99d72fc1b0517faaf073e40bd57f0
firewood <karamaki@gmail.com>
2012-08-24 02:53:25 +0900

    translate

--- srm_553/Suminator.cpp
+++ srm_553/Suminator.cpp
@@ -1,121 +1,13 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 553 Div1 Easy (250)
 
-// PROBLEM STATEMENT
-// Suminator is a very limited programming language.
-A program in this language is simply a sequence of nonnegative integers.
-The program is evaluated using a stack of nonnegative integers.
-Initially, the stack is empty.
-Trying to pop an element from an empty stack returns a zero.
-(Alternately, you can imagine that the stack already contains a sufficient
-number of zeros in the beginning.)
-
-A Suminator program is evaluated using the following algorithm:
-
-for i = 0 to length(program) - 1 {
-    if ( program[i] is 0) {
-         Pop the top two elements from the stack, add them together
-         and push the result to the top of the stack.
-    } else {
-         Push program[i] to the top of the stack.
-    }
-}
-Pop the top element of the stack and print it.
-
-For example, when executing the program {1}, we first push the 1 to the stack,
-and then we print it. 
-When executing the program {5,0,1,2,0}, we take the following steps:
-
-We push the 5 to the top of the stack.
-We pop the top two elements (5 and 0), add them together and push the result (5).
-We push the 1 to the top of the stack.
-We push the 2 to the top of the stack. At this moment, the stack contains the
-values 5, 1, and 2 (from bottom to top).
-We pop the top two elements (2 and 1), add them together and push the result (3).
-We print the top element of the stack: the number 3. Note that the stack
-also contains the value 5, which is ignored.
-
-
-You are given a vector <int> program containing a Suminator program
-(a sequence of nonnegative integers), in which one of the elements of the
-sequence was changed to -1.
-You are also given a int wantedResult.
-Your task is to change the -1 back to a nonnegative integer X in such a way
-that the resulting program prints the number wantedResult.
-Return X.
-If there are multiple possible values of X, return the smallest one.
-If there is no way to make the program print wantedResult, return -1 instead.
-
-
-DEFINITION
-Class:Suminator
-Method:findMissing
-Parameters:vector <int>, int
-Returns:int
-Method signature:int findMissing(vector <int> program, int wantedResult)
-
-
-NOTES
--The return value always fits into an int. This follows from the constraints
- and the nature of the problem.
-
-
-CONSTRAINTS
--program will contain between 1 and 50 elements, inclusive.
--Each element of program will be between -1 and 1000000000 (10^9), inclusive.
--program will contain -1 exactly once.
--wantedResult will be between 1 and 1000000000 (10^9), inclusive.
-
-
-EXAMPLES
-
-0)
-{7,-1,0}
-10
-
-Returns: 3
-
-
-1)
-{100, 200, 300, 0, 100, -1}
-600
-
-Returns: 0
-
-
-2)
-{-1, 7, 3, 0, 1, 2, 0, 0}
-13
-
-Returns: 0
-
-We can replace the first element with many other values, but 0 is the smallest that achieves the wanted result.
-
-
-3)
-{-1, 8, 4, 0, 1, 2, 0, 0}
-16
-
-Returns: -1
-
-It does not matter what value we use in the first element of the array, the result will always be 15.
-
-
-4)
-{1000000000, 1000000000, 1000000000,  1000000000, -1, 0, 0, 0, 0}
-1000000000
-
-Returns: -1
-
-It does not matter what we replace the -1 with, the result will be larger than 1000000000.
-
-
-5)
-{7, -1, 3, 0}
-3
-
-Returns: -1
+問題
+Suminatorは加算だけ可能なスタックマシンである。
+プログラムとして非負の整数の配列が与えられ、順番に実行する。
+スタックへは、プログラムが0なら2値を足して格納し、0以外ならそのまま格納する。
+最後にスタックの先頭を表示して終了する。
+一箇所だけ書き換えて結果をwantedResultにするための最小の数値を求める。
 
 */
 // END CUT HERE
@@ -175,7 +67,6 @@ public:
 		}
 		return -1;
 	}
-
 
 // BEGIN CUT HERE
 private:
@@ -262,7 +153,6 @@ public:
 // BEGIN CUT HERE
 int main() {
 	Suminator ___test;
-//	___test.run_test(2);
 	___test.run_test(-1);
 	return 0;
 }
