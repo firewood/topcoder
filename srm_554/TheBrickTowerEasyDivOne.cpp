1701416c70d97942cc1e7f7775eb9100decafdc5
firewood <karamaki@gmail.com>
2012-09-03 07:04:58 +0900

    transtalte

--- srm_554/TheBrickTowerEasyDivOne.cpp
+++ srm_554/TheBrickTowerEasyDivOne.cpp
@@ -2,82 +2,10 @@
 /*
 // SRM 554 Div1 Easy (250)
 
-// PROBLEM STATEMENT
-// 
-John and Brus are building towers using toy bricks.
-They have two types of bricks: red and blue ones.
-The number of red bricks they have is redCount and each of them has a height of redHeight.
-The number of blue bricks they have is blueCount and each of them has a height of blueHeight.
-
-A tower is built by placing bricks one atop another.
-A brick can be placed either on the ground, or on a brick of a different color.
-(I.e., you are not allowed to put two bricks of the same color immediately on one another.)
-A tower has to consist of at least one brick.
-The height of a tower is the sum of all heights of bricks that form the tower.
-Two towers are considered to be different if they have different heights.
-(Two towers of the same height are considered the same, even if they differ in the number and colors of bricks that form them.)
-
-You are given the ints redCount, redHeight, blueCount and blueHeight.
-Return the number of different towers that John and Brus can build.
-
-
-DEFINITION
-Class:TheBrickTowerEasyDivOne
-Method:find
-Parameters:int, int, int, int
-Returns:int
-Method signature:int find(int redCount, int redHeight, int blueCount, int blueHeight)
-
-
-CONSTRAINTS
--redCount will be between 1 and 474,747,474, inclusive.
--redHeight will be between 1 and 474,747,474, inclusive.
--blueCount will be between 1 and 474,747,474, inclusive.
--blueHeight will be between 1 and 474,747,474, inclusive.
-
-
-EXAMPLES
-
-0)
-1
-2
-3
-4
-
-Returns: 4
-
-John and Brus have 1 red brick of height 2 and 3 blue bricks of height 4. Using these bricks, it's possible to build 4 towers:
-red (height 2);
-blue (height 4);
-red, blue (height 6);
-blue, red, blue (height 10).
-
-
-1)
-4
-4
-4
-7
-
-Returns: 12
-
-
-2)
-7
-7
-4
-4
-
-Returns: 13
-
-
-3)
-47
-47
-47
-47
-
-Returns: 94
+問題
+二種類のブロックを交互に積む。
+各ブロックの高さと個数が与えられる。
+何通りの高さが作れるか求める。
 
 */
 // END CUT HERE
