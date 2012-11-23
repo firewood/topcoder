6911a6074f7e98c00693bac5d6204a2c98d1492e
firewood <karamaki@gmail.com>
2012-11-24 06:54:28 +0900

    translate

--- srm_559/BlockTower.cpp
+++ srm_559/BlockTower.cpp
@@ -1,77 +1,11 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 559 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// Josh loves playing with blocks. Currently, he has N blocks, labeled 0 through N-1. The heights of all blocks are positive integers. More precisely, for each i, the height of block i is blockHeights[i]. Josh is interested in making the tallest block tower possible. He likes all his towers to follow three simple rules:
-
-The blocks must be stacked in a single column, one atop another. The height of the tower is simply the sum of heights of all its blocks.
-The labels of blocks used in the tower must increase from the bottom to the top. In other words, whenever Josh places box x on top of box y, we have x > y.
-Josh will never place a box of an even height on top of a box of an odd height.
-
-You are given the vector <int> blockHeights. Return the height of the tallest possible block tower Josh can build.
-
-DEFINITION
-Class:BlockTower
-Method:getTallest
-Parameters:vector <int>
-Returns:int
-Method signature:int getTallest(vector <int> blockHeights)
-
-
-CONSTRAINTS
--blockHeights will contain between 1 and 50 elements, inclusive.
--Each element of blockHeights will be between 1 and 50, inclusive.
-
-
-EXAMPLES
-
-0)
-{4,7}
-
-Returns: 11
-
-The optimal tower contains both blocks. Block 0 is on the bottom of the tower.
-
-
-1)
-{7,4}
-
-Returns: 7
-
-This time the optimal tower contains just block 0. Josh cannot put block 1 on top of it, because 4 is even and 7 is odd.
-
-
-2)
-{7}
-
-Returns: 7
-
-
-3)
-{4}
-
-Returns: 4
-
-
-4)
-{48,1,50,1,50,1,48}
-
-Returns: 196
-
-Note that in a valid tower the labels of the blocks have to increase from bottom to top. Their heights do not have to. In this case the optimal tower consists of blocks 0, 2, 4, and 6, in this order. Its total height is 48 + 50 + 50 + 48 = 196.
-
-
-5)
-{49,2,49,2,49}
-
-Returns: 147
-
-
-6)
-{44,3,44,3,44,47,2,47,2,47,2}
-
-Returns: 273
+問題
+N個のブロックを順番に積む。
+奇数の高さのブロックの上には偶数の高さのブロックは積めない。
+最大の高さを求める。
 
 */
 // END CUT HERE
--- srm_559/HyperKnight.cpp
+++ srm_559/HyperKnight.cpp
@@ -1,102 +1,10 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 559 Div1 Easy (250)
 
-// PROBLEM STATEMENT
-// Fernando loves to play chess. One day he decided to play chess on an unusually large rectangular board. To compensate for the board's size he also decided to change the distance a knight can move in a single jump.
-
-To describe the moves easily, we will now introduce a coordinate system. Each cell of the chessboard can be described using two integers (r,c): its row number and its column number. Now, if we have a piece at (r,c), the move (x,y) takes the piece to the cell (r+x,c+y).
-
-The new chess piece will be called an (a,b)-hyperknight. The hyperknight always has 8 possible moves: (+a,+b), (+a,-b), (-a,+b), (-a,-b), (+b,+a), (+b,-a), (-b,+a), and (-b,-a). Note that the original chess knight is a (2,1)-hyperknight.
-
-Of course, as the chessboard is finite, it is not always possible to make each of the 8 moves. Some of them may cause our hyperknight to leave the chessboard. A move is called valid if the destination cell is on the chessboard. Fernando would like to know the number of cells on his board such that his hyperknight will have exactly k valid moves from that cell.
-
-You are given the ints a, b, numRows, numColumns and k. The values numRows and numColumns define the number of rows and number of columns on the chessboard, respectively. The other three values were already explained above. Compute and return the number of cells on the chessboard that have exactly k valid (a,b)-hyperknight moves.
-
-DEFINITION
-Class:HyperKnight
-Method:countCells
-Parameters:int, int, int, int, int
-Returns:long long
-Method signature:long long countCells(int a, int b, int numRows, int numColumns, int k)
-
-
-NOTES
--If you wish, you may assume that the rows are numbered 0 through numRows-1 and columns 0 through numColumns-1. However, note that the actual row/column numbers do not matter, as long as they are consecutive.
-
-
-CONSTRAINTS
--a will be between 1 and 1,000,000,000 (10^9), inclusive.
--b will be between 1 and 1,000,000,000 (10^9), inclusive.
--a will not be equal to b.
--numRows will be between 1 and 1,000,000,000 (10^9), inclusive.
--numColumns will be between 1 and 1,000,000,000 (10^9), inclusive.
--2*max(a,b) will be strictly less than min(numRows,numColumns).
--k will be between 0 and 8, inclusive.
-
-
-EXAMPLES
-
-0)
-2
-1
-8
-8
-4
-
-Returns: 20
-
-This is a standard chessboard. We have a traditional chess knight and we are looking for cells such that the knight has exactly 4 valid moves.
-
-
-1)
-3
-2
-8
-8
-2
-
-Returns: 16
-
-
-2)
-1
-3
-7
-11
-0
-
-Returns: 0
-
-
-3)
-3
-2
-10
-20
-8
-
-Returns: 56
-
-
-4)
-1
-4
-100
-10
-6
-
-Returns: 564
-
-
-5)
-2
-3
-1000000000
-1000000000
-8
-
-Returns: 999999988000000036
+問題
+チェス盤の大きさと(±a,±b)で動けるナイトが与えられる。
+移動可能な先がちょうどk箇所ある升目の総数を求める。
 
 */
 // END CUT HERE
