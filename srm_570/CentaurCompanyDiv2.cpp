d21113391a3105531846590fb78e867adf4e3ee2
firewood <karamaki@gmail.com>
2013-02-23 23:37:37 +0900

    translate

--- srm_570/CentaurCompanyDiv2.cpp
+++ srm_570/CentaurCompanyDiv2.cpp
@@ -1,91 +1,21 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 SRM 570 Div2 Hard (1000)
 
-// PROBLEM STATEMENT
-// The Centaur company has N servers, numbered 1 through N.
-These servers are currently connected into a network.
-The topology of the network is a tree.
-In other words, there are exactly N-1 bidirectional cables, each connecting some two servers in such a way that the entire network is connected.
-
-The Centaur company is about to split into two new companies: the Human company and the Horse company.
-When this happens, the companies will divide the servers somehow.
-Once they divide their servers, they will cut each cable that connects a server of the Horse company and a server of the Human company.
-
-While the Horse company has a lot of cables, the Human company does not have any.
-Therefore, when dividing the servers, the Human company must get a set of servers that will remain connected after the cables are cut.
-
-You are given two vector <int>s a and b, each containing N-1 elements.
-These two arrays describe the original cables:
-for each i, there is a cable that connects the servers a[i] and b[i].
-
-Compute and return the number of different ways in which the two companies may divide the servers.
-(It is possible that one of the companies will get no servers at all.)
-
-DEFINITION
-Class:CentaurCompanyDiv2
-Method:count
-Parameters:vector <int>, vector <int>
-Returns:long long
-Method signature:long long count(vector <int> a, vector <int> b)
-
-
-NOTES
--N can be determined as (1 + the length of a).
-
-
-CONSTRAINTS
--N will be between 2 and 51, inclusive.
--a and b will contain exactly N-1 elements.
--Each element of a and b will be between 1 and N, inclusive.
--The network defined by a and b will be a tree (as explained in the problem statement).
-
-
-EXAMPLES
-
-0)
-{1}
-{2}
-
-Returns: 4
-
-There are 2^2 = 4 ways to divide the servers between two companies. For any division, the Human company's servers will remain connected.
-
-1)
-{2,2}
-{1,3}
-
-Returns: 7
-
-There are 2^3 = 8 ways to divide the servers between two companies. However, if the Human company gets server 1 and server 3, and the Horse company gets server 2, the Human company's servers will not be connected. Therefore the number of valid ways is 8 - 1 = 7.
-
-2)
-{1,2,3,4,5,6,7,8,9}
-{2,3,4,5,6,7,8,9,10}
-
-Returns: 56
-
-
-
-3)
-{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-{2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51}
-
-Returns: 1125899906842675
-
-The answer overflows a 32-bit integer data type.
-
-4)
-{10, 7, 2, 5, 6, 2, 4, 9, 7}
-{8, 10, 10, 4, 1, 6, 2, 2, 3}
-
-Returns: 144
-
-
+問題
+-株式会社ケンタウロスにはN台のサーバがある
+-会社を株式会社人間と株式会社馬に分割することにした
+-各サーバは木構造で接続されている
+-各サーバはそれぞれのどちらかに所属する
+-分割後、元々接続されていなかったサーバはケーブルが必要になる
+-馬は十分なケーブルを持っているが人間は持っていない
+-人間側がケーブルを必要とする分割はできない
+-有効な分割が何通りか求める
 
 */
 // END CUT HERE
 #include <string>
+#include <numeric>
 #include <vector>
 #include <iostream>
 #include <sstream>
@@ -122,11 +52,7 @@ public:
 	long long count(vector <int> a, vector <int> b) {
 		_a = a, _b = b;
 		dfs(-1, 1);
-		LL ans = 1;
-		for (int i = 0; i <= (int)a.size(); ++i) {
-			ans += dp[i];
-		}
-		return ans;
+		return accumulate(dp, dp + a.size() + 1, 0LL) + 1;
 	}
 
 
--- srm_570/RobotHerb.cpp
+++ srm_570/RobotHerb.cpp
@@ -1,88 +1,12 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 // SRM 570 Div1 Easy (250)
 
-PROBLEM STATEMENT
-Robot Herb is playing on an infinite square grid.
-At any moment, Herb stands on one of the squares and he faces one of the four cardinal directions.
-In his memory, Herb has a program.
-The program is a sequence of commands.
-For each i, the i-th of these commands has the following form:
-
-First move forward a[i] tiles.
-Then turn 90 degrees to the right, a[i] times in a row.
-
-Herb has decided to run the program T times.
-You are given the int T and the vector <int> a that describes Herb's program.
-Let A be the initial position of Herb and B be his position after the program was executed T times.
-Return the Manhattan distance between tiles A and B.
-
-
-DEFINITION
-Class:RobotHerb
-Method:getdist
-Parameters:int, vector <int>
-Returns:long long
-Method signature:long long getdist(int T, vector <int> a)
-
-
-NOTES
--Let's introduce a Cartesian coordinate system on the grid so that each cardinal direction is parallel to one of the axes.
- The Manhattan distance between two tiles with centers at points (x1, y1) and (x2, y2) is defined as |x1-x2| + |y1-y2|.
-
-
-CONSTRAINTS
--T will be between 1 and 1,000,000,000, inclusive.
--a will contain between 1 and 50 elements, inclusive.
--Each element of a will be between 1 and 10,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-1
-{1,2,3}
-
-Returns: 2
-
-Suppose that initially Herb stands on the tile with center at (0, 0) and faces the positive y direction. The program will get executed as follows:
-
-                       tile         direction
-After 1st command:     (0, 1)       positive x
-After 2nd command:     (2, 1)       negative x
-After 3rd command:     (-1, 1)      negative y
-
-The manhattan distance between the initial and the final positions is |-1| + |1| = 2.
-
-
-1)
-100
-{1}
-
-Returns: 0
-
-
-2)
-5
-{1,1,2}
-
-Returns: 10
-
-
-3)
-1000000000
-{100}
-
-Returns: 100000000000
-
-The answer doesn't fit into a 32-bit integer data type.
-
-
-4)
-570
-{2013,2,13,314,271,1414,1732}
-
-Returns: 4112
+問題
+-ロボットがXY平面上を移動する
+-命令が数値の配列で与えられる
+-各命令では数値nだけ直進し、n回90度右回転する
+-命令列全体をT回実行後、スタート地点からのマンハッタン距離を求める
 
 */
 // END CUT HERE
