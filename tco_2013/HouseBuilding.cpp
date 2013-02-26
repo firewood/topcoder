03eef2e835d79796e4342cb81275d6240904a76e
firewood <karamaki@gmail.com>
2013-02-27 00:53:01 +0900

    translate

--- tco_2013/HouseBuilding.cpp
+++ tco_2013/HouseBuilding.cpp
@@ -1,75 +1,11 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 TCO13 Round 1A Easy (250)
 
-PROBLEM STATEMENT
-Manao is building a new house. He already purchased a rectangular area where he will place the house.
-The basement of the house should be built on a level ground, so Manao will have to level the entire area.
-The area is leveled if the difference between the heights of its lowest and highest square meter is at most 1.
-Manao wants to measure the effort he needs to put into ground leveling.
-
-You are given a vector <string> area.
-Each character in area denotes the height at the corresponding square meter of Manao's area.
-Using 1 unit of effort, Manao can change the height of any square meter on his area by 1 up or down.
-Return the minimum total effort he needs to put to obtain a leveled area.
-
-
-DEFINITION
-Class:HouseBuilding
-Method:getMinimum
-Parameters:vector <string>
-Returns:int
-Method signature:int getMinimum(vector <string> area)
-
-
-CONSTRAINTS
--area will contain between 1 and 50 elements, inclusive.
--Each element of area will be between 1 and 50 characters long, inclusive.
--All elements of area will be of the same length.
--Each element of area will contain digits ('0'-'9') only.
-
-
-EXAMPLES
-
-0)
-{"10",
- "31"}
-
-Returns: 2
-
-The given area is not leveled, because the minimum height is 0 and the maximum height is 3. Manao needs to reduce the height of lower left square by 2.
-
-
-1)
-{"54454",
- "61551"}
-
-Returns: 7
-
-In the optimal solution each square will have height either 4 or 5. To reach such a configuration, Manao should reduce the height of one square from 6 to 5, and increase the heights of two other squares from 1 to 4.
-
-
-2)
-{"989"}
-
-Returns: 0
-
-The area is already leveled.
-
-
-3)
-{"90"}
-
-Returns: 8
-
-
-4)
-{"5781252",
- "2471255",
- "0000291",
- "1212489"}
-
-Returns: 53
+問題
+-家を建てるために土地を買った
+-土地はでこぼこで、高さの配列が与えられる
+-全体の高低差を1以下にするためのコストを求める
 
 */
 // END CUT HERE
--- tco_2013/TheFrog.cpp
+++ tco_2013/TheFrog.cpp
@@ -1,92 +1,11 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 TCO13 Round 1A Medium (500)
 
-// PROBLEM STATEMENT
-// Frog Jim is fond of jumping.
-Moreover, all his jumps have the same length X.
-Currently, he is standing at the point 0 on the real axis.
-He needs to jump along the axis until he gets to point D or beyond it.
-
-There are several pits between points 0 and D. 
-For each valid i, there is a pit strictly between L[i] and R[i].
-This means that Jim cannot step onto any point that is strictly between points L[i] and R[i].
-(He is allowed to step onto the points L[i] and R[i]: as all pits are disjoint, the endpoints of each pit are always safe.)
-
-Find the minimum length of jump X such that if Jim performs only jumps of length X, he can avoid every pit and get to point D or any point beyond it. Note that this length can be non-integer.
-
-DEFINITION
-Class:TheFrog
-Method:getMinimum
-Parameters:int, vector <int>, vector <int>
-Returns:double
-Method signature:double getMinimum(int D, vector <int> L, vector <int> R)
-
-
-NOTES
--The returned value must have an absolute or relative error less than 1e-9.
-
-
-CONSTRAINTS
--D will be between 1 and 30,000, inclusive.
--L will contain between 1 and 50 elements, inclusive.
--Each element of L will be between 0 and D-1, inclusive.
--R will contain the same number of elements as L.
--The i-th element of R will be between L[i]+1 and D, inclusive.
--The intervals given by (L[i], R[i]) will not intersect.
-
-
-EXAMPLES
-
-0)
-16
-{2}
-{7}
-
-Returns: 7.0
-
-Moving by jumps of length 7, Jim goes from point 0 to point 7,
-then to point 14 and then to point 21, which is beyond 16.
-If Jim chose a shorter jump, he would end up in the pit.
-
-
-1)
-25
-{11, 3}
-{21, 7}
-
-Returns: 10.5
-
-There are two pits. One of them is between points 11 and 21 and the other between points 3 and 7.
-By fixing jump length at 10.5, Jim successfully avoids them.
-
-
-2)
-100
-{0}
-{100}
-
-Returns: 100.0
-
-Jim has no other choice but to jump right to point 100.
-
-
-3)
-100
-{0, 50}
-{50, 100}
-
-Returns: 50.0
-
-In this case, point 50 is a suitable place to land, so Jim can choose X = 50.
-
-
-4)
-30000
-{17, 25281, 5775, 2825, 14040}
-{55, 26000, 5791, 3150, 14092}
-
-Returns: 787.8787878787879
+問題
+-蛙が距離Xずつジャンプする
+-穴の開始と終了の座標が与えられる
+-穴を全て飛び越えるための最小のXを求める
 
 */
 // END CUT HERE
