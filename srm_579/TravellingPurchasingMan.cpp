0b087936a27bec9d30532b3bb7282dd43ac8074e
firewood <karamaki@gmail.com>
2013-06-03 02:12:13 +0900

    translate

--- srm_579/TravellingPurchasingMan.cpp
+++ srm_579/TravellingPurchasingMan.cpp
@@ -1,76 +1,14 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 SRM 579 Div1 Medium (450)
 
-PROBLEM STATEMENT
-You are interested in purchasing items from a number of stores in a local market. The market is composed of N stores numbered from 0 to N-1. The stores with numbers from 0 to M-1 are interesting to you and all the other stores are not interesting. Some pairs of stores are connected by roads.
-
-You are given a vector <string> interestingStores which contains M elements and describes the interesting stores. The i-th element corresponds to store i and is formatted "OPEN CLOSE DURATION" (quotes for clarity), where OPEN is the opening time (in seconds), CLOSE is the closing time (in seconds) and DURATION is the time (in seconds) required to make a purchase in this store. You can initiate a purchase from a store at any time T between OPEN and CLOSE, inclusive. In order to do so, you need to arrive to the store at time T (or earlier). The purchase will be finalized at time T + DURATION and you need to stay at the store for the entire duration of your purchase. Note that it is possible for a purchase to end when the store is already closed. You cannot make multiple purchases in the same store.
-
-The roads are given by the vector <string> roads. Each element of roads describes a single bidirectional road and is formatted "A B LENGTH" (quotes for clarity). Here A and B are the numbers of stores connected by the road and LENGTH is the time (in seconds) required to move from A to B (or from B to A) using this road.
-
-Your start at time 0 at the location of store N-1. Return the maximum number of purchases in interesting stores that you can make.
-
-DEFINITION
-Class:TravellingPurchasingMan
-Method:maxStores
-Parameters:int, vector <string>, vector <string>
-Returns:int
-Method signature:int maxStores(int N, vector <string> interestingStores, vector <string> roads)
-
-
-NOTES
--You are allowed to wait for any amount of time at any location.
-
-
-CONSTRAINTS
--N will be between 1 and 50, inclusive.
--roads will contain between 1 and 50 elements, inclusive.
--Each element of roads will be formatted "A B LENGTH" (quotes for clarity), where A, B and LENGTH are integers with no unnecessary leading zeros.
--In each road, A and B will each be between 0 and N-1, inclusive.
--In each road, A and B will be distinct.
--In each road, LENGTH will be between 1 and 604,800, inclusive.
--There will exist at most one road between each pair of stores.
--interestingStores will contain between 1 and min{16, N} elements, inclusive,
--Each element of interestingStores will be formatted "OPEN CLOSE DURATION" (quotes for clarity), where OPEN, CLOSE and DURATION are integers with no unnecessary leading zeros.
--In each store, OPEN will be between 0 and 604,800, inclusive.
--In each store, CLOSE will be between OPEN+1 and 604,800, inclusive.
--In each store, DURATION will be between 1 and 604,800, inclusive.
-
-
-EXAMPLES
-
-0)
-3
-{"1 10 10" , "1 55 31", "10 50 100" }
-{"1 2 10"}
-
-Returns: 1
-
-It is not possible to make more than one purchase:
-If you decide to make the purchase at store 2: You need to wait 10 seconds until it opens. Then wait until time = 110 seconds for the purchase to finish. At 110 seconds, all the other stores will be closed.
-If you instead decide to make the purchase at store 1: You first need travel through the road and arrive store 1 at time = 10. The purchase finishes at time = 41. After you travel back to store 2, the time will be 51 seconds and store 2 will be closed.
-There is no way to reach store 0 when store 2 is the starting point.
-
-
-1)
-3
-{"1 10 10" , "1 55 30", "10 50 100" }
-{"1 2 10"}
-
-Returns: 2
-
-This time we can travel to store 1, make the purchase and return to store 2 exactly at time = 50 to make two purchases in total.
-
-
-2)
-5
-{"0 1000 17"}
-{"2 3 400", "4 1 500", "4 3 300", "1 0 700", "0 2 400"}
-
-Returns: 0
-
-It is not possible to reach store 0 before it closes.
+問題
+-店がN個、興味のある店がM個ある
+-それぞれの店の開店時間、閉店時間、買い物にかかる時間が与えられる
+-店Aと店Bが道路で結ばれている場合、かかる時間が与えられる
+-同じ店では1度しか買い物できない
+-閉店時間ちょうどに入店してもよい
+-店(N-1)からスタートして、買い物ができる最大数を求める
 
 */
 // END CUT HERE
--- srm_579/UndoHistory.cpp
+++ srm_579/UndoHistory.cpp
@@ -1,92 +1,13 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 SRM 579 Div1 Easy (250)
 
-// PROBLEM STATEMENT
-// Bob is using a peculiar text editor to write a sequence of lines of text.
-The editor consists of three parts: a results window, a text buffer and an undo history.
-More details about the three parts follow.
-
-The results window contains a sequence of strings: the lines of text you already wrote. Initially, the results window is empty.
-The text buffer contains a string: the line you are writing at the moment. Initially, the string in the text buffer is empty.
-The undo history contains a sequence of strings: all the past states of the text buffer. Initially, the undo history contains a single element: an empty string.
-
-
-You are given a vector <string> lines.
-Bob would like to print the contents of lines into the results window.
-(At the end, the sequence of strings stored in the results window must be precisely equal to lines. Order of elements matters.)
-Additionally, Bob would like to do so as quickly as possible.
-He is able to take the following actions:
-
-
-Bob may type a lowercase letter. The letter is appended to the text buffer. The new text buffer is then added as a new element of the undo history. (For example, if the text buffer currently contains "do", then pressing 'g' changes the text buffer to "dog" and then stores "dog" into the undo history.)
-Bob may press Enter. When he does so, the current content of the text buffer is printed to the results window as a new line, after the lines that were printed earlier. The text buffer remains unmodified. (For example, if the text buffer contains "dog" and Bob presses Enter, "dog" will be appended to the results window, and the results buffer still contains "dog".)
-Bob may use two mouse clicks to restore any entry from the undo history to the text buffer. This operation does not modify the undo history.
-
-
-Return the minimum total number of button presses (keyboard and mouse) that Bob needs to print all the given lines into the results window.
-
-
-DEFINITION
-Class:UndoHistory
-Method:minPresses
-Parameters:vector <string>
-Returns:int
-Method signature:int minPresses(vector <string> lines)
-
-
-CONSTRAINTS
--lines will contain between 1 and 50 elements, inclusive.
--Each element of lines will contain between 1 and 50 characters, inclusive.
--Each element of lines will contain only lowercase letters ('a'-'z').
-
-
-EXAMPLES
-
-0)
-{"tomorrow", "topcoder"}
-
-Returns: 18
-
-
-Type 't'. The text buffer now contains "t", and the undo history now contains "" and "t".
-Type 'o'. The text buffer now contains "to", and the undo history now contains "", "t", and "to".
-Using six more keypresses, type the letters in "morrow". The text buffer now contains "tomorrow" and the undo history contains all prefixes of "tomorrow". The results window is still empty.
-Press Enter. The results window now contains one string: "tomorrow".
-Click the mouse twice to restore "to" from undo history.
-Using another six keypresses, type the letters in "pcoder".
-Press Enter. The results window now contains "tomorrow" and "topcoder", in this order, and we are done.
-
-The total number of button presses was 8 (typing "tomorrow") + 1 (Enter) + 2 (mouse) + 6 (typing "pcoder") + 1 (Enter) = 18.
-
-
-1)
-{"a","b"}
-
-Returns: 6
-
-After typing "a" and pressing enter, we need to restore the empty string (which is always present at the top of the undo buffer) before typing "b".
-
-
-2)
-{"a", "ab", "abac", "abacus" }
-
-Returns: 10
-
-There are times when it is not necessary to use the undo history at all.
-
-
-3)
-{"pyramid", "sphinx", "sphere", "python", "serpent"}
-
-Returns: 39
-
-
-4)
-{"ba","a","a","b","ba"}
-
-
-Returns: 13
+問題
+-historyバッファを持つエディタがある
+-現在行の状態は常にhistoryバッファに履歴として残る
+-任意のhistoryバッファから2クリックで現在行にコピーすることができる
+-入力すべき行が与えられる
+-キーストロークまたはクリックの合計の最小値を求める
 
 */
 // END CUT HERE
