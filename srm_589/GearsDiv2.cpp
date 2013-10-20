dd3b169c4a229c8c2d3217a874eb81bf4526731f
firewood <karamaki@gmail.com>
2013-10-21 07:23:46 +0900

    translate

--- srm_589/GearsDiv2.cpp
+++ srm_589/GearsDiv2.cpp
@@ -2,73 +2,11 @@
 /*
 SRM 589 Div2 Medium (500)
 
-// PROBLEM STATEMENT
-// Goose Tattarrattat has a machine that contains N gears (cogwheels).
-The gears are numbered 0 through N-1.
-Currently, the gears are arranged into a cycle: for each i, gear i meshes with gears i-1 and i+1 (counting modulo N).
-In particular, gear 0 meshes with gear N-1.
-
-Let X and Y be two meshing gears.
-Note that if X is turning, Y must clearly be turning in the opposite direction (clockwise vs. counter-clockwise).
-
-For each of the N gears we have a desired direction of turning.
-You are given this information encoded as a string Directions.
-Character i of Directions is 'R' if we want gear i to turn to the right (clockwise), and it is 'L' if we want it to turn to the left.
-
-Of course, it may be impossible to satisfy all the desired directions of turning.
-Return the minimal number of gears that have to be removed from the machine so that all remaining gears can turn in the desired directions at the same time.
-
-DEFINITION
-Class:GearsDiv2
-Method:getmin
-Parameters:string
-Returns:int
-Method signature:int getmin(string Directions)
-
-
-NOTES
--Removing a gear creates a gap between the other gears. For example, after removing the gear 7, gears 6 and 8 will not mesh.
-
-
-CONSTRAINTS
--Directions will contain between 3 and 50 characters, inclusive.
--Each character of Directions will be 'R' or 'L'.
-
-
-EXAMPLES
-
-0)
-"LRRR"
-
-Returns: 1
-
-Once we remove gear 2, the other three are free to turn in the desired directions.
-
-
-1)
-"RRR"
-
-Returns: 2
-
-Gear 0 meshes with gear 2. 
-
-
-2)
-"LRLR"
-
-Returns: 0
-
-
-3)
-"LRLLRRLLLRRRLLLL"
-
-Returns: 6
-
-
-4)
-"RRRRRRRLRRRRRRRLRLRLLRLRLRLRRLRLRLLLRLRLLRLLRRLRRR"
-
-Returns: 14
+問題
+-N個のギアがある
+-最後のギアと最初のギアはつながっている
+-ギアの向きが与えられる
+-全てのギアがまわるようにするために取り除かなければならない個数を求める
 
 */
 // END CUT HERE
--- srm_589/GooseTattarrattatDiv2.cpp
+++ srm_589/GooseTattarrattatDiv2.cpp
@@ -2,77 +2,10 @@
 /*
 SRM 589 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// Goose Tattarrattat has a string S of lowercase letters.
-A string is called smooth if all its letters are the same.
-Tattarrattat wants to change her string into a smooth one.
-
-She will do this in a series of steps.
-In each step, Tattarrattat will choose two letters of the alphabet: X and Y.
-She will then change each X in her string into an Y.
-Changing each single character takes 1 second.
-
-For example, if S="goose" and Tattarrattat chooses X='o' and Y='e' in the next step, the step will take 2 seconds (because there are two 'o's in S) and after the step she would have S="geese".
-
-You are given the string S.
-Return the smallest number of seconds in which Tattarrattat can change S into a smooth string.
-
-DEFINITION
-Class:GooseTattarrattatDiv2
-Method:getmin
-Parameters:string
-Returns:int
-Method signature:int getmin(string S)
-
-
-CONSTRAINTS
--S will contain between 1 and 50 characters, inclusive.
--Each character in S will be a lowercase letter ('a'-'z').
-
-
-EXAMPLES
-
-0)
-"geese"
-
-Returns: 2
-
-There are many ways how Tattarrattat can change this S into a smooth string.
-For example, she could do it in two steps as follows:
-
-Change all 'g's to 'e's: this takes 1 second and produces the string "eeese".
-Change all 'e's to 's's: this takes 4 seconds and produces the string "sssss".
-
-This way took her 1+4 = 5 seconds.
-However, there are faster ways.
-The best one only takes 2 seconds.
-For example, she can first change all 'g's to 'e's (1 second), and then change all 's's to 'e's (1 second), obtaining the smooth string "eeeee".
-
-
-1)
-"tattarrattat"
-
-Returns: 6
-
-
-2)
-"www"
-
-Returns: 0
-
-This string is already smooth so no changes are needed.
-
-
-3)
-"topcoder"
-
-Returns: 6
-
-
-4)
-"xrepayuyubctwtykrauccnquqfuqvccuaakylwlcjuyhyammag"
-
-Returns: 43
+問題
+-文字列が与えられる
+-1文字変更するのに1秒かかる
+-全ての文字を同じにするのにかかる時間を求める
 
 */
 // END CUT HERE
