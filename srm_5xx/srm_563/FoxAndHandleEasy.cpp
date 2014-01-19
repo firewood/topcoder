eab508f84ca6d58aaa0f719a7bb0cf2999fbe6ea
firewood <karamaki@gmail.com>
2014-01-19 22:04:36 +0900

    translate

--- srm_5xx/srm_563/FoxAndHandleEasy.cpp
+++ srm_5xx/srm_563/FoxAndHandleEasy.cpp
@@ -2,85 +2,9 @@
 /*
 // SRM 563 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// Fox Ciel has a new favorite string operation that works as follows:
-She takes two copies of some string X, and inserts one copy somewhere into the other copy of the same string.
-(She can also insert it at the beginning or, equivalently, at the end.)
-This operation is called expansion of the string X.
-For example, if X = "Ciel", she can expand it to "CielCiel", "CCieliel", "CiCielel", or "CieCiell".
-
-
-You are given two strings: S and T.
-Return "Yes" (quotes for clarity) if T can be obtained by expanding S exactly once.
-Otherwise, return "No".
-
-DEFINITION
-Class:FoxAndHandleEasy
-Method:isPossible
-Parameters:string, string
-Returns:string
-Method signature:string isPossible(string S, string T)
-
-
-CONSTRAINTS
--S will contain between 1 and 50 characters, inclusive.
--T will contain between 1 and 50 characters, inclusive.
--Both S and T will contain uppercase and lowercase letters only ('A'-'Z' and 'a'-'z').
-
-
-EXAMPLES
-
-0)
-"Ciel"
-"CieCiell"
-
-Returns: "Yes"
-
-She can insert "Ciel" between "Cie" and "l".
-
-
-1)
-"Ciel"
-"FoxCiel"
-
-Returns: "No"
-
-Each string obtained by expanding S="Ciel" has exactly 8 letters.
-
-
-2)
-"FoxCiel"
-"FoxFoxCielCiel"
-
-Returns: "Yes"
-
-
-3)
-"FoxCiel"
-"FoxCielCielFox"
-
-Returns: "No"
-
-
-4)
-"Ha"
-"HaHaHaHa"
-
-Returns: "No"
-
-
-5)
-"TheHandleCanBeVeryLong"
-"TheHandleCanBeVeryLong"
-
-Returns: "No"
-
-
-6)
-"Long"
-"LongLong"
-
-Returns: "Yes"
+問題
+-文字列の途中に同じ文字列を挿入する操作を拡張とする
+-与えられた文字列が拡張操作によるものかどうかを求める
 
 */
 // END CUT HERE
