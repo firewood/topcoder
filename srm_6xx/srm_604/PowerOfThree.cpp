ff4d0af639f37184ce6cd24cad1a8c6b186669df
firewood <karamaki@gmail.com>
2014-04-06 23:06:40 +0900

    translate

--- srm_6xx/srm_604/PowerOfThree.cpp
+++ srm_6xx/srm_604/PowerOfThree.cpp
@@ -2,97 +2,10 @@
 /*
 SRM 604 Div1 Easy (250)
 
-PROBLEM STATEMENT
-Fox Ciel has a robot.
-The robot is located on an infinite plane.
-At the beginning, the robot starts at the coordinates (0, 0).
-The robot can then make several steps.
-The steps are numbered starting from 0.
-
-In each step, Ciel must choose one of four directions for the robot: left (x coordinate decreases), right (x coordinate increases), up (y coordinate increases), or down (y coordinate decreases).
-In step k, the robot will move 3^k units in the chosen direction.
-It is not allowed to skip a step.
-
-You are given ints x and y.
-Return "Possible" (quotes for clarity) if it is possible that the robot reaches the point (x,y) after some (possibly zero) steps.
-Otherwise, return "Impossible".
-
-
-DEFINITION
-Class:PowerOfThree
-Method:ableToGet
-Parameters:int, int
-Returns:string
-Method signature:string ableToGet(int x, int y)
-
-
-CONSTRAINTS
--x will be between -1,000,000,000 and 1,000,000,000, inclusive.
--y will be between -1,000,000,000 and 1,000,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-1
-3
-
-Returns: "Possible"
-
-In step 0 Ciel will send the robot right to (1,0). In step 1 she will send it up to (1,3).
-
-
-1)
-0
-2
-
-Returns: "Possible"
-
-The robot will move from (0,0) down to (0,-1) and then up to (0,2).
-
-
-2)
-1
-9
-
-Returns: "Impossible"
-
-Note that it is not allowed to move the robot right in step 0, skip step 1, and then move the robot up in step 2.
-
-
-3)
-3
-0
-
-Returns: "Impossible"
-
-
-4)
-1
-1
-
-Returns: "Impossible"
-
-
-5)
--6890
-18252
-
-Returns: "Possible"
-
-
-6)
-1000000000
--1000000000
-
-Returns: "Impossible"
-
-
-7)
-0
-0
-
-Returns: "Possible"
+問題
+-4方向の移動を受け付けるロボットがいる
+-ターンkに距離3^k移動できる
+-与えられた座標に移動可能かどうかを求める
 
 */
 // END CUT HERE
@@ -109,12 +22,11 @@ typedef long long LL;
 class PowerOfThree {
 
 	bool check(int x, int y) {
-		x = abs(x), y = abs(y);
 		while (x != 0 || y != 0) {
 			int a = x % 3, b = y % 3;
-			if (((a == 1 || a == 2) && b == 0) || (a == 0 && (b == 1 || b == 2))) {
-				x = (x + 1) / 3;
-				y = (y + 1) / 3;
+			if ((a != 0 && b == 0) || (a == 0 && b != 0)) {
+				x = (x + a) / 3;
+				y = (y + b) / 3;
 			} else {
 				return false;
 			}
