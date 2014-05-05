80e6026d6e8264c33c673ebf59f0b1860d38c54a
firewood <karamaki@gmail.com>
2014-05-06 00:37:34 +0900

    translate

--- srm_6xx/srm_613/TaroFriends.cpp
+++ srm_6xx/srm_613/TaroFriends.cpp
@@ -2,77 +2,11 @@
 /*
 SRM 613 Div1 Easy (250)
 
-PROBLEM STATEMENT
-
-Cat Taro likes to play with his cat friends.
-Each of his friends currently sits on some coordinate on a straight line that goes from the left to the right.
-When Taro gives a signal, each of his friends must move exactly X units to the left or to the right.
-
-You are given an vector <int> coordinates and the int X.
-For each i, the element coordinates[i] represents the coordinate of the i-th cat before the movement.
-Return the smallest possible difference between the positions of the leftmost cat and the rightmost cat after the movement.
-
-
-DEFINITION
-Class:TaroFriends
-Method:getNumber
-Parameters:vector <int>, int
-Returns:int
-Method signature:int getNumber(vector <int> coordinates, int X)
-
-
-CONSTRAINTS
--coordinates will contain between 1 and 50 elements, inclusive.
--Each element of coordinates will be between -100,000,000 and 100,000,000, inclusive.
--X will be between 0 and 100,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{-3, 0, 1}
-3
-
-Returns: 3
-
-The difference 3 is obtained if the cats move from {-3,0,1} to {0,-3,-2}.
-
-
-1)
-{4, 7, -7}
-5
-
-Returns: 4
-
-The difference 4 is obtained if the cats move from {4,7,-7} to {-1,2,-2}.
-
-
-2)
-{-100000000, 100000000}
-100000000
-
-Returns: 0
-
-
-3)
-{3, 7, 4, 6, -10, 7, 10, 9, -5}
-7
-
-Returns: 7
-
-
-4)
-{-4, 0, 4, 0}
-4
-
-Returns: 4
-
-
-5)
-{7}
-0
-
-Returns: 0
+問題
+-猫がN匹いる
+-それぞれの猫の位置が一次元の座標で与えられる
+-太郎が合図をしたら、全ての猫が右か左にXだけ動く
+-最も左と最も右の猫の距離の最小値を求める
 
 */
 // END CUT HERE
