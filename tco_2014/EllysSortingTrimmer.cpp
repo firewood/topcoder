cf4c03695512438bb0ec2ebc2f3e7e2dbb3dc30f
firewood <karamaki@gmail.com>
2014-05-15 00:36:48 +0900

    translate

--- tco_2014/EllysSortingTrimmer.cpp
+++ tco_2014/EllysSortingTrimmer.cpp
@@ -2,105 +2,11 @@
 /*
 TCO 2014 Round 1A Easy (250)
 
-PROBLEM STATEMENT
-Elly has a string S of uppercase letters and a magic device that can modify the string. The strength of the device is an int L.
-
-The device is used in the following way. The user enters a 0-based index i such that 0 <= i <= length(S)-L. The device then performs the following changes:
-
-It leaves the first i characters (i.e., characters with indices 0 through i-1) untouched.
-It rearranges the next L characters (i.e., characters with indices i through i+L-1) into alphabetical order.
-It erases all the remaining characters (i.e., characters with indices i+L and more). Note that for i=length(S)-L no characters are erased.
-
-The girl can use this "sorting trimmer" as many times as she likes. After each use she is left with the new version of the string.
-
-In the examples below we use brackets to highlight the region that shall be sorted. For example, "ABRA[CADAB]RA" means that L=5 and Elly chose i=4. The device keeps the letters in front of the brackets, sorts the letters in the brackets, and throws away the rest.
-
-Here is one way how Elly could have used a device with L = 5, starting with the string S = "ABRACADABRA":
-
-"ABRAC[ADABR]A" -> "ABRACAABDR"
-"ABR[ACAAB]DR" -> "ABRAAABC"
-"A[BRAAA]BC" -> "AAAABR"
-
-
-You are given the string S and the int L. Return the lexicographically smallest string Elly can obtain.
-
-DEFINITION
-Class:EllysSortingTrimmer
-Method:getMin
-Parameters:string, int
-Returns:string
-Method signature:string getMin(string S, int L)
-
-
-NOTES
--A string A is lexicographically smaller than string B if A contains a smaller character in the first position where they differ. In case one of the strings ends before they have a different character, the shorter one is considered smaller.
-
-CONSTRAINTS
--S will contain between 2 and 50 characters, inclusive.
--L will be between 2 and |S|, inclusive, where |S| denotes the number of characters in S.
--S will consist of uppercase characters of the English alphabet, only ('A'-'Z').
-
-
-EXAMPLES
-
-0)
-"ABRACADABRA"
-5
-
-Returns: "AAAAA"
-
-Please note that the example in the problem statement does not obtain the lexicographically smallest string. In fact, it is optimal to start by using the device on the last five characters of the string, transforming it from ABRACA[DABRA] to ABRACAAABDR.
-
-
-1)
-"ESPRIT"
-3
-
-Returns: "EIP"
-
-We can obtain the answer in the following steps:
-
-ES[PRI]T -> ESIPR
-E[SIP]R -> EIPS
-[EIP]S -> EIP
- 
-
-2)
-"BAZINGA"
-7
-
-Returns: "AABGINZ"
-
-We can use the sorting trimmer on the entire word.
-
-
-3)
-"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-13
-
-Returns: "ABCDEFGHIJKLM"
-
-Even though the string is already sorted, shorter strings are considered lexicographically smaller, so we can use the device once to make the string as short as possible.
-
-
-4)
-"GOODLUCKANDHAVEFUN"
-10
-
-Returns: "AACDDEFGHK"
-
-
-5)
-"AAAWDIUAOIWDESBEAIWODJAWDBPOAWDUISAWDOOPAWD"
-21
-
-Returns: "AAAAAAAAABBDDDDDDDEEI"
-
-6)
-"TOPCODER"
-3
-
-Returns: "CDT"
+問題
+-文字列を変更する装置がある
+-先頭のN文字をそのまま、途中のL文字をソートして、末尾を削除できる
+-何回でも適用できる
+-適用結果のうちの辞書順最小のものを求める
 
 */
 // END CUT HERE
