541a8ffc7940236d27225e48992ce8cce077d3b0
firewood <karamaki@gmail.com>
2014-06-18 00:52:08 +0900

    translate

--- srm_5xx/srm_562/CucumberMarket.cpp
+++ srm_5xx/srm_562/CucumberMarket.cpp
@@ -1,88 +1,11 @@
 // BEGIN CUT HERE
 /*
-// SRM 562 Div2 Easy (250)
+SRM 562 Div2 Easy (250)
 
-// PROBLEM STATEMENT
-// 
-Cucumber Boy is young and loves cucumbers.  Therefore, Cucumber Boy will go to the cucumber market to buy some cucumbers.
-
-Different cucumbers may have different costs.  For each i, buying the i-th cucumber (0-based index) costs price[i] yen.
-
-Cucumber Boy's mother gave him budget yen.  However, he does not understand money well.
-He just chooses some k unique cucumbers he likes.  If the total price of the chosen cucumbers is not greater than budget yen, he can buy them, otherwise he cannot.
-
-You are given the vector <int> price, the int budget and the int k.  Your method must return "YES" (quotes for clarity) if Cucumber Boy can buy any set of k unique cucumbers, and "NO" if there is some set of k cucumbers that is too expensive for him.
-
-
-DEFINITION
-Class:CucumberMarket
-Method:check
-Parameters:vector <int>, int, int
-Returns:string
-Method signature:string check(vector <int> price, int budget, int k)
-
-
-CONSTRAINTS
--price will contain between 1 and 9 elements, inclusive.
--Each element of price will be between 1 and 1000, inclusive.
--budget will be between 1 and 10000, inclusive.
--k will be between 1 and the number of elements in price, inclusive.
-
-
-EXAMPLES
-
-0)
-{1000,1,10,100}
-1110
-3
-
-Returns: "YES"
-
-There are 4 choices.
-
-If he chooses cucumbers 0, 1, and 2, the total price will be 1000+1+10 = 1011 yen.
-If he chooses cucumbers 0, 1, and 3, the total price will be 1000+1+100 = 1101 yen.
-If he chooses cucumbers 0, 2, and 3, the total price will be 1000+10+100 = 1110 yen.
-If he chooses cucumbers 1, 2, and 3, the total price will be 1+10+100 = 111 yen.
-
-The result is "YES", because Cucumber Boy can buy any set of k unique cucumbers.
-
-
-1)
-{1000,1,10,100}
-1109
-3
-
-Returns: "NO"
-
-This case is the same as the example 0, except for the value of budget. 
-
-If he chooses cucumbers 0, 2, and 3, the total price will be 1000+10+100 = 1110 yen.
- 1110 yen is greater than the budget therefore the result is "NO".
-
-
-2)
-{33,4}
-33
-1
-
-Returns: "YES"
-
-
-3)
-{1,1,1,1,1,1}
-2
-4
-
-Returns: "NO"
-
-
-4)
-{1000,1000,1000,1000,1000,1000,1000,1000,1000}
-10000
-9
-
-Returns: "YES"
+問題
+-きゅうりが大好き
+-ランダムでK本買う
+-どういう選び方でも買えるかどうか求める
 
 */
 // END CUT HERE
--- srm_5xx/srm_564/FauxPalindromes.cpp
+++ srm_5xx/srm_564/FauxPalindromes.cpp
@@ -2,88 +2,9 @@
 /*
 SRM 564 Div2 Easy (250)
 
-PROBLEM STATEMENT
-A word is a palindrome if it can be read the same forwards and backwards.
-For example, the strings "ANA", "AAXAA", "Z" and "XYYYYYX" are palindromes (quotes for clarity).
-
-A word is a faux palindrome if, after replacing every group of consecutive equal letters
-with a single instance of the letter, the resulting word is a palindrome.
-For example, the string "AAAAANNAA" is a faux palindrome.
-A detailed explanation why this is a faux palindrome is given below in Example 1.
-Other examples of faux palindromes are the strings "AAAAAAAAAAAAAAAXA and "XYX".
-Note that every palindrome is also a faux palindrome.
-
-You are given a string word. You are asked to classify it.
-If word is a palindrome, return "PALINDROME" (quotes for clarity, returned values are case sensitive).
-If word is not a palindrome but is a faux palindrome, return "FAUX".
-In case word is not a faux palindrome, return "NOT EVEN FAUX".
-
-
-DEFINITION
-Class:FauxPalindromes
-Method:classifyIt
-Parameters:string
-Returns:string
-Method signature:string classifyIt(string word)
-
-
-CONSTRAINTS
--word will contain between 1 and 50 characters, inclusive.
--The characters in word will be upper case letters between 'A' and 'Z', inclusive (quotes for clarity).
-
-
-EXAMPLES
-
-0)
-"ANA"
-
-Returns: "PALINDROME"
-
-"ANA" reads the same from left to right and right to left.
-
-
-1)
-"AAAAANNAA"
-
-Returns: "FAUX"
-
-This is obviously not a palindrome. Now suppose that we replace each group of consecutive equal letters by a single copy of that letter. That is, we change "AAAAA" to "A", "NN" to "N", and "AA" to "A". In this way we will obtain the new string "ANA", which is a palindrome. Hence the original string is a faux palindrome.
-
-
-2)
-"LEGENDARY"
-
-Returns: "NOT EVEN FAUX"
-
-The word "LEGENDARY" already does not contain any group of consecutive equal letters and it is not a palindrome.
-
-
-3)
-"XXXYTYYTTYX"
-
-Returns: "FAUX"
-
-After replacing the groups of consecutive equal letters with a single letter, the string becomes "XYTYTYX". "XYTYTYX" is a palindrome.
-
-
-4)
-"TOPCOODEREDOOCPOT"
-
-Returns: "PALINDROME"
-
-This word contains some groups of consecutive equal letters, but it is already a palindrome without replacing them by single letters.
-
-
-5)
-"TOPCODEREDOOCPOT"
-
-Returns: "FAUX"
-
-
-6)
-"XXXXYYYYYZZXXYYY"
-
-Returns: "NOT EVEN FAUX"
+問題
+-連続する文字を1文字に置き換えた結果、左右対称ならFAUXとする
+-文字列が左右対称なのか、FAUXなのか、そうでないのかを求める
 
 */
 // END CUT HERE
