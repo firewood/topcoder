ebad592b0750de1f7160ba190e3456066563e02d
firewood <karamaki@gmail.com>
2014-07-06 16:41:54 +0900

    translate

--- tco_2014/SubstringReversal.cpp
+++ tco_2014/SubstringReversal.cpp
@@ -2,73 +2,10 @@
 /*
 TCO 2014 Round 2C Easy (300)
 
-PROBLEM STATEMENT
-
-You are given a string S. The string can have up to 2500 characters.
-You have to reverse exactly one substring of S. Formally, you have to choose two 0-based indices {x,y} such that x <= y, and then you have to reverse the order of the letters with indices x through y, inclusive. That is, S[x]S[x+1]...S[y] becomes S[y]...S[x+1]S[x].
-
-For example, if S="abcdefg", you can choose the indices {2,5} to obtain "abfedcg", the indices {0,1} to obtain "bacdefg", or the indices {3,3} to obtain "abcdefg". (In the last example, only one letter was selected, so the string did not change.)
-
-Your goal is to produce the lexicographically smallest string possible. Return a vector <int> containing two elements: the optimal indices x and y.
-If there are multiple optimal choices, find the one with the smallest x.
-If there are still multiple optimal choices, find the one with the smallest y.
-
-
-DEFINITION
-Class:SubstringReversal
-Method:solve
-Parameters:string
-Returns:vector <int>
-Method signature:vector <int> solve(string S)
-
-
-NOTES
--Given two strings A and B of equal length, we say that A is lexicographically smaller than B if A has a smaller character than B at the first position where they differ.
-
-
-CONSTRAINTS
--S will contain between 1 and 2500 elements, inclusive.
--Each character of S will be a lowercase letter ('a'-'z').
-
-
-EXAMPLES
-
-0)
-"abdc"
-
-Returns: {2, 3 }
-
-Reverse "dc" to "cd".
-
-
-1)
-"aabbcc"
-
-Returns: {0, 0 }
-
-Nothing to reverse here, you cannot produce a lexicographically smaller string.
-
-
-2)
-"misof"
-
-Returns: {0, 4 }
-
-Reverse the entire string.
-
-
-3)
-"ivan"
-
-Returns: {0, 2 }
-
-Reverse "iva" to bring 'a' to the beginning.
-
-
-4)
-"thisseemstobeaneasyproblem"
-
-Returns: {0, 13 }
+問題
+-文字列Sが与えられる
+-位置iからjまでを逆転させる
+-辞書順最小になるiとjを求める
 
 */
 // END CUT HERE
