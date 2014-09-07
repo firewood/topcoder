cce78474e68657f1dd2b8d82e4f20d153f87f22d
firewood <karamaki@gmail.com>
2014-09-08 01:15:58 +0900

    translate

--- srm_6xx/srm_630/SuffixArrayDiv2.cpp
+++ srm_6xx/srm_630/SuffixArrayDiv2.cpp
@@ -2,70 +2,11 @@
 /*
 SRM 630 Div2 Hard (1000)
 
-PROBLEM STATEMENT
-Suffix number i of a string S is the suffix that starts with the character S[i].
-For example, for S="abcde" suffix 0 is "abcde" and suffix 3 is "de". 
-
-The suffix array of a string S is defined as the permutation of suffix numbers that corresponds to their lexicographic order.
-For example, suppose that S="abca".
-If we order all suffixes of S lexicographically, we get the following: "a" < "abca" < "bca" < "ca".
-The corresponding suffix numbers are 3, 0, 1, and 2, in this order.
-Thus, for this string S the suffix array would be {3, 0, 1, 2}.
-Note that the length of a suffix array is the same as the length of the original string.
-
-In this problem, we will only consider strings of lowercase English letters ('a'-'z').
-You are given a string s.
-We are interested in a string t that is lexicographically smaller than s but has exactly the same suffix array.
-Return "Exists" if at least one such string t exists, and "Does not exist" otherwise.
-
-
-DEFINITION
-Class:SuffixArrayDiv2
-Method:smallerOne
-Parameters:string
-Returns:string
-Method signature:string smallerOne(string s)
-
-
-NOTES
--Given two different strings A and B of the same length, A is lexicographically smaller than B if we have A[i] < B[i] for the smallest i such that A[i] and B[i] differ.
-
-
-CONSTRAINTS
--s will contain between 1 and 50 elements, inclusive.
--Each element in s will be a lowercase letter ('a'-'z').
-
-
-EXAMPLES
-
-0)
-"abca"
-
-Returns: "Exists"
-
-For example, "aaqa" < "abca" but their suffix arrays are the same.
-
-
-1)
-"bbbb"
-
-Returns: "Exists"
-
-Obviously, one of the strings smaller than "bbbb" with the same suffix array is "aaaa".
-
-
-2)
-"aaaa"
-
-Returns: "Does not exist"
-
-The string "aaaa" is already the lexicographically smallest 4-letter string.
-
-
-3)
-"examplesareveryweakthinktwicebeforesubmit"
-
-Returns: "Exists"
+問題
+-文字列Sの位置i以降の部分文字列を接尾辞iとする
+-接尾辞を辞書順に並べ、その番号を配列にしたものを接尾辞配列とする
+-文字列Sが与えられる
+-接尾辞配列がSと等しく、かつ、Sより辞書順が小さい文字列が存在するかどうか求める
 
 */
 // END CUT HERE
