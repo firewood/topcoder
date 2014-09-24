cc894793dc71462baffda8ddd1085b9696ea5b36
firewood <karamaki@gmail.com>
2014-09-24 12:02:29 +0900

    translate

--- srm_6xx/srm_633/Target.cpp
+++ srm_6xx/srm_633/Target.cpp
@@ -2,83 +2,8 @@
 /*
 SRM 633 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Here at [topcoder], we call a contestant a "target" if their rating is 3000 or more.
-In the arena, the targets have a red icon with a small target on it.
-Do you want to become a target as well?
-Sure you do.
-But before you get there, let's start with something easier: drawing a target.
-
-The target you need to draw consists of nested squares.
-The innermost square is just a single '#' character.
-The larger squares use alternatingly the character ' ' (space) and the character '#'.
-Here is an example in which the side of the largest square is n = 5:
-
-#####
-#   #
-# # #
-#   #
-#####
-
-And here is an example for n = 9:
-
-#########
-#       #
-# ##### #
-# #   # #
-# # # # #
-# #   # #
-# ##### #
-#       #
-#########
-
-You will be given an int n.
-Your method must return a vector <string> which contains a drawing of the target with side n.
-More precisely, each element of the returned vector <string> must be one row of the drawing, in order.
-Therefore, the returned vector <string> will consist of n elements, each with n characters.
-(See the examples below for clarification.)
-
-The value of n will be such that a target like the ones above can be drawn: 5, 9, 13, and so on.
-Formally, n will be of the form 4k+1, where k is a positive integer.
-
-
-DEFINITION
-Class:Target
-Method:draw
-Parameters:int
-Returns:vector <string>
-Method signature:vector <string> draw(int n)
-
-
-CONSTRAINTS
--n will be between 5 and 49, inclusive.
--n mod 4 will be 1.
-
-
-EXAMPLES
-
-0)
-5
-
-Returns: {"#####", "#   #", "# # #", "#   #", "#####" }
-
-
-1)
-9
-
-Returns: {"#########", "#       #", "# ##### #", "# #   # #", "# # # # #", "# #   # #", "# ##### #", "#       #", "#########" }
-
-
-2)
-13
-
-Returns: {"#############", "#           #", "# ######### #", "# #       # #", "# # ##### # #", "# # #   # # #", "# # # # # # #", "# # #   # # #", "# # ##### # #", "# #       # #", "# ######### #", "#           #", "#############" }
-
-
-3)
-17
-
-Returns: {"#################", "#               #", "# ############# #", "# #           # #", "# # ######### # #", "# # #       # # #", "# # # ##### # # #", "# # # #   # # # #", "# # # # # # # # #", "# # # #   # # # #", "# # # ##### # # #", "# # #       # # #", "# # ######### # #", "# #           # #", "# ############# #", "#               #", "#################" }
+ñ‚ëË
+-ëÂÇ´Ç≥nÇÃè°ñ⁄Ç…ìIÇÃäGÇï`Ç≠
 
 */
 // END CUT HERE
