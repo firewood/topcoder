25db181c6f1ca26d57b5da0d1d9140884bf035e1
firewood <karamaki@gmail.com>
2014-09-24 23:42:28 +0900

    translate

--- srm_6xx/srm_626/FixedDiceGameDiv2.cpp
+++ srm_6xx/srm_626/FixedDiceGameDiv2.cpp
@@ -1,113 +1,20 @@
 // BEGIN CUT HERE
 /*
-SRM 610 Div1 Easy (250)
-SRM 610 Div1 Medium (500)
+SRM 626 Div2 Medium (500)
 
-PROBLEM STATEMENT
-
-Alice and Bob are playing a game.
-Alice has an a-sided die.
-Bob has a b-sided die.
-(The faces of an n-sided die have numbers 1 through n written on them.)
-
-
-
-The game is simple:
-Each player rolls their die, and the player who rolls a strictly larger number wins.
-(It is possible that neither player wins.)
-
-
-
-You are given the ints a and b.
-The players already rolled their dice.
-You do not know what numbers they rolled, but you know that Alice won the game.
-Compute and return the expected value of the number Alice rolled.
-
-
-
-DEFINITION
-Class:FixedDiceGameDiv2
-Method:getExpectation
-Parameters:int, int
-Returns:double
-Method signature:double getExpectation(int a, int b)
-
-
-NOTES
--Your return value must have absolute or relative error smaller than 1e-9.
--The expected value can be seen as the average of many experiments. In our case, suppose that you folow a lot of games, and each time Alice wins, you write down her score. As you watch more and more games, the average of all numbers you have written down will converge to the expected value you should return. 
-
-
-CONSTRAINTS
--a and b will each be between 2 and 1000, inclusive.
-
-
-EXAMPLES
-
-0)
-2
-2
-
-Returns: 2.0
-
-Alice can only win if she rolls a 2 and Bob rolls a 1. Thus, if we know Alice wins, we know she rolled a 2.
-
-1)
-4
-2
-
-Returns: 3.2
-
-
-
-2)
-3
-3
-
-Returns: 2.6666666666666665
-
-
-
-3)
-11
-13
-
-Returns: 7.999999999999999
-
-
+問題
+-Aliceがa面のダイス、Bobがb面のダイスを振る
+-Aliceが勝つときの出目の期待値を求める
 
 */
 // END CUT HERE
 #include <algorithm>
-#include <cmath>
-#include <numeric>
 #include <string>
-#include <map>
-#include <set>
 #include <vector>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
-
-typedef long long LL;
-
-// BEGIN CUT HERE
-/*
-typedef pair<int, int> II;
-typedef vector<int> IntVec;
-typedef vector<LL> LLVec;
-typedef vector<II> IIVec;
-typedef vector<string> StrVec;
-typedef set<int> IntSet;
-typedef set<LL> LLSet;
-typedef set<II> IISet;
-typedef set<string> StrSet;
-
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
-*/
-// END CUT HERE
 
 class FixedDiceGameDiv2 {
 public:
@@ -181,12 +88,6 @@ public:
 // BEGIN CUT HERE
 int main() {
 	FixedDiceGameDiv2 ___test;
-	___test.run_test(0);
-//	___test.run_test(1);
-//	___test.run_test(2);
-//	___test.run_test(3);
-//	___test.run_test(4);
-//	___test.run_test(5);
 	___test.run_test(-1);
 }
 // END CUT HERE
