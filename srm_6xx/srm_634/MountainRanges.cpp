c5648a1d21322a62c34bedb957e2325eb3813daf
firewood <karamaki@gmail.com>
2014-10-03 00:40:57 +0900

    translate

--- srm_6xx/srm_634/MountainRanges.cpp
+++ srm_6xx/srm_634/MountainRanges.cpp
@@ -2,81 +2,10 @@
 /*
 SRM 634 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-Tom is in charge of a tourist agency.
-He has a lovely picture of the local mountain range.
-He would like to sell it to the tourists but first he needs to know how many peaks are visible in the picture.
-
-The mountain range in the picture can be seen as a sequence of heights.
-You are given these heights as a vector <int> height.
-An element of height is called a peak if its value is strictly greater than each of the values of adjacent elements.
-Compute and return the number of peaks in the given mountain range.
-
-
-DEFINITION
-Class:MountainRanges
-Method:countPeaks
-Parameters:vector <int>
-Returns:int
-Method signature:int countPeaks(vector <int> heights)
-
-
-CONSTRAINTS
--heights will contain between 1 and 50 elements, inclusive.
--Each element of heights will be between 1 and 100, inclusive.
-
-
-EXAMPLES
-
-0)
-{5, 6, 2, 4}
-
-Returns: 2
-
-Element 1 (0-based index) is a peak.
-Its height is 6, which is strictly greater than each of its neighbors' heights (5 and 2). 
-Element 3 is also a peak since its height is 4 and that is strictly greater than its neighbor's height (which is 2).
-
-
-1)
-{1, 1, 1, 1, 1, 1, 1}
-
-Returns: 0
-
-This is a very flat mountain with no peaks.
-
-
-2)
-{2, 1}
-
-Returns: 1
-
-Element 0 is a peak.
-
-
-3)
-{2,5,3,7,2,8,1,3,1}
-
-Returns: 4
-
-The peaks here are the elements with 0-based indices 1, 3, 5, and 7. Their heights are 5, 7, 8, and 3, respectively.
-
-
-4)
-{1}
-
-Returns: 1
-
-Element 0 is a peak. Even though it has no neighbors, the condition from the problem statement is still satisfied.
-
-
-5)
-{1,2,3,4,4,3,2,1}
-
-Returns: 0
-
-According to our definition there is no peak in this mountain range.
+問題
+-高さの配列が与えられる
+-隣より高いものを山頂とする
+-山頂の総数を求める
 
 */
 // END CUT HERE
