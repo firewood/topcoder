d99c45f743ac6d67260a9b767dc814b7b959029f
firewood <karamaki@gmail.com>
2014-10-03 23:57:36 +0900

    translate

--- srm_6xx/srm_633/Jumping.cpp
+++ srm_6xx/srm_633/Jumping.cpp
@@ -2,101 +2,11 @@
 /*
 SRM 633 Div2 Medium (500)
 
-PROBLEM STATEMENT
-Frog Suwako lives on a two-dimensional plane.
-She likes to jump.
-Currently, she is located in the point (0, 0).
-She would like to reach the point (x, y).
-You are given the ints x and y.
-
-Suwako wants to reach the desired destination in a specific way: using a series of jumps with pre-determined lengths.
-You are given these lengths in a vector <int> jumpLenghts.
-For example, if jumpLengths = { 2, 5 }, Suwako must make a jump of length exactly 2, followed by a jump of length exactly 5.
-
-Note that Suwako can jump onto arbitrary points in the plane, they are not required to have integer coordinates.
-Return "Able" (quotes for clarity) if Suwako is able to reach her desired destination from (0, 0) using the desired sequence of jump lengths.
-Otherwise, return "Not able".
-
-
-DEFINITION
-Class:Jumping
-Method:ableToGet
-Parameters:int, int, vector <int>
-Returns:string
-Method signature:string ableToGet(int x, int y, vector <int> jumpLengths)
-
-
-CONSTRAINTS
--x will be between -1,000 and 1,000, inclusive.
--y will be between -1,000 and 1,000, inclusive.
--len will contain between 1 and 50 elements, inclusive.
--Each element in len will be between 1 and 1,000, inclusive.
-
-
-EXAMPLES
-
-0)
-5
-4
-{2, 5}
-
-Returns: "Able"
-
-One possibility is to jump from (0, 0) to (2, 0), and then from (2, 0) to (5, 4).
-
-
-1)
-3
-4
-{4}
-
-Returns: "Not able"
-
-The distance from (0, 0) to (3, 4) is 5. You cannot get there using a single jump of length 4 - it is too short.
-
-
-2)
-3
-4
-{6}
-
-Returns: "Not able"
-
-The distance from (0, 0) to (3, 4) is 5. You cannot get there using a single jump of length 6 - it is too long.
-
-
-3)
-0
-1
-{100, 100}
-
-Returns: "Able"
-
-Here, one possible solution looks as follows: Let t = sqrt(100*100 - 0.5*0.5). Suwoko will make her first jump from (0, 0) to (t, 0.5), and her second jump from (t, 0.5) to (0, 1).
-
-
-4)
-300
-400
-{500}
-
-Returns: "Able"
-
-
-5)
-11
-12
-{1,2,3,4,5,6,7,8,9,10}
-
-Returns: "Able"
-
-
-6)
-11
-12
-{1,2,3,4,5,6,7,8,9,100}
-
-Returns: "Not able"
+問題
+-二次元平面上に蛙がいる
+-ジャンプできる距離の配列が与えられる
+-ジャンプする方向はx軸またはy軸に平行でなくてよい
+-(x,y)に到達できるかどうかを求める
 
 */
 // END CUT HERE
