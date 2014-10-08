da2bf829a5ea851579790e0cf5edad98209a4fde
firewood <karamaki@gmail.com>
2014-10-08 15:00:20 +0900

    translate

--- srm_6xx/srm_627/ManySquares.cpp
+++ srm_6xx/srm_627/ManySquares.cpp
@@ -2,83 +2,9 @@
 /*
 SRM 627 Div2 Easy (250)
 
-PROBLEM STATEMENT
-You found a box from an old game.
-The box contains a lot of sticks and a manual.
-Frustrated by the fact the manual was unreadable, you decided to invent your own game with the sticks.
-In your game, the goal is to use the sticks to build as many squares as possible.
-There are only two rules:
-
-Each stick can only be used in one square.
-Each square must consist of exactly 4 sticks. That is, you cannot combine two or more sticks to create one side of the square.
-
-You are given a vector <int> sticks.
-The elements of sticks are the lengths of the sticks you have.
-Return the maximum number of squares you can make.
-
-
-DEFINITION
-Class:ManySquares
-Method:howManySquares
-Parameters:vector <int>
-Returns:int
-Method signature:int howManySquares(vector <int> sticks)
-
-
-NOTES
--If you can't make any square, return 0.
-
-
-CONSTRAINTS
--sticks will contain between 1 and 50 elements.
--Each element of sticks will be between 1 and 1000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,1,2,2,1,1,2}
-
-Returns: 1
-
-You can build a square with side 1.
-
-
-1)
-{3, 1, 4, 4, 4, 10, 10, 10, 10}
-
-
-Returns: 1
-
-You can build a square with side 10. You cannot build a square with side 4. (Note that you are not allowed to use 3+1 instead of a 4.)
-
-
-2)
-{1,2,3,4,1,2,3,4,1,2,3,1,2,3,4,1,2,3,3,3}
-
-Returns: 3
-
-
-3)
-{1,1,1,2,2,2,3,3,3,4,4,4}
-
-Returns: 0
-
-Sometimes you can't make any square.
-
-
-4)
-{1,1,1,2,1,1,1,3,1,1,1}
-
-Returns: 2
-
-
-5)
-{2,2,4,4,8,8}
-
-Returns: 0
-
-You are also not allowed to break the sticks.
+問題
+-何本かの棒がある
+-4本使って四角形を作るとき、できる最大の個数を求める
 
 */
 // END CUT HERE
