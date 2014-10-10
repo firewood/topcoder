e819644878f0a678ff0865132005264c42a213cb
firewood <karamaki@gmail.com>
2014-10-11 00:42:49 +0900

    translate

--- srm_6xx/srm_635/QuadraticLaw.cpp
+++ srm_6xx/srm_635/QuadraticLaw.cpp
@@ -2,84 +2,9 @@
 /*
 SRM 635 Div2 Medium (500)
 
-PROBLEM STATEMENT
-
-"Němec's quadratic law: how many minutes the teacher was late to the lesson, that many minutes squared he'll end the lesson earlier."
-
-In other words, if the teacher is t minutes late (for some non-negative integer t), he should end the lesson t2 minutes early. Of course, this means the teacher can't be too late, because a lesson can't end before even starting. It is, however, possible for the teacher to arrive and end the lesson immediately (in fact, he then only arrives to tell the students that the lesson's cancelled).
-
-You're given a long long d. The lesson was supposed to take d minutes. Compute and return the largest non-negative integer t such that the teacher can be t minutes late.
-
-
-DEFINITION
-Class:QuadraticLaw
-Method:getTime
-Parameters:long long
-Returns:long long
-Method signature:long long getTime(long long d)
-
-
-CONSTRAINTS
--d will be between 1 and 1,000,000,000,000,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-1
-
-Returns: 0
-
-The lesson was supposed to take 1 minute. The teacher can only be 0 minutes late, in which case he ends the lesson 0 minutes early (i.e. he arrives and ends the lecture on time).
-
-1)
-2
-
-Returns: 1
-
-It's possible for the teacher to be 1 minute late and end the lecture 1 minute early (so there's no lecture at all).
-
-2)
-5
-
-Returns: 1
-
-
-
-3)
-6
-
-Returns: 2
-
-
-
-4)
-7
-
-Returns: 2
-
-
-
-5)
-1482
-
-Returns: 38
-
-
-
-6)
-1000000000000000000
-
-Returns: 999999999
-
-
-
-7)
-31958809614643170
-
-Returns: 178770270
-
-
+問題
+-教師がt分遅刻してくるときには、授業をt^2分早く終わらせなければならない
+-授業の長さd分が与えられる。何分まで遅刻できるかを求める
 
 */
 // END CUT HERE
