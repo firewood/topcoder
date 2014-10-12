5dcc992fa200913d9006f63df636108d04b1db7b
firewood <karamaki@gmail.com>
2014-10-13 01:07:14 +0900

    translate

--- srm_6xx/srm_617/SilverbachConjecture.cpp
+++ srm_6xx/srm_617/SilverbachConjecture.cpp
@@ -1,62 +1,10 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 SRM 617 Div2 Easy (250)
 
-PROBLEM STATEMENT
-In this problem, some test cases have more than one correct output. We are using a special checker to verify that the output of your program is correct.
-
-A positive integer x is called composite if there are positive integers y > 1 and z > 1 such that x = yz.
-In other words, a composite positive integer has a positive integer divisor other than 1 and itself.
-For example, 48 is composite because 48 = 4*12.
-The smallest few composite integers are 4, 6, 8, 9, and 10.
-
-You are given an int n.
-Return a vector <int> with two elements.
-Both elements of the returned vector <int> must be positive composite integers, and their sum must be n.
-
-For the given constraints on n at least one such pair always exists.
-Note that if there are many such pairs, your solution can return any one of them.
-
-DEFINITION
-Class:SilverbachConjecture
-Method:solve
-Parameters:int
-Returns:vector <int>
-Method signature:vector <int> solve(int n)
-
-
-CONSTRAINTS
--n will be between 20 and 2000, inclusive.
-
-
-EXAMPLES
-
-0)
-20
-
-Returns: {8, 12 }
-
-For n=20 there are seven correct answers: {4,16}, {6,14}, {8,12}, {10,10}, {12,8}, {14,6}, and {16,4}. Your program must return one of these seven answers.
-
-
-1)
-30
-
-Returns: {15, 15 }
-
-You may return two identical numbers.
-
-
-2)
-999
-
-Returns: {699, 300 }
-
-
-3)
-45
-
-Returns: {15, 30 }
+問題
+-整数nが与えられる
+-x+y=nを満たす合成数x,yを求める
 
 */
 // END CUT HERE
