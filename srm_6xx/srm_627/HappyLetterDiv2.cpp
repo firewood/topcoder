acd3560bc774e8f5f7746637f4d028346a9f9f38
firewood <karamaki@gmail.com>
2014-10-21 19:08:53 +0900

    translate

--- srm_6xx/srm_627/HappyLetterDiv2.cpp
+++ srm_6xx/srm_627/HappyLetterDiv2.cpp
@@ -2,70 +2,10 @@
 /*
 SRM 627 Div2 Medium (500)
 
-PROBLEM STATEMENT
-The Happy Letter game is played as follows:
-At the beginning, several players enter the field.
-Each player has a lowercase English letter on their back.
-The game is played in turns.
-In each turn, you select two players with different letters, and both selected players leave the field.
-The game ends once it is impossible to take another turn.
-
-If there are some players left in the field at the end of the game, they must all have the same letter.
-That letter is called the winning letter.
-If there are no players left in the field at the end of the game, there is no winning letter.
-
-In general, there may be many different ways how to play the game, and these may lead to different outcomes of the game.
-However, sometimes we can look at the initial players and determine that each possible sequence of turns leads to the same winning letter.
-That winning letter is then called a happy letter.
-
-You are given a string letters.
-The characters in letters are the characters carried by the players at the beginning of the game.
-Return a string with one character: either the happy letter (if it exists), or a period ('.') if there is no happy letter.
-
-
-DEFINITION
-Class:HappyLetterDiv2
-Method:getHappyLetter
-Parameters:string
-Returns:char
-Method signature:char getHappyLetter(string letters)
-
-
-CONSTRAINTS
--letters will contain between 1 and 50 elements.
--Each element of letters will be a lowercase English letter ('a'-'z').
-
-
-EXAMPLES
-
-0)
-"aacaaa"
-
-Returns: 'a'
-
-In the first turn we send away the player with the 'c' and one of the players with an 'a'. All remaining players have an 'a', hence the game ends and 'a' is the winning letter.
-
-
-1)
-"dcdjx"
-
-Returns: '.'
-
-Each of the letters 'c', 'd', 'j', and 'x' can be the winning letter. Thus there is no happy letter.
-
-
-2)
-"bcbbbbba"
-
-Returns: 'b'
-
-
-3)
-"aabc"
-
-Returns: '.'
-
-Here, only 'a' can be the winning letter. However, it is also possible that there is no winning letter at the end of the game. Therefore 'a' is not considered to be the happy letter.
+問題
+-文字列が与えられる
+-1ターンに異なる文字を消していく
+-同じ文字だけが残るかどうかを求める
 
 */
 // END CUT HERE
