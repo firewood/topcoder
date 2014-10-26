212712d5d0898abde8ec05662cc3ccd55cfcf490
firewood <karamaki@gmail.com>
2014-10-26 18:47:33 +0900

    translate

--- srm_6xx/srm_637/GreaterGame.cpp
+++ srm_6xx/srm_637/GreaterGame.cpp
@@ -2,98 +2,13 @@
 /*
 SRM 637 Div1 Easy (250)
 
-// PROBLEM STATEMENT
-Cat Snuke and wolf Sothe are playing the Greater Game.
-The game is played with cards.
-Each card has a number written on it.
-There are 2N cards.
-The numbers on the cards are the integers between 1 and 2N, inclusive.
-
-At the beginning of the game, each player gets N of the cards and chooses the order in which he wants to play them.
-The game then consists of N turns.
-In each turn, both players play one of their cards simultaneously.
-The player who revealed the card with the larger number gets a point.
-
-You are given a vector <int> hand.
-The elements of hand are the numbers on Snuke's cards.
-He can play those cards in any order he chooses.
-
-Obviously, Snuke can determine the numbers on Sothe's cards, but he does not necessarily know the order in which Sothe is going to play his cards.
-You are given the information Snuke has about Sothe in a vector <int> sothe.
-For each i, element i of sothe is either the number on the card Sothe will play in turn i (0-based index), or -1 if Snuke does not know the card Sothe will play in that turn.
-
-Snuke wants to play according to a strategy that maximizes the expected number of points he'll get.
-Find that strategy and return the corresponding expected number of Snuke's points at the end of the game.
-
-As shown in Example 0, the optimal strategy for Snuke may involve some random decisions.
-However, note that before the game starts Snuke must choose the order in which he is going to play all his cards.
-He is not allowed to change their order after he sees some of the cards played by Sothe.
-
-
-DEFINITION
-Class:GreaterGame
-Method:calc
-Parameters:vector <int>, vector <int>
-Returns:double
-Method signature:double calc(vector <int> hand, vector <int> sothe)
-
-
-CONSTRAINTS
--N will be between 1 and 50, inclusive.
--hand and sothe will contain exactly N elements each.
--Each element of hand will be an integer between 1 and 2N, inclusive.
--Each element of sothe will be either -1, or an integer between 1 and 2N, inclusive.
--The positive integers in hand and sothe will be distinct.
-
-
-EXAMPLES
-
-0)
-{4,2}
-{-1,-1}
-
-Returns: 1.5
-
-Sothe will play the cards 1 and 3 in some unknown order.
-The best strategy for Snuke is to flip a coin and to play his cards either in the order {2,4} or in the order {4,2} with equal probability.
-This leads to two equally likely results of the game:
-
-Snuke plays his 2 against Sothe's 1, and his 4 against Sothe's 3. In this case, Snuke wins both turns and thus scores 2 points.
-Snuke plays his 2 against Sothe's 3, and his 4 against Sothe's 1. In this case, Snuke scores 1 point.
-
-Therefore, the expected number of Snuke's points is 1.5.
-
-
-1)
-{4,2}
-{1,3}
-
-Returns: 2.0
-
-If Snuke plays card 2 first and card 4 next, he is guaranteed to score 2 points.
-
-
-2)
-{2}
-{-1}
-
-Returns: 1.0
-
-Sothe's only card has to be 1, and thus Snuke is guaranteed to win the only turn of this game.
-
-
-3)
-{1,3,5,7}
-{8,-1,4,-1}
-
-Returns: 2.5
-
-
-4)
-{6,12,17,14,20,8,16,7,2,15}
-{-1,-1,4,-1,11,3,13,-1,-1,18}
-
-Returns: 8.0
+問題
+-1から2×Nまでの数が書かれた2×N枚のカードがある
+-2人のプレーヤーにそれぞれN枚のカードを配る
+-それぞれのプレーヤーの手札の配列が与えられる
+-相手は順番に出す手札と、順番が決まっていない手札がある
+-出した数値が大きいほうが1点得る
+-一人目のプレーヤーの点数の期待値を求める
 
 */
 // END CUT HERE
