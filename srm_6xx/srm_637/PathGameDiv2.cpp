5afffd41739a8f970e423dac4eb59c226989f94e
firewood <karamaki@gmail.com>
2014-10-27 23:49:08 +0900

    translate

--- srm_6xx/srm_637/PathGameDiv2.cpp
+++ srm_6xx/srm_637/PathGameDiv2.cpp
@@ -2,78 +2,10 @@
 /*
 SRM 637 Div2 Medium (500)
 
-PROBLEM STATEMENT
-Cat Snuke is playing the Path Game.
-
-The Path Game is played on a rectangular grid of square cells.
-The grid has 2 rows and some positive number of columns.
-Each cell is either black or white.
-
-A left-to-right path in the grid is a sequence of white cells such that the first cell in the sequence is in the leftmost column, the last cell in the sequence is in the rightmost column, and each pair of consecutive cells shares a common side.
-
-The initial coloring of the grid is such that there is at least one left-to-right path.
-You are given this initial coloring as a vector <string> board with two elements.
-For each i and j, board[i][j] is either '#' (representing a black cell) or '.' (representing a white cell).
-
-Snuke may color some of the white cells black.
-After he does so, there must still be at least one left-to-right path left on the board.
-The goal of the game is to color as many cells black as possible.
-Compute and return the largest number of cells Snuke can color black.
-(Note that the cells that are already black do not count.)
-
-
-DEFINITION
-Class:PathGameDiv2
-Method:calc
-Parameters:vector <string>
-Returns:int
-Method signature:int calc(vector <string> board)
-
-
-CONSTRAINTS
--board will contain 2 elements.
--Each element in board will contain between 1 and 50 characters, inclusive.
--All elements in board will have the same length.
--Each character in board will be '#' or '.'.
--The grid described by board will contain a left-to-right path.
-
-
-EXAMPLES
-
-0)
-{"#...."
-,"...#."}
-
-Returns: 2
-
-Snuke can color at most two white cells black.
-One possible final state of the board looks as follows:
-
-#....
-..###
-
-
-1)
-{"#"
-,"."}
-
-Returns: 0
-
-Snuke can't color any cells.
-
-
-2)
-{"."
-,"."}
-
-Returns: 1
-
-
-3)
-{"....#.##.....#..........."
-,"..#......#.......#..#...."}
-
-Returns: 13
+問題
+-幅が2の通路がある
+-#の部分は通れない
+-左から右に通れるようにしながら、#が埋められる個数を求める
 
 */
 // END CUT HERE
