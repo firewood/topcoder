7f3755d7eb1b0800e4f2901172ac2e3f9f6e7f47
firewood <karamaki@gmail.com>
2014-11-03 00:16:55 +0900

    translate

--- srm_6xx/srm_608/OneDimensionalRobotEasy.cpp
+++ srm_6xx/srm_608/OneDimensionalRobotEasy.cpp
@@ -2,74 +2,11 @@
 /*
 SRM 608 Div2 Easy (250)
 
-PROBLEM STATEMENT
-A robot is placed on an infinitely long line. Initially the position of the robot is 0. Cat Snuke sends commands to move this robot.
-
-You are given a string commands. For each i, the i-th character of commands (0-based index) represents the i-th command Snuke sends. If the i-th character of commands is 'R', the robot moves one unit to the right (i.e., from position x to position x+1). If this character is 'L', the robot moves one unit to the left (i.e., from position x to position x-1). The robot has a built-in safety mechanism that prevents it from going too far and losing the signal. The safety mechanism makes sure that the robot always stays between the positions -A and B, inclusive. If the robot receives the command 'R' when the robot is at B, or the command 'L' when the robot is at -A, the command will be ignored.
-
-You are given the string commands and the ints A and B. Return the final position of the robot.
-
-
-DEFINITION
-Class:OneDimensionalRobotEasy
-Method:finalPosition
-Parameters:string, int, int
-Returns:int
-Method signature:int finalPosition(string commands, int A, int B)
-
-
-CONSTRAINTS
--commands will contain between 1 and 50 characters, inclusive.
--Each character in commands will be either 'R' or 'L'.
--A and B will be between 1 and 50, inclusive.
-
-
-EXAMPLES
-
-0)
-"RRLRRLLR"
-10
-10
-
-Returns: 2
-
-The robot will move as follows: 0 -> 1 -> 2 -> 1 -> 2 -> 3 -> 2 -> 1 -> 2.
-
-
-1)
-"RRRRR"
-3
-4
-
-Returns: 4
-
-The robot will move as follows: 0 -> 1 -> 2 -> 3 -> 4 -> 4.
-
-
-2)
-"LLLLLLLLLLR"
-2
-6
-
-Returns: -1
-
-The robot will move as follows: 0 -> -1 -> -2 -> -2 -> -2 -> -2 -> -2 -> -2 -> -2 -> -2 -> -2 -> -1.
-
-
-3)
-"RRRRRRRLRRLRRRRRRRRRRRRLRLRRRRRRRRLRRRRRLRRRRRRRRR"
-5
-20
-
-Returns: 20
-
-
-4)
-"RLRLLLLLLLRLLLRLLLLLLLLRLLLLLRLLLRRLLLLLRLLLLLRLLL"
-34
-15
-
-Returns: -30
+問題
+-一次元平面上をロボットが移動する
+-ロボットは座標(-A,0)から(B,0)までの範囲を取る
+-コマンドが与えられる
+-ロボットの最終座標を求める
 
 */
 // END CUT HERE
