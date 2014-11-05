8dae06f731189393499e8ce080d3e2fd21ff7c35
firewood <karamaki@gmail.com>
2014-11-06 00:18:53 +0900

    translate

--- srm_6xx/srm_638/NamingConvention.cpp
+++ srm_6xx/srm_638/NamingConvention.cpp
@@ -2,65 +2,9 @@
 /*
 SRM 638 Div2 Easy (250)
 
-PROBLEM STATEMENT
-In most programming languages variable names cannot contain spaces.
-If we want a variable name that consists of two or more words, we have to encode the spaces somehow.
-In this problem, we will look at two ways of doing so: Snake Case and Camel Case.
-When using Snake Case, we just replace each space by an underscore ('_').
-When using Camel Case, we capitalize the first letter of each word except for the first one,
-and then we remove all spaces.
-
-For example, suppose that we want to declare a variable called "good morning world" (quotes for clarity).
-In Snake Case, we would write this variable as "good_morning_world",
-while in Camel Case it would be "goodMorningWorld".
-You are given a string variableName.
-This string contains a valid variable name written in Snake Case.
-Return the same variable name in Camel Case.
-
-
-DEFINITION
-Class:NamingConvention
-Method:toCamelCase
-Parameters:string
-Returns:string
-Method signature:string toCamelCase(string variableName)
-
-
-CONSTRAINTS
--variableName will contain between 1 and 50 characters.
--Each character of variableName will be 'a'-'z' or '_'.
--The first and last character of variableName will not be '_'.
--variableName will not contain two consecutive underscores ('_').
-
-
-EXAMPLES
-
-0)
-"sum_of_two_numbers"
-
-Returns: "sumOfTwoNumbers"
-
-We have 4 words: "sum", "of", "two", "numbers". So the answer is "sum" + "Of" + "Two" + "Numbers".
-
-
-1)
-"variable"
-
-Returns: "variable"
-
-Note that if we have only 1 word, then the varaible name will remain same.
-
-
-2)
-"t_o_p_c_o_d_e_r"
-
-Returns: "tOPCODER"
-
-
-3)
-"the_variable_name_can_be_very_long_like_this"
-
-Returns: "theVariableNameCanBeVeryLongLikeThis"
+問題
+-スネークケースの名前が与えられる
+-キャメルケースに変換する
 
 */
 // END CUT HERE
