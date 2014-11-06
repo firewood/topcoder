297b4753fc8d6e8a82d9a374b6d61be92ed4ad2c
firewood <karamaki@gmail.com>
2014-11-06 18:39:46 +0900

    translate

--- srm_6xx/srm_638/NarrowPassage2Easy.cpp
+++ srm_6xx/srm_638/NarrowPassage2Easy.cpp
@@ -2,83 +2,11 @@
 /*
 SRM 638 Div2 Medium (500)
 
-PROBLEM STATEMENT
-There is a narrow passage.
-Inside the passage there are some wolves.
-You are given a vector <int> size that contains the sizes of those wolves, from left to right.
-
-The passage is so narrow that some pairs of wolves cannot pass by each other.
-More precisely, two adjacent wolves may swap places if and only if the sum of their sizes is maxSizeSum or less.
-Assuming that no wolves leave the passage, what is the number of different permutations of wolves in the passage?
-Note that two wolves are considered different even if they have the same size.
-
-Compute and return the number of permutations of wolves that can be obtained from their initial order by swapping a pair of wolves zero or more times.
-
-
-DEFINITION
-Class:NarrowPassage2Easy
-Method:count
-Parameters:vector <int>, int
-Returns:int
-Method signature:int count(vector <int> size, int maxSizeSum)
-
-
-CONSTRAINTS
--size will contain between 1 and 6 elements, inclusive.
--Each element in size will be between 1 and 1,000, inclusive.
--maxSizeSum will be between 1 and 1,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1, 2, 3}
-3
-
-Returns: 2
-
-From {1, 2, 3}, you can swap 1 and 2 to get {2, 1, 3}. But you can't get other permutations.
-
-
-1)
-{1, 2, 3}
-1000
-
-Returns: 6
-
-Here you can swap any two adjacent wolves. Thus, all 3! = 6 permutations are possible.
-
-
-2)
-{1, 2, 3}
-4
-
-Returns: 3
-
-You can get {1, 2, 3}, {2, 1, 3} and {2, 3, 1}.
-
-
-3)
-{1,1,1,1,1,1}
-2
-
-Returns: 720
-
-All of these wolves are different, even though their sizes are the same. Thus, there are 6! different permutations possible.
-
-
-4)
-{2,4,6,1,3,5}
-8
-
-Returns: 60
-
-
-5)
-{1000}
-1000
-
-Returns: 1
+問題
+-通路に何匹かの狼がいる
+-それぞれの狼の大きさが配列で与えられる
+-隣り合う2匹の大きさの合計がmaxSizeSum以下なら位置を交換できる
+-可能な全ての並び方が何通りか求める
 
 */
 // END CUT HERE
@@ -101,7 +29,7 @@ public:
 			int f = 1;
 			for (int i = 0; i < N; ++i) {
 				for (int j = 0; j < i; ++j) {
-					if (seq[i] < seq[j] && size[seq[i]]+size[seq[j]]>maxSizeSum) {
+					if (seq[i] < seq[j] && size[seq[i]] + size[seq[j]] > maxSizeSum) {
 						f = 0;
 					}
 				}
