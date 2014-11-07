f7a60c1a69d33d1fafb131277270cc970a159ed7
firewood <karamaki@gmail.com>
2014-11-08 00:12:04 +0900

    translate

--- srm_6xx/srm_612/LeftAndRightHandedDiv2.cpp
+++ srm_6xx/srm_612/LeftAndRightHandedDiv2.cpp
@@ -1,68 +1,12 @@
-// BEGIN CUT HERE
+﻿// BEGIN CUT HERE
 /*
 SRM 612 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Some students are seated in a row next to each other.
-Each of them is either left-handed or right-handed.
-You are given a string S that describes the row of students.
-Each character of S is either 'L' or 'R', representing a left-handed or a right-handed person, respectively.
-The characters describe the row from the left to the right: for all i, the person described by character i+1 sits to the right of the person described by character i.
-
-The students are trying to write down lecture notes.
-Whenever a left-handed person sits immediately to the right of a right-handed person, their elbows collide when they both try to write at the same time.
-Compute and return the number of elbow collisions, assuming that all students in the row attempt to write at the same time.
-
-
-DEFINITION
-Class:LeftAndRightHandedDiv2
-Method:count
-Parameters:string
-Returns:int
-Method signature:int count(string S)
-
-
-CONSTRAINTS
--S will contain between 1 and 50 characters, inclusive.
--Each character of S will be either 'L' or 'R'.
-
-
-EXAMPLES
-
-0)
-"L"
-
-Returns: 0
-
-There's only one person in the row so there are no collisions.
-
-
-1)
-"RRR"
-
-Returns: 0
-
-Everybody is right-handed so there are no collisions.
-
-
-2)
-"LRLRLR"
-
-Returns: 2
-
-There will be two collisions: one of them between the second and the third person from the left (described by S[1] and S[2]) and the other between the fourth and the fifth person.
-
-
-3)
-"LLLRRR"
-
-Returns: 0
-
-
-4)
-"RLRLRL"
-
-Returns: 3
+問題
+-生徒が横一列に座っている
+-生徒は左利きか右利きである
+-右利きの生徒のすぐ右に左利きの生徒が座ると肘がぶつかる
+-肘がぶつかる総数を求める
 
 */
 // END CUT HERE
