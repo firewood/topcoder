c34f7c6137b21c2b868bb2bd71ae834163dafd76
firewood <karamaki@gmail.com>
2014-11-11 02:45:05 +0900

    translate

--- srm_6xx/srm_611/InterestingNumber.cpp
+++ srm_6xx/srm_611/InterestingNumber.cpp
@@ -2,77 +2,11 @@
 /*
 SRM 611 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Fox Ciel thinks that the number 41312432 is interesting.
-This is because of the following property:
-There is exactly 1 digit between the two 1s, there are exactly 2 digits between the two 2s, and so on.
-
-Formally, Ciel thinks that a number X is interesting if the following property is satisfied:
-For each D between 0 and 9, inclusive, X either does not contain the digit D at all, or it contains exactly two digits D, and there are precisely D other digits between them.
-
-You are given a string x that contains the digits of a positive integer.
-Return "Interesting" if that integer is interesting, otherwise return "Not interesting".
-
-
-DEFINITION
-Class:InterestingNumber
-Method:isInteresting
-Parameters:string
-Returns:string
-Method signature:string isInteresting(string x)
-
-
-CONSTRAINTS
--x will correspond to an integer between 1 and 1,000,000,000, inclusive.
--x will not start with a '0'.
-
-
-EXAMPLES
-
-0)
-"2002"
-
-Returns: "Interesting"
-
-There are 0 digits between the two 0s, and 2 digits between the two 2s, so this is an interesting number.
-
-
-1)
-"1001"
-
-Returns: "Not interesting"
-
-There should be 1 digit between the two 1s, but there are 2 digits between them. Hence, this number is not interesting.
-
-
-2)
-"41312432"
-
-Returns: "Interesting"
-
-This is the number in the statement.
-
-
-3)
-"611"
-
-Returns: "Not interesting"
-
-There is only one digit 6 in this number, so it's not interesting.
-
-
-4)
-"64200246"
-
-Returns: "Interesting"
-
-
-5)
-"631413164"
-
-Returns: "Not interesting"
-
-This number contains the digit 1 three times.
+問題
+-41312432は面白い数である
+-1の間に1個の数があり、2の間に2個の数がある
+-数値Xが与えられる
+-Xの各桁Dについて、同じ数値が2個だけ出現し、かつ、その間にD個の数があるかどうかを求める
 
 */
 // END CUT HERE
