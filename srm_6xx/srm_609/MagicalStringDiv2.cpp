cb9f705d8b266b6420da22a9cf08e0fe897e823d
firewood <karamaki@gmail.com>
2014-11-15 02:34:10 +0900

    translate

--- srm_6xx/srm_609/MagicalStringDiv2.cpp
+++ srm_6xx/srm_609/MagicalStringDiv2.cpp
@@ -2,59 +2,10 @@
 /*
 SRM 609 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Magical Girl Illy uses "magical strings" to cast spells.
-For her, a string X is magical if and only if there exists a positive integer k such that X is composed of k consecutive '>' characters followed by k consecutive '<' characters.
-
-Once Illy picked up a string S.
-The length of S was even, and each character of S was either '<' or '>'.
-Illy wants to change S into a magical string.
-In each step, she can change a single '>' to a '<' or vice versa.
-Compute and return the smallest number of steps in which she can change S into a magical string.
-
-
-DEFINITION
-Class:MagicalStringDiv2
-Method:minimalMoves
-Parameters:string
-Returns:int
-Method signature:int minimalMoves(string S)
-
-
-CONSTRAINTS
--S will contain between 2 and 50 characters, inclusive.
--S will contain even number of characters.
--Each character of S will be '<' or '>'.
-
-
-EXAMPLES
-
-0)
-">><<><"
-
-Returns: 2
-
-She needs to change character 2 (0-based index) from '<' to '>', and character 4 from '>' to '<'.
-
-
-1)
-">>>><<<<"
-
-Returns: 0
-
-S is already a magical string, so no changes are needed.
-
-
-2)
-"<<>>"
-
-Returns: 4
-
-
-3)
-"<><<<>>>>><<>>>>><>><<<>><><><><<><<<<<><<>>><><><"
-
-Returns: 20
+問題
+-'<'と'>'だけからなる文字列がある
+-連続するk個の'>'と連続するk個の'<'だけからなる文字列にしたい
+-変更する個数を求める
 
 */
 // END CUT HERE
