edbc2e93996121791215d14b06e72aa60d207f73
firewood <karamaki@gmail.com>
2014-11-18 02:10:40 +0900

    translate

--- srm_6xx/srm_607/BoundingBox.cpp
+++ srm_6xx/srm_607/BoundingBox.cpp
@@ -2,62 +2,9 @@
 /*
 SRM 607 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Andrew drew a bunch of points on a sheet of graph paper.
-You are given the coordinates of these points in two vector <int>s: X and Y.
-That is, for each valid i, there is a point at the coordinates (X[i], Y[i]).
-
-Now Andrew wants to draw a rectangle.
-The sides of the rectangle must be parallel to the coordinate axes.
-(In other words, each side of the rectangle must be either horizontal or vertical.)
-Additionally, each of Andrew's points must be inside the rectangle or on its boundary.
-
-Return the area of the smallest possible rectangle Andrew can draw.
-
-
-DEFINITION
-Class:BoundingBox
-Method:smallestArea
-Parameters:vector <int>, vector <int>
-Returns:int
-Method signature:int smallestArea(vector <int> X, vector <int> Y)
-
-
-CONSTRAINTS
--X will have between 2 and 50 elements, inclusive.
--X and Y will have the same number of elements.
--Each element of X and Y will be between -100 and 100, inclusive.
--The points described by X and Y will not be in a straight line horizontally or vertically. That is, the smallest rectangle will have a positive area.
-
-
-EXAMPLES
-
-0)
-{0,1}
-{1,0}
-
-Returns: 1
-
-
-1)
-{0,-2,-1}
-{-1,-1,-2}
-
-Returns: 2
-
-
-2)
-{0,0,1,0,-1,2}
-{0,1,2,-2,0,-1}
-
-Returns: 12
-
-
-3)
-{9,-88,-40,98,-55,41,-38}
-{-65,56,-67,7,-58,33,68}
-
-Returns: 25110
+問題
+-座標が書かれた何枚かの紙がある
+-それらを全て含み、各辺が軸に平行な長方形の面積の最小値を求める
 
 */
 // END CUT HERE
