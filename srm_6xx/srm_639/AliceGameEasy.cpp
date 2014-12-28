84e764aa75877b06b1383678a6fbeba5c4334da9
firewood <karamaki@gmail.com>
2014-12-28 21:49:02 +0900

    translate

--- srm_6xx/srm_639/AliceGameEasy.cpp
+++ srm_6xx/srm_639/AliceGameEasy.cpp
@@ -2,83 +2,11 @@
 /*
 SRM 639 Div2 Medium (500)
 
-PROBLEM STATEMENT
-
-Alice and Kirito just played a game.
-The game consisted of a finite (possibly empty) sequence of turns.
-You do not know the exact number of turns.
-The turns were numbered starting from 1.
-In each turn, exactly one of our two players won.
-The winner of turn i scored i points.
-
-You are given two long longs x and y.
-Find out whether it is possible that at the end of the game Alice had exactly x points and Kirito had exactly y points.
-If it is possible, return the smallest number of turns Alice could have won.
-If the given final result is not possible, return -1 instead.
-
-
-DEFINITION
-Class:AliceGameEasy
-Method:findMinimumValue
-Parameters:long long, long long
-Returns:long long
-Method signature:long long findMinimumValue(long long x, long long y)
-
-
-CONSTRAINTS
--x and y will be between 0 and 1,000,000,000,000(10^12), inclusive.
-
-
-EXAMPLES
-
-0)
-7
-14
-
-Returns: 2
-
-This final result is possible.
-One possibility is that Alice won turns 1, 2, and 4 (for 1+2+4 = 7 points) and Kirito won turns 3, 5, and 6 (for 3+5+6 = 14 points).
-However, there are also some other possibilities in which Alice only won two of the six turns, so the correct answer is 2.
-
-
-1)
-10
-0
-
-Returns: 4
-
-There must have been four turns and Alice must have won all four of them.
-
-
-2)
-932599670050
-67400241741
-
-Returns: 1047062
-
-Watch out for integer overflow.
-
-
-3)
-7
-13
-
-Returns: -1
-
-
-4)
-0
-0
-
-Returns: 0
-
-
-5)
-100000
-400500
-
-Returns: 106
+問題
+-2人でターン制のゲームをする
+-各ターンの勝者はターン番号と同じポイントを得る
+-2人の総ポイントxとyが与えられる
+-xポイントを取るための最小の勝利ターン数を求める
 
 */
 // END CUT HERE
