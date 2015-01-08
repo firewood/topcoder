1542fbd550a476e469d204c17227100f2f2adbe7
firewood <karamaki@gmail.com>
2015-01-09 00:42:01 +0900

    yukicoder #118

---
+++ yukicoder_1xx/118.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+
+#define MOD 1000000007LL
+
+int main(int argc, char *argv[])
+{
+	string s;
+	getline(cin, s);
+	int N = atoi(s.c_str());
+	getline(cin, s);
+	stringstream ss(s);
+	int cnt[102] = {};
+	int t = 0;
+	for (LL i = 0; i < N; ++i) {
+		int n;
+		ss >> n;
+		cnt[n] += 1;
+	}
+
+	LL ans = 0;
+	for (LL i = 1; i <= 100; ++i) {
+		for (LL j = i + 1; j <= 100; ++j) {
+			for (LL k = j + 1; k <= 100; ++k) {
+				ans = (ans + cnt[i] * cnt[j] * cnt[k]) % MOD;
+			}
+		}
+	}
+
+	cout << ans << endl;
+	return 0;
+}
