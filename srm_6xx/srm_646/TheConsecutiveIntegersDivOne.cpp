553a1e7eff7e77cb0b5fb91796b0a6d79e2fcaa7
firewood <karamaki@gmail.com>
2015-01-17 21:11:15 +0900

    translate

--- srm_6xx/srm_646/TheConsecutiveIntegersDivOne.cpp
+++ srm_6xx/srm_646/TheConsecutiveIntegersDivOne.cpp
@@ -2,73 +2,10 @@
 /*
 SRM 646 Div1 Easy (250)
 
-PROBLEM STATEMENT
-John and Brus have some integers.
-You are given these integers in a vector <int> numbers.
-In one operation they can pick a number and increase/decrease it by 1.
-They want to have at least k consecutive integers.
-The order in which those integers appear in numbers does not matter.
-Return the minimal number of operations required to achieve the goal.
-
-
-DEFINITION
-Class:TheConsecutiveIntegersDivOne
-Method:find
-Parameters:vector <int>, int
-Returns:int
-Method signature:int find(vector <int> numbers, int k)
-
-
-CONSTRAINTS
--numbers will contain between 2 and 47 elements, inclusive.
--Each element of numbers will be between -10,000,000 and 10,000,000, inclusive.
--All elements of numbers will be distinct.
--k will be between 1 and the number of elements in numbers, inclusive.
-
-
-EXAMPLES
-
-0)
-{4, 7, 47}
-2
-
-Returns: 2
-
-There are three optimal strategies:
-
-Increase 4 two times to obtain {6,7,47}.
-Decrease 7 two times to obtain {4,5,47}.
-Increase 4 and decrease 7 to obtain {5,6,47}.
-
-
-1)
-{1, 100}
-1
-
-Returns: 0
-
-No operation is needed.
-
-
-2)
-{-96, -53, 82, -24, 6, -75}
-2
-
-Returns: 20
-
-
-3)
-{64, -31, -56}
-2
-
-Returns: 24
-
-
-4)
-{-96, -53, 82, -24, 6, -75}
-4
-
-Returns: 90
+問題
+-いくつかの数が与えられる
+-任意の数を+1または-1して、k個の連続した数を作る
+-最小の操作回数を求める
 
 */
 // END CUT HERE
