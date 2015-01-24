29fc713f9f73f5ec88c6da94f2dd5a404ba51cef
firewood <karamaki@gmail.com>
2015-01-25 01:56:31 +0900

    translate

--- srm_6xx/srm_646/TheConsecutiveIntegersDivTwo.cpp
+++ srm_6xx/srm_646/TheConsecutiveIntegersDivTwo.cpp
@@ -2,70 +2,10 @@
 /*
 SRM 646 Div2 Easy (250)
 
-PROBLEM STATEMENT
-John and Brus have a set of integers.
-You are given its elements in a vector <int> numbers.
-They can change the integers in numbers by performing some operations.
-In each operation John will pick a number and Brus will either increase or decrease it by 1.
-
-You are also given an int k which is either 1 or 2.
-John and Brus want to have at least k integers with consecutive values in their set.
-Compute and return the smallest number of operations they have to perform.
-
-
-DEFINITION
-Class:TheConsecutiveIntegersDivTwo
-Method:find
-Parameters:vector <int>, int
-Returns:int
-Method signature:int find(vector <int> numbers, int k)
-
-
-CONSTRAINTS
--numbers will contain between 2 and 47 elements, inclusive.
--Each element of numbers will be between -10,000,000 and 10,000,000, inclusive.
--All elements of numbers will be distinct.
--k will be between 1 and 2, inclusive.
-
-
-EXAMPLES
-
-0)
-{4, 47, 7}
-2
-
-Returns: 2
-
-There are three optimal strategies:
-
-Increase 4 two times to obtain {6,47,7}.
-Decrease 7 two times to obtain {4,47,5}.
-Increase 4 and decrease 7 to obtain {5,47,6}.
-
-Note that the consecutive values can appear anywhere in the set, their position in numbers does not matter.
-
-
-1)
-{1, 100}
-1
-
-Returns: 0
-
-No operation is needed.
-
-
-2)
-{-96, -53, 82, -24, 6, -75}
-2
-
-Returns: 20
-
-
-3)
-{64, -31, -56}
-2
-
-Returns: 24
+問題
+-いくつかの数が与えられる
+-任意の数を+1または-1して、1または2個の連続した数を作る
+-最小の操作回数を求める
 
 */
 // END CUT HERE
