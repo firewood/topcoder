95db6c8914c4112c0aae06fbe68f0fa54d54bc22
firewood <karamaki@gmail.com>
2015-02-02 00:17:38 +0900

    translate

--- srm_6xx/srm_644/OkonomiyakiShop.cpp
+++ srm_6xx/srm_644/OkonomiyakiShop.cpp
@@ -2,76 +2,11 @@
 /*
 SRM 644 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Cat Snuke and Wolf Sothe came to an okonomiyaki restaurant.
-(Okonomiyaki is a Japanese pancake.)
-The restaurant makes several types of okonomiyaki.
-Each type of okonomiyaki has its specific size.
-You are given the sizes of all okonomiyaki types as a vector <int> osize.
-
-The restaurant offers a discount if you order a meal for two persons.
-Each such order must consist of two okonomiyakis of different types.
-Additionally, the sizes of the two okonomiyakis must differ by K or less.
-
-You are given the vector <int> osize and the int K.
-Compute and return the number of different two-person meals offered by the restaurant.
-
-
-DEFINITION
-Class:OkonomiyakiShop
-Method:count
-Parameters:vector <int>, int
-Returns:int
-Method signature:int count(vector <int> osize, int K)
-
-
-CONSTRAINTS
--osize will contain between 2 and 50 elements, inclusive.
--Each element in osize will be between 1 and 10,000, inclusive.
--K will be between 1 and 10,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,4,6,7,9}
-3
-
-Returns: 6
-
-There are five types of okonomiyaki.
-Let's call them types A, B, C, D, and E.
-(Type A has size 1, type B has size 4, and so on.)
-There are 6 valid two-person meals: A+B, B+C, B+D, C+D, C+E, and D+E.
-
-Note that B+A is the same order as A+B: you get one pancake of type A and one pancake of type B.
-
-
-1)
-{1,1,3,3,3}
-2
-
-Returns: 10
-
-Different types of okonomiyaki may have equal sizes.
-A two-person meal may contain two okonomiyaki of the same size, they are only required to have a different type.
-Therefore, in this test case each pair of different okonomiyaki can be ordered as a two-person meal.
-
-
-2)
-{1,5,9,14,20}
-3
-
-Returns: 0
-
-In this test case there is no valid two-person meal: the differences in pancake sizes are all greater than K.
-
-
-3)
-{7,2,6,3,4,2,7,8,3,4,9,1,8,4,3,7,5,2,1,9,9,4,5}
-6
-
-Returns: 234
+問題
+-お好み焼きの店がある
+-何種類かのお好み焼きがある
+-2人で違う種類かつ大きさの差がK以内のものを頼むと割引になる
+-割引になる場合の数を求める
 
 */
 // END CUT HERE
