c0d59f272f8d7d946e11a6e0770d9ca9dd3aaa68
firewood <karamaki@gmail.com>
2015-04-02 01:26:40 +0900

    translate

--- srm_6xx/srm_642/ForgetfulAddition.cpp
+++ srm_6xx/srm_642/ForgetfulAddition.cpp
@@ -2,73 +2,10 @@
 /*
 SRM 642 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-Alice had two positive integers, a and b.
-She typed the expression "a+b" into her computer, but the '+' key malfunctioned.
-For example, instead of "128+9" the computer's screen now shows "1289".
-
-Later, Bob saw the string on the screen.
-He knows that the '+' sign is missing but he does not know where it belongs.
-He now wonders what is the smallest possible result of Alice's original expression.
-
-For example, if Bob sees the string "1289", Alice's expression is either "128+9" or "12+89" or "1+289".
-These expressions evaluate to 137, 101, and 290.
-The smallest of those three results is 101.
-
-You are given a string expression that contains the expression on Alice's screen.
-Compute and return the smallest possible result after inserting the missing plus sign
-
-
-DEFINITION
-Class:ForgetfulAddition
-Method:minNumber
-Parameters:string
-Returns:int
-Method signature:int minNumber(string expression)
-
-
-CONSTRAINTS
--expression will contain between 2 and 8 characters, inclusive.
--Each character of expression will be between '1' and '9'.
-
-
-EXAMPLES
-
-0)
-"22"
-
-Returns: 4
-
-The only possible expression Alice could have typed is "2+2". Thus, Bob knows this evaluates to 4.
-
-
-1)
-"123"
-
-Returns: 15
-
-The expression Alice has typed could have been "1+23" or "12+3". Of these two, the second is smaller, thus Bob will get the answer 15. 
-
-
-2)
-"1289"
-
-Returns: 101
-
-This is the example from the problem statement.
-
-
-3)
-"31415926"
-
-Returns: 9067
-
-
-4)
-"98765"
-
-Returns: 863
+問題
+-数値が与えられる
+-数値のどこかの間にプラス記号をはさむ
+-ありうる最小の計算結果を求める
 
 */
 // END CUT HERE
