9925ec11232df3e8a03a42b730b0d8cae7258a08
firewood <karamaki@gmail.com>
2015-04-09 00:26:41 +0900

    translate

--- srm_6xx/srm_648/Fragile2.cpp
+++ srm_6xx/srm_648/Fragile2.cpp
@@ -2,83 +2,9 @@
 /*
 SRM 648 Div2 Medium (500)
 
-PROBLEM STATEMENT
-Lun the dog has found an undirected graph in Shuseki Forest. The graph consisted of N vertices and some edges. The vertices of the graph were numbered 0 through N-1. Each edge connected a different pair of vertices.
-
-You are given the description of the graph in a vector <string> graph with N elements, each containing N characters. For each i and j, graph[i][j] will be 'Y' if vertex i and vertex j are connected by an edge, and 'N' otherwise. (Note that for each i, graph[i][i] will be 'N': there are no self-loops.)
-
-Lun is interested in articulation pairs in this graph. An articulation pair is an unordered pair of two different vertices whose deletion increases the number of connected components in the graph. (The deletion of a vertex also removes all edges incident with that vertex.)
-
-Return the number of the articulation pairs in Lun's graph.
-
-DEFINITION
-Class:Fragile2
-Method:countPairs
-Parameters:vector <string>
-Returns:int
-Method signature:int countPairs(vector <string> graph)
-
-
-NOTES
--You are not given the value of N explicitly, but you can determine it as the number of elements in graph.
--Two vertices belong to the same connected component if and only if we can reach one of them from the other by following a sequence of zero or more edges.
-
-
-CONSTRAINTS
--graph will contain between 3 and 20 elements, inclusive.
--Each element of graph will contain N characters, where N is the number of the elements in graph.
--Each character of each element of graph will be either 'Y' or 'N'.
--For each valid i and j, graph[i][j] will be equal to graph[j][i].
--For each valid i, graph[i][i] will be 'N'.
-
-
-EXAMPLES
-
-0)
-{"NYNN", "YNYN", "NYNY", "NNYN"}
-
-Returns: 3
-
-The graph looks as follows:
-
-
-The articulation pairs are (0, 2), (1, 2) and (1, 3).
-For example, here is why (0, 2) is an articulation pair:
-Currently there is one connected component.
-(It contains all four vertices.)
-If we remove the vertices 0 and 2, and all edges incident to these vertices, we will be left with two isolated vertices: 1 and 3.
-Each of these vertices now forms a different connected component, so the number of connected components increased from 1 to 2.
-
-1)
-{"NYNNNN", "YNYNNN", "NYNNNN", "NNNNYN", "NNNYNY", "NNNNYN"}
-
-Returns: 5
-
-
-The articulation pairs are (0, 4), (1, 3), (1, 4), (1, 5) and (2, 4).
-
-2)
-{"NNN", "NNN", "NNN"}
-
-Returns: 0
-
-
-There are no articulation pairs.
-
-3)
-{"NYNYNNYYNN", "YNNNYNYYNN", "NNNNYNNNYN", "YNNNYYNNNN", "NYYYNNNNYN",
- "NNNYNNNNYN", "YYNNNNNNNN", "YYNNNNNNYN", "NNYNYYNYNY", "NNNNNNNNYN"}
-
-Returns: 9
-
-
-4)
-{"NNNYNNYNNNNNNNYYNNNY", "NNNNNNNNYNNNNNNNNNNN", "NNNNNNNNNNNNNNNNNNNN", "YNNNNNNNNNYNNNNNNNNN", "NNNNNNNYNNNNNYNNNNYN",
- "NNNNNNNNNNNNNNNNYNNY", "YNNNNNNNNNNNNYYYNYNN", "NNNNYNNNNNNNNYYNNNNN", "NYNNNNNNNYNNNNNNNNNN", "NNNNNNNNYNNNYNNNNNYN",
- "NNNYNNNNNNNNNNYNNNNN", "NNNNNNNNNNNNNNNNNNNN", "NNNNNNNNNYNNNNNNNYNN", "NNNNYNYYNNNNNNNNNNNN", "YNNNNNYYNNYNNNNNNNNN",
- "YNNNNNYNNNNNNNNNYNNN", "NNNNNYNNNNNNNNNYNYNN", "NNNNNNYNNNNNYNNNYNNN", "NNNNYNNNNYNNNNNNNNNN", "YNNNNYNNNNNNNNNNNNNN"}
-
-Returns: 42
+問題
+-N個の頂点と、いくつかの辺がある
+-2点を除去したときに連結な集合の個数が増える場合の数を求める
 
 */
 // END CUT HERE
