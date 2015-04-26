cd91ccf25cc6f2ff5e5ea19811b951b73a1fb9ac
firewood <karamaki@gmail.com>
2015-04-27 01:29:26 +0900

    translate

--- srm_6xx/srm_656/RandomPancakeStack.cpp
+++ srm_6xx/srm_656/RandomPancakeStack.cpp
@@ -4,89 +4,12 @@ SRM 656 Div1 Easy (250)
 
 PROBLEM STATEMENT
 
-Charlie has N pancakes.
-He wants to serve some of them for breakfast.
-We will number the pancakes 0 through N-1.
-For each i, pancake i has width i+1 and deliciousness d[i].
-
-Charlie chooses the pancakes he is going to serve using the following randomized process:
-He starts by choosing the first pancake uniformly at random from all the pancakes he has.
-He places the chosen pancake onto a plate.
-This pancake now forms the bottom of a future stack of pancakes.
-Then, Charlie repeats the following procedure:
-
-If there are no more pancakes remaining, terminate.
-Choose a pancake uniformly at random from the pancakes that have not been chosen yet.
-If the width of this pancake is greater than the width of the pancake on top of the stack, terminate without taking it.
-Place the chosen pancake on top of the stack and go back to step 1.
-
-You are given the vector <int> d with N elements.
-The total deliciousness of a serving of pancakes is the sum of the deliciousness of all pancakes used in the serving.
-Compute and return the expected value of the total deliciousness of the pancakes chosen by Charlie.
-
-
-DEFINITION
-Class:RandomPancakeStack
-Method:expectedDeliciousness
-Parameters:vector <int>
-Returns:double
-Method signature:double expectedDeliciousness(vector <int> d)
-
-
-NOTES
--Your return value must have an absolute or relative error smaller than or equal to 1e-6
-
-
-CONSTRAINTS
--The number of elements in d will be between 1 and 250, inclusive.
--Each element of d will be between 1 and 1,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,1,1}
-
-Returns: 1.6666666666666667
-
-The following scenarios may occur:
-
-With probability 1/3, Charlie chooses pancake 0 first. In this case he won't be able to add any more pancakes and the total deliciousness of his serving of pancakes will be 1.
-With probability 1/3, Charlie chooses pancake 1 first. What happens in the second round? With probability 1/2 he will choose pancake 0 and with probability 1/2 it will be pancake 2. In the first case the total deliciousness of Charlie's pancakes will be 2, in the second case it will be 1.
-With probability 1/3, Charlie chooses pancake 2 first. If he chooses pancake 0 next, the total deliciousness of his pancakes will be 2. If he happens to choose pancake 1 next (followed by pancake 0 in the third round), the total deliciousness will be 3.
-
-Summing this up, we get the expected deliciousness to be 1/3 * (1) + 1/3 * (1/2 * 1 + 1/2 * 2) + 1/3 * (1/2 * 2 + 1/2 * 3) = 5/3 = 1.666...
-
-
-1)
-{3,6,10,9,2}
-
-Returns: 9.891666666666667
-
-
-2)
-{10,9,8,7,6,5,4,3,2,1}
-
-Returns: 10.999999724426809
-
-
-3)
-{1,2,3,4,5,6,7,8,9,10}
-
-Returns: 7.901100088183421
-
-
-4)
-{2,7,1,8,2,8,1,8,2,8,4,5,90,4,5,2,3,5,60,2,8,74,7,1}
-
-Returns: 19.368705050402465
-
-
-5)
-{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
- 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-
-Returns: 1.718281828459045
+問題
+-N枚のパンケーキがある
+-それぞれの大きさは1からNで、それぞれのおいしさが与えられる
+-1枚ずつランダムに選んで積んでいく
+-ただし、一つ前に積んだものより大きければ、それを積まないで終了
+-詰まれたパンケーキのおいしさの合計の期待値を求める
 
 */
 // END CUT HERE
