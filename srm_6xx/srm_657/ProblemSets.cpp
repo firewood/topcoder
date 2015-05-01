0829c87105b7ba55e9427d01051c7992aa49b58f
firewood <karamaki@gmail.com>
2015-05-01 22:07:34 +0900

    translate

--- srm_6xx/srm_657/ProblemSets.cpp
+++ srm_6xx/srm_657/ProblemSets.cpp
@@ -2,87 +2,10 @@
 /*
 SRM 657 Div1 Easy (250)
 
-PROBLEM STATEMENT
-Cat Snuke came up with some problems. He wants to construct as many problem sets as possible using those problems.
-
-Each problem set must contain exactly three problems: one for the Easy slot, one for the Medium slot, and one for the Hard slot. Each problem can only be assigned to a single slot in a single problem set. He came up with E + EM + M + MH + H problems in total. The distribution of the problems is as follows:
-
-E problems can only be used in the Easy slot.
-EM problems can be used either in the Easy slot or the Medium slot.
-M problems can only be used in the Medium slot.
-MH problems can be used either in the Medium slot or the Hard slot.
-H problems can only be used in the Hard slot.
-
-Return the maximal number of problem sets he can construct.
-
-
-DEFINITION
-Class:ProblemSets
-Method:maxSets
-Parameters:long long, long long, long long, long long, long long
-Returns:long long
-Method signature:long long maxSets(long long E, long long EM, long long M, long long MH, long long H)
-
-
-CONSTRAINTS
--E, EM, M, MH, H will be between 0 and 1,000,000,000,000,000,000 (10^18), inclusive.
-
-
-EXAMPLES
-
-0)
-2
-2
-1
-2
-2
-
-Returns: 3
-
-One of EM problems should be used for the Easy slot, and the other should be used for the Medium slot.
-One of MH problems should be used for the Medium slot, and the other should be used for the Hard slot.
-
-
-1)
-100
-100
-100
-0
-0
-
-Returns: 0
-
-Unfortunately, no problem can be used for the Hard slot.
-
-
-2)
-657
-657
-657
-657
-657
-
-Returns: 1095
-
-
-3)
-1
-2
-3
-4
-5
-
-Returns: 3
-
-
-4)
-1000000000000000000
-1000000000000000000
-1000000000000000000
-1000000000000000000
-1000000000000000000
-
-Returns: 1666666666666666666
+問題
+-SRM用の問題がある
+-問題はEasy専用、EasyまたはMedium用、Medium専用、MediumまたはHard専用のいずれかである
+-EasyとMediumとHardの一つずつをセットにするとき、最大何セット作れるかを求める
 
 */
 // END CUT HERE
