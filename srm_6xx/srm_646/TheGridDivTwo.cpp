b697c3eff246098534a19b13cbf982e388e6e15a
firewood <karamaki@gmail.com>
2015-06-27 01:02:06 +0900

    translate

--- srm_6xx/srm_646/TheGridDivTwo.cpp
+++ srm_6xx/srm_646/TheGridDivTwo.cpp
@@ -2,74 +2,11 @@
 /*
 SRM 646 Div2 Medium (500)
 
-PROBLEM STATEMENT
-John is standing at the origin of an infinite two-dimensional grid.
-He is going to move along this grid.
-During each second he can either stay where he is or he can move by one unit in one of the four cardinal directions (north, south, east, or west).
-Some of the grid points are blocked.
-John is not allowed to move to a blocked grid point.
-
-You are given the coordinates of the blocked grid points as vector <int>s x and y.
-For each valid i, the grid point that is x[i] units east and y[i] units north of the origin is blocked.
-You are also given an int k.
-Compute and return the maximal possible x-coordinate of a point John can reach in k seconds.
-
-
-DEFINITION
-Class:TheGridDivTwo
-Method:find
-Parameters:vector <int>, vector <int>, int
-Returns:int
-Method signature:int find(vector <int> x, vector <int> y, int k)
-
-
-CONSTRAINTS
--x will contain between 0 and 47 elements, inclusive.
--x and y will contain the same number of elements.
--Each element of x will be between -1,000 and 1,000, inclusive.
--Each element of y will be between -1,000 and 1,000, inclusive.
--All pairs (x[i], y[i]) will be distinct.
--Each pair (x[i], y[i]) will be different from (0, 0).
--k will be between 1 and 1,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,1,1,1}
-{-2,-1,0,1}
-4
-
-Returns: 2
-
-The optimal strategy is to move two times north to (0, 2) and then two times east to (2,2).
-
-
-1)
-{-1, 0, 0, 1}
-{0, -1, 1, 0}
-9
-
-Returns: 0
-
-John can not make any moves.
-
-
-2)
-{}
-{}
-1000
-
-Returns: 1000
-
-
-3)
-{1,0,0,-1,-1,-2,-2,-3,-3,-4,-4}
-{0,-1,1,-2,2,-3,3,-4,4,-5,5}
-47
-
-Returns: 31
-
+問題
+-二次元の無限の升目がある
+-単位時間に縦または横に1マス移動できる
+-移動できないマスが与えられる
+-単位時間k経過後に行けるx位置の最大値を求める
 
 */
 // END CUT HERE
