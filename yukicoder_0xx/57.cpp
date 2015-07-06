8b92e9af9d7dad2a647453fe5f862fc0f41ad821
firewood <karamaki@gmail.com>
2015-07-07 01:02:42 +0900

    yukicoder #57

---
+++ yukicoder_0xx/57.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout.precision(10);
+	int N;
+	cin >> N;
+	long double ans = N * 3.5;
+	cout << ans << endl;
+	return 0;
+}
