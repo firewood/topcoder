e804bd8003e05a0186ab497872fc3b6d82cedfd6
firewood <karamaki@gmail.com>
2015-07-22 00:28:08 +0900

    translate

--- tco_2015/YetAnotherCardGame.cpp
+++ tco_2015/YetAnotherCardGame.cpp
@@ -2,82 +2,12 @@
 /*
 TCO 2015 Round 2C Easy (250)
 
-PROBLEM STATEMENT
-Petr and Snuke are playing a cooperative card game.
-The game is played with special cards: each card is labeled with some positive integer.
-The integers on cards are not necessarily distinct.
-
-At the beginning of the game Petr is holding some cards in his hand and Snuke is holding all the other cards in his hand.
-You are given vector <int>s petr and snuke that describe the state at the beginning of the game:
-the elements of petr are the numbers on Petr's cards and the elements of snuke are the numbers on Snuke's cards.
-
-During the game the players will place some of their cards onto a pile.
-Initially, the pile is empty.
-The players take alternating turns, Petr goes first.
-In each turn, if the current player has no cards in his hand, the game ends.
-Otherwise, the player must make exactly one valid move.
-There are three types of valid moves:
-
-If the pile is empty, the player may choose any card and place it onto the pile.
-If the pile is not empty, the player may choose any card and place it on top of the pile. However, this move is only valid if the number on the new card is strictly greater than the number on the card that was previously on the top of the pile.
-The player may always choose one of his cards and eat it.
-
-Petr and Snuke have a common goal: they want to create a pile with as many cards as possible.
-Return the size of the pile at the end of the game, assuming that they cooperate and play the game optimally.
-
-
-DEFINITION
-Class:YetAnotherCardGame
-Method:maxCards
-Parameters:vector <int>, vector <int>
-Returns:int
-Method signature:int maxCards(vector <int> petr, vector <int> snuke)
-
-
-CONSTRAINTS
--petr and snuke will contain between 1 and 50 elements, inclusive.
--petr and snuke will contain the same number of elements.
--Each element in petr and snuke will be between 1 and 100, inclusive.
-
-
-EXAMPLES
-
-0)
-{2, 5}
-{3, 1}
-
-Returns: 3
-
-One optimal way is as follows.
-
-Petr puts 2 onto the pile.
-Snuke puts 3 onto the pile.
-Petr puts 5 onto the pile.
-Snuke eats 1.
-The game ends because Petr has no cards in his hand.
-
-
-1)
-{1, 1, 1, 1, 1}
-{1, 1, 1, 1, 1}
-
-Returns: 1
-
-The integers on cards are not necessarily distinct.
-
-
-2)
-{1, 4, 6, 7, 3}
-{1, 7, 1, 5, 7}
-
-Returns: 6
-
-
-3)
-{19, 99, 86, 30, 98, 68, 73, 92, 37, 69, 93, 28, 58, 36, 86, 32, 46, 34, 71, 29}
-{28, 29, 22, 75, 78, 75, 39, 41, 5, 14, 100, 28, 51, 42, 9, 25, 12, 59, 98, 83}
-
-Returns: 28
+問題
+-1以上の整数が書かれたカードが何枚かある
+-PetrとSnukeにN枚ずつ配り、交互に1枚ずつ出す
+-場にカードがないか、場のカードより大きな数のときだけ積むことができる
+-詰めないときはカードを捨てる
+-場に積める枚数の最大値を求める
 
 */
 // END CUT HERE
@@ -91,25 +21,6 @@ Returns: 28
 using namespace std;
 
 typedef vector<int> IntVec;
-
-typedef long long LL;
-
-// BEGIN CUT HERE
-/*
-typedef pair<int, int> II;
-typedef vector<int> IntVec;
-typedef vector<LL> LLVec;
-typedef vector<II> IIVec;
-typedef vector<string> StrVec;
-typedef set<int> IntSet;
-typedef set<LL> LLSet;
-typedef set<II> IISet;
-typedef set<string> StrSet;
-
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
-*/
-// END CUT HERE
 
 class YetAnotherCardGame {
 public:
