99fe64c02ba54470239fceb03d1fb30bdd012b5f
firewood <karamaki@gmail.com>
2015-08-22 01:22:14 +0900

    yukicoder #111

---
+++ yukicoder_1xx/111.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	long long L;
+	cin >> L;
+	long long ans = (L / 2) * (L / 2);
+	cout << ans << endl;
+	return 0;
+}
