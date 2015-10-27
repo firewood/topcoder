71ab1355952f8b7dce733665229de1d0d7342b88
firewood <karamaki@gmail.com>
2015-10-28 01:24:40 +0900

    translate

--- srm_6xx/srm_671/BearCries.cpp
+++ srm_6xx/srm_671/BearCries.cpp
@@ -2,93 +2,12 @@
 /*
 SRM 671 Div1 Easy (250)
 
-PROBLEM STATEMENT
-Limak is a polar bear who often chats online with his friends.
-Nowadays, bears often use emoticons to express their feelings.
-In this problem we consider one particular emoticon: the crying emoticon.
-
-A crying emoticon consists of an arbitrary positive number of underscores between two semicolons.
-Hence, the shortest possible crying emoticon is ";_;" (quotes for clarity).
-The strings ";__;" and ";_____________;" are also valid crying emoticons.
-
-Today Limak is sad, so he sent his friend a sequence of crying emoticons.
-However, due to a network malfunction all those emoticons got mixed together into a single string.
-
-You are given a string message containing the message Limak's friend received.
-You guess that the message can be divided into one or more crying emoticons.
-Each emoticon must be a subsequence of the message, and each character of the message must belong to exactly one emoticon.
-Note that the subsequences are not required to be contiguous.
-
-Let X be the number of ways in which one can divide the given message into emoticons.
-Compute and return the value (X modulo 1,000,000,007).
-
-
-DEFINITION
-Class:BearCries
-Method:count
-Parameters:string
-Returns:int
-Method signature:int count(string message)
-
-
-CONSTRAINTS
--message will contain between 1 and 200 characters, inclusive.
--Each character in message will be either semicolon or underscore.
-
-
-EXAMPLES
-
-0)
-";_;;_____;"
-
-Returns: 2
-
-There are two ways to divide this string into two crying emoticons.
-One looks as follows:
-
- ;_;
-    ;_____;
-
-and the other looks like this:
-
- ;_ ;
-   ; _____;
-
-
-1)
-";;;___;;;"
-
-Returns: 36
-
-This message consists of 3 semicolons, 3 underscores and 3 semicolons again.
-Clearly, we have to divide this message into exactly three crying emoticons, each with a single underscore.
-There are 36 different ways to do so.
-
-
-2)
-"_;__;"
-
-Returns: 0
-
-As this message begins with an underscore, it clearly cannot be divided into one or more crying emoticons: each crying emoticon starts with a semicolon.
-
-
-3)
-";_____________________________________;"
-
-Returns: 1
-
-
-4)
-";__;____;"
-
-Returns: 0
-
-
-5)
-";_;;__;_;;"
-
-Returns: 52
+問題
+-1個以上の泣きの顔文字;_;をネットワーク上に送信する
+-アンダースコアは1つ以上なら何個でもよい
+-複数の顔文字が混ざって1つの文字列として送信される
+-1つの顔文字内の文字の順番は入れ替わらない
+-最終的に送信されるmessageが何通りに復元できるかを求める
 
 */
 // END CUT HERE
