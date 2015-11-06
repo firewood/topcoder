0933e90cade551654caee846310cbaad0ad66763
firewood <karamaki@gmail.com>
2015-11-07 00:51:22 +0900

    fix

--- yukicoder_0xx/25.cpp
+++ yukicoder_0xx/25.cpp
@@ -2,8 +2,6 @@
 #include <algorithm>
 #include <numeric>
 #include <sstream>
-#include <set>
-#include <vector>
 
 using namespace std;
 typedef long long LL;
@@ -12,28 +10,23 @@ LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
 
 int main(int argc, char *argv[]) {
 	LL N, M;
-	string s;
-	getline(cin, s);
-	stringstream sa(s);
-	sa >> N;
-	getline(cin, s);
-	stringstream sb(s);
-	sb >> M;
+	cin >> N >> M;
 	LL g = gcd(N, M);
 	LL a = N / g, b = M / g;
+	for (LL d = 2; d <= 5; d += 3) {
+		while ((b % d) == 0) {
+			b /= d;
+			if ((a % d) == 0) {
+				a /= d;
+			} else {
+				a = ((a % 10) * 10) / d;
+			}
+		}
+	}
 	while ((a % 10) == 0) {
 		a /= 10;
 	}
-	a = a % 10;
-	while ((b % 2) == 0) {
-		a = (a * 5) % 10;
-		b /= 2;
-	}
-	while ((b % 5) == 0) {
-		a = (a * 2) % 10;
-		b /= 5;
-	}
-	LL ans = b == 1 ? a : -1;
+	LL ans = b == 1 ? (a % 10) : -1;
 	cout << ans << endl;
 	return 0;
 }
