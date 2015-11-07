d316d32a65ca05db955c6e5399ddccea2a6d4878
firewood <karamaki@gmail.com>
2015-11-08 00:36:20 +0900

    translate

--- srm_6xx/srm_670/Bracket107.cpp
+++ srm_6xx/srm_670/Bracket107.cpp
@@ -2,84 +2,14 @@
 /*
 SRM 670 Div1 Easy (250)
 
-PROBLEM STATEMENT
-Correct bracket sequences are strings in which each character is a '(' or a ')',
-the total number of opening brackets is the same as the total number of closing brackets,
-and each prefix contains at least as many opening as closing brackets.
-For example, the shortest few correct bracket sequences are "", "()", "(())", and "()()".
-
-The subsequence of a string S is any string that can be obtained by erasing zero or more characters of S.
-For example, each of the strings "", "ace", and "abcde" is a subsequence of "abcde".
-
-We will use LCS(S,T) to denote the length of a longest common subsequence of strings S and T.
-In other words, LCS(S,T) is the largest k such that there is a string U of length k that is both a subsequence of S and a subsequence of T.
-For example, LCS("abcde","bad") = 2.
-
-You are given a string s that contains a correct bracket sequence.
-You are looking for a string t with the following properties:
-
-t will have the same length as s,
-t will be a correct bracket sequence,
-t will not be equal to s,
-LCS(s,t) will be as large as possible.
-
-Compute and return the number of strings with these properties.
-
-
-DEFINITION
-Class:Bracket107
-Method:yetanother
-Parameters:string
-Returns:int
-Method signature:int yetanother(string s)
-
-
-NOTES
--You may assume that the answer for each valid test case fits into a signed 32-bit integer variable.
-
-
-CONSTRAINTS
--s will contain between 4 and 50 characters, inclusive.
--Each character in s will be either '(' or ')'.
--s will be a correct bracket sequence.
-
-
-EXAMPLES
-
-0)
-"(())"
-
-Returns: 1
-
-There is only one other correct bracket sequence of the same length.
-
-
-1)
-"(())()"
-
-Returns: 3
-
-There are four other correct bracket sequences of the same length: "((()))", "()(())", "()()()", and "(()())".
-However, only in three of those four cases the length of the longest common subsequence is 5.
-LCS( "(())()", "()(())" ) is only 4, therefore "()(())" is not a valid choice of the string t.
-
-
-2)
-"()()()"
-
-Returns: 3
-
-
-3)
-"(((())))"
-
-Returns: 5
-
-
-4)
-"((())())"
-
-Returns: 9
+問題
+-'('と')'だけからなる文字列がある
+-それらのうち閉じ括弧が正しく対応している文字列Sがある
+-文字列Tが次の条件を満たすとき、Tの個数を求める
+-Sと同じ長さ
+-閉じ括弧が正しく対応している
+-Sとは異なる
+-SとTの部分文字列の共通部分の長さが最大
 
 */
 // END CUT HERE
