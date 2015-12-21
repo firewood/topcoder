e4bc0ff2a68ca580f8d474a89a4a67e3334ab38f
firewood <karamaki@gmail.com>
2015-12-22 00:23:48 +0900

    yukicoder #326

---
+++ yukicoder_3xx/326.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+
+int main(int argc, char *argv[])
+{
+	int N, K;
+	cin >> N >> K;
+	int p[101];
+	for (int i = 1; i <= 100; ++i) {
+		p[i] = i;
+	}
+	for (int i = 0; i < K; ++i) {
+		int x, y;
+		cin >> x >> y;
+		swap(p[x], p[y]);
+	}
+	int target[101];
+	for (int i = 1; i <= N; ++i) {
+		cin >> target[i];
+	}
+	vector<int> v;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			if (target[j] == i) {
+				for (int k = 1; k <= N; ++k) {
+					if (p[k] == j) {
+						while (k > i) {
+							--k;
+							v.push_back(k);
+							swap(p[k], p[k + 1]);
+						}
+					}
+				}
+			}
+		}
+	}
+						
+	for (int i = 0; i != v.size(); ++i) {
+		cout << v[i] << " " << (v[i] + 1) << endl;
+	}
+
+
+
+	return 0;
+}
