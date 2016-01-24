64d124d14c87484e04f3d891c4e5eb07c713244b
firewood <karamaki@gmail.com>
2016-01-25 00:02:40 +0900

    fix

--- yukicoder_1xx/130.cpp
+++ yukicoder_1xx/130.cpp
@@ -2,44 +2,43 @@
 #include <algorithm>
 #include <sstream>
 #include <cstdio>
+#include <vector>
 
 using namespace std;
+
+typedef vector<int> IntVec;
+
+int getmin(int b, IntVec &a) {
+	if (b <= 0) {
+		return 0;
+	}
+	IntVec c[2];
+	for (int n : a) {
+		if (n & b) {
+			c[1].push_back(n ^ b);
+		} else {
+			c[0].push_back(n);
+		}
+	}
+	if (c[0].size() == 0) {
+		return getmin(b >> 1, c[1]);
+	}
+	if (c[1].size() == 0) {
+		return getmin(b >> 1, c[0]);
+	}
+	return b | min(getmin(b >> 1, c[0]), getmin(b >> 1, c[1]));
+}
 
 int main(int argc, char *argv[]) {
 	int N;
 	cin >> N;
-	int A[100000], H[100000], L[100000], f = 0, h = 0, l = 0;
+	IntVec A(N);
 	for (int i = 0; i < N; ++i) {
 		cin >> A[i];
-		H[i] = A[i] >> 15, L[i] = A[i] & ((1 << 15) - 1);
-		f |= H[i], h ^= H[i], l ^= L[i];
 	}
-	int lm = 1 << 30;
-	for (int b = 1; b < (1 << 15); ++b) {
-		int x = 0;
-		for (int i = 0; i < N; ++i) {
-			x ^= L[i];
-		}
-		lm = min(lm, x);
-	}
-	int ans = lm;
-	if (f) {
-		int hm = 1 << 30;
-		for (int b = 1; b < (1 << 15); ++b) {
-			int x = 0;
-			for (int i = 0; i < N; ++i) {
-				x ^= H[i];
-			}
-			hm = min(hm, x);
-		}
-		if (h < hm) {
-			ans = (h << 15) | lm;
-		} else if (l < lm) {
-			ans = (hm << 15) | l;
-		} else {
-			ans = (hm << 15) | lm;
-		}
-	}
+
+	int ans = getmin(1 << 29, A);
+
 	cout << ans << endl;
 	return 0;
 }
