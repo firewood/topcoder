4be0f00ee78c79d974bfeab37a41258db87c4c7d
firewood <karamaki@gmail.com>
2016-02-14 21:08:40 +0900

    translate

--- srm_6xx/srm_681/CoinFlipsDiv2.cpp
+++ srm_6xx/srm_681/CoinFlipsDiv2.cpp
@@ -2,79 +2,9 @@
 /*
 SRM 681 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-You have n coins, labeled 0 through n-1.
-Each of the coins shows either heads or tails.
-You are given the states of all coins in the string state with n characters.
-For each i, state[i] is 'H' if coin i shows heads, or 'T' if it shows tails.
-
-The coins are laid out in a row, ordered from coin 0 to coin n-1.
-A coin is called interesting if it differs from at least one of its neighbors.
-(For example, a coin that shows heads is interesting if at least one of its neighbors shows tails.)
-Return the number of interesting coins.
-
-
-DEFINITION
-Class:CoinFlipsDiv2
-Method:countCoins
-Parameters:string
-Returns:int
-Method signature:int countCoins(string state)
-
-
-NOTES
--The value of n is not given explicitly. Instead, you can determine it as the number of characters in state.
-
-
-CONSTRAINTS
--state will have between 1 and 50 elements, inclusive.
--Each character of state will be either 'H' or 'T'.
-
-
-EXAMPLES
-
-0)
-"HT"
-
-Returns: 2
-
-Coin 0 is interesting because it shows heads and its only neighbor shows tails.
-Similarly, coin 1 is interesting because it shows tails and its only neighbor shows heads.
-Thus, the answer is 2.
-
-
-1)
-"T"
-
-Returns: 0
-
-In this test case there is a single coin.
-It has no neighbors, and therefore it has no different neighbors.
-This means that the coin is not interesting.
-
-
-2)
-"HHH"
-
-Returns: 0
-
-None of these coins are interesting, because each of them is only adjacent to coins that show the same side.
-
-
-3)
-"HHTHHH"
-
-Returns: 3
-
-Here, the three interesting coins are coins 1, 2, and 3.
-(Remember that the indices are 0-based.)
-
-
-4)
-"HHHTTTHHHTTTHTHTH"
-
-Returns: 12
+問題
+-N枚のコインが左右に並んでいて、裏か表の状態が与えられる
+-左か右の隣のコインの状態と異なるコインの枚数を求める
 
 */
 // END CUT HERE
