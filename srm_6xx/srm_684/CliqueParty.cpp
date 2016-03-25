193269e9d0e3653a3e44d9d6e74650ee32c438a1
firewood <karamaki@gmail.com>
2016-03-26 00:21:34 +0900

    translate

--- srm_6xx/srm_684/CliqueParty.cpp
+++ srm_6xx/srm_684/CliqueParty.cpp
@@ -2,78 +2,11 @@
 /*
 SRM 684 Div1 Easy (250)
 
-PROBLEM STATEMENT
-A set of positive integers is called k-smooth if each pair (A,B) of elements of the set satisfies A <= k * B.
-For example, the set {3,4,7,9} is 3-smooth but the set {30,60,100} is not: 100 is strictly more than 3 * 30.
-
-Hero likes some sets of integers.
-In order to determine whether he likes a set S, Hero uses the following procedure:
-
-Form a new set D of pairwise differences of elements in S. In other words: for each pair of elements A, B in S, put the number |A-B| into D.
-Hero likes the original set S if and only if the new set D is k-smooth.
-
-
-You are given a vector <int> a containing a set of distinct integers.
-You are also given the int k Hero uses while determining whether he likes a set of integers.
-
-Select the largest subset of a Hero likes.
-Return the number of elements in that subset.
-
-
-DEFINITION
-Class:CliqueParty
-Method:maxsize
-Parameters:vector <int>, int
-Returns:int
-Method signature:int maxsize(vector <int> a, int k)
-
-
-CONSTRAINTS
--Number of elements in a will be between 2 and 50, inclusive.
--Each elements in a will be between 1 and 1,000,000,000, inclusive.
--All elements in a will be distinct.
--k will be between 1 and 1,000,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,2,3}
-2
-
-Returns: 3
-
-Hero likes the entire set a.
-
-
-1)
-{1,2,3}
-1
-
-Returns: 2
-
-Here, Hero does not like the entire set a, but he does like, for example, the subset {1,3}.
-
-
-2)
-{4,10,5,6}
-2
-
-Returns: 3
-
-
-3)
-{1,2,3,4,5,6}
-3
-
-Returns: 4
-
-
-4)
-{10,9,25,24,23,30}
-7
-
-Returns: 4
+問題
+-数値の集合Sが与えられる
+-任意の要素A,Bが常にA <= k×Bを満たすとき、良い集合とする
+-Sの部分集合Xの任意の要素A,Bの差|A-B|からなる集合をDとする
+-Dが良い集合となるXの要素数の最大値を求める
 
 */
 // END CUT HERE
