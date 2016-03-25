97f1b57bf9d766cd86b920f3153cbfaa829f8970
firewood <karamaki@gmail.com>
2016-03-25 23:32:29 +0900

    delete unnecessary code

--- yukicoder_2xx/268.cpp
+++ yukicoder_2xx/268.cpp
@@ -8,8 +8,6 @@ int main() {
 	sort(L, L + 3);
 	sort(R, R + 3);
 	int ans = (L[0] + L[1]) * R[2] + (L[0] + L[2]) * R[1] + (L[1] + L[2]) * R[0];
-	ans = min(ans, (L[0] + L[1]) * R[0] + (L[0] + L[2]) * R[2] + (L[1] + L[2]) * R[1]);
-	ans = min(ans, (L[0] + L[1]) * R[0] + (L[0] + L[2]) * R[1] + (L[1] + L[2]) * R[2]);
 	cout << ans * 2 << endl;
 	return 0;
 }
