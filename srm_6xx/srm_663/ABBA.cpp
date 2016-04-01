7851704f458203bd68d98fd33c40b4d85cd463ca
firewood <karamaki@gmail.com>
2016-04-02 02:55:42 +0900

    translate

--- srm_6xx/srm_663/ABBA.cpp
+++ srm_6xx/srm_663/ABBA.cpp
@@ -2,83 +2,10 @@
 /*
 SRM 663 Div2 Medium (500)
 
-PROBLEM STATEMENT
-One day, Jamie noticed that many English words only use the letters A and B.
-Examples of such words include "AB" (short for abdominal), "BAA" (the noise a sheep makes), "AA" (a type of lava), and "ABBA" (a Swedish pop sensation).
-
-Inspired by this observation, Jamie created a simple game.
-You are given two strings: initial and target.
-The goal of the game is to find a sequence of valid moves that will change initial into target.
-There are two types of valid moves:
-
-Add the letter A to the end of the string.
-Reverse the string and then add the letter B to the end of the string.
-
-Return "Possible" (quotes for clarity) if there is a sequence of valid moves that will change initial into target.
-Otherwise, return "Impossible".
-
-
-DEFINITION
-Class:ABBA
-Method:canObtain
-Parameters:string, string
-Returns:string
-Method signature:string canObtain(string initial, string target)
-
-
-CONSTRAINTS
--The length of initial will be between 1 and 999, inclusive.
--The length of target will be between 2 and 1000, inclusive.
--target will be longer than initial.
--Each character in initial and each character in target will be either 'A' or 'B'.
-
-
-EXAMPLES
-
-0)
-"B"
-"ABBA"
-
-Returns: "Possible"
-
-Jamie can perform the following moves:
-
-Initially, the string is "B".
-Jamie adds an 'A' to the end of the string. Now the string is "BA".
-Jamie reverses the string and then adds a 'B' to the end of the string. Now the string is "ABB".
-Jamie adds an 'A' to the end of the string. Now the string is "ABBA".
-
-Since there is a sequence of moves which starts with "B" and creates the string "ABBA", the answer is "Possible".
-
-
-1)
-"AB"
-"ABB"
-
-Returns: "Impossible"
-
-The only strings of length 3 Jamie can create are "ABA" and "BAB".
-
-
-2)
-"BBAB"
-"ABABABABB"
-
-Returns: "Impossible"
-
-
-3)
-"BBBBABABBBBBBA"
-"BBBBABABBABBBBBBABABBBBBBBBABAABBBAA"
-
-Returns: "Possible"
-
-
-4)
-"A"
-"BB"
-
-Returns: "Impossible"
+問題
+-文字列initialが与えられる
+-文字Aを末尾に加えるか、文字列を反転して末尾に文字Bを加える
+-文字列targetにできるかどうかを求める
 
 */
 // END CUT HERE
