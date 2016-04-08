8f88c763119973e129aa86d681fd946c31815941
firewood <karamaki@gmail.com>
2016-04-09 01:51:41 +0900

    solved

--- gcj_2015/QR_A.clj
+++ gcj_2015/QR_A.clj
@@ -4,7 +4,12 @@
   (def input (split (read-line) #"\s"))
   (def n (read-string (first input)))
   (def s (map read-string (split (second input) #"")))
-  n)
+  (defn add[level n sum s]
+    (def p (max 0 (- level n)))
+    (if (empty? s)
+      sum
+      (add (+ level 1) (+ n (first s) p) (+ sum p) (rest s))))
+  (add 0 0 0 s))
 
 (defn output-result[t ans] (println (format "Case #%d: %d" t ans)))
 (def T (read-string (read-line)))
