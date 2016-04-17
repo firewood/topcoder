7e77bc40a6308d2f2d48738fd1c7218229dfb9f6
firewood <karamaki@gmail.com>
2016-04-17 12:11:24 +0900

    simplified

--- gcj_2016/QR_A.clj
+++ gcj_2016/QR_A.clj
@@ -1,21 +1,32 @@
 ; Google Code Jam 2016 Qualification Round
 ; Problem A. Counting Sheep
 
-(use '[clojure.string :only (join split)])
+(use '[clojure.string :only (join split includes?)])
 
-(defn gen[n] (iterate (fn[x] (+ x n)) n))
+; generates [N N*2 N*3, ...]
+(defn gen[n]
+  (iterate (fn[x] (+ x n)) n))
 
+; check if digit D is in N
 (defn check[d n]
-  (some (fn[x] (= x d)) (map read-string (split (str n) #""))))
+  (includes? (str n) (str d)))
 
-(defn findFirst[d n] (first (filter (fn[x] (check d x)) (gen n))))
+; first N contains digit D
+(defn find-first[d n]
+  (first
+    (filter (fn[x] (check d x)) (gen n))))
 
-(defn solve[]
-  (def n (read-string (read-line)))
+(defn solve[n]
   (if (= n 0)
     "INSOMNIA"
-    (str (apply max (map (fn[d] (findFirst d n)) (range 0 10))))))
+    (apply max (map (fn[d] (find-first d n)) (range 0 10)))))
 
-(defn output-result[t ans] (println (format "Case #%d: %s" t ans)))
+(defn output-result[t ans]
+  (println (str "Case #" t ": " ans)))
+
 (def T (read-string (read-line)))
-(doall (map (fn[t] (output-result (+ t 1) (solve))) (range 0 T)))
+(loop [t 1]
+  (when (<= t T)
+    (def n (read-string (read-line)))
+    (output-result t (solve n))
+    (recur (inc t))))
