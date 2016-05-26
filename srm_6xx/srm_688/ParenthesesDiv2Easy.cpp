df98c57133b496223e9a86d117794435c23f1b24
firewood <karamaki@gmail.com>
2016-05-27 00:02:09 +0900

    translate

--- srm_6xx/srm_688/ParenthesesDiv2Easy.cpp
+++ srm_6xx/srm_688/ParenthesesDiv2Easy.cpp
@@ -2,85 +2,13 @@
 /*
 SRM 688 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Correct parentheses sequences can be defined recursively as follows:
-
-The empty string "" is a correct sequence.
-If "X" and "Y" are correct sequences, then "XY" (the concatenation of X and Y) is a correct sequence.
-If "X" is a correct sequence, then "(X)" is a correct sequence.
-Each correct parentheses sequence can be derived using the above rules.
-
-Examples of correct parentheses sequences include "", "()", "()()()", "(()())", and "(((())))".
-
-We can define the depth of a correct parentheses sequence recursively as follows:
-
-The empty string "" has depth 0.
-If the depth of "X" is x and the depth of "Y" is y then the depth of "XY" is max(x,y).
-If the depth of "X" is x then the depth of "(X)" is x+1.
-
-For example, the depth of "(((())))" is 4, the depth of "()()()" is 1, and the depth of "(())()" is 2.
-
-Note that the depth of each correct parentheses sequence is uniquely defined using the above rules.
-For example, when evaluating the depth of "()()()" it does not matter whether we take X = "()" and Y = "()()" or we take X = "()()" and Y = "()", the result will be the same in both cases.
-
-Given a string s that is a correct parentheses sequence, calculate and return the depth of s.
-
-
-DEFINITION
-Class:ParenthesesDiv2Easy
-Method:getDepth
-Parameters:string
-Returns:int
-Method signature:int getDepth(string s)
-
-
-CONSTRAINTS
--s will contain between 2 and 50 characters, inclusive.
--Each character in s will be '(' or ')'.
--s will be a correct parentheses sequence.
-
-
-EXAMPLES
-
-0)
-"(())"
-
-Returns: 2
-
-
-The depth of "" is 0.
-Therefore, the depth of "()" is 1.
-Next, the depth of "(())" is the depth of "()" plus 1, which makes it 1+1 = 2.
-
-
-1)
-"()()()"
-
-Returns: 1
-
-
-The depth of "()()" is the maximum of the depth of "()" and the depth of "()". In other words, it is max(1,1) = 1.
-The depth of "()()()" is the maximum of the depth of "()()" and the depth of "()". Hence, this also equals max(1,1) = 1.
-
-
-2)
-"(())()"
-
-Returns: 2
-
-The answer is max(2, 1) = 2.
-
-
-3)
-"((())())(((())(()))())"
-
-Returns: 4
-
-
-4)
-"()"
-
-Returns: 1
+–â‘è
+-Š‡ŒÊ‚¾‚¯‚Ì•¶š—ñ‚ª—^‚¦‚ç‚ê‚é
+-‹ó•¶š—ñ‚Ì[‚³‚ğ0‚Æ‚·‚é
+-"()"‚Ì[‚³‚ğ1‚Æ‚·‚é
+-"(X)"‚Ì[‚³‚ğX‚Ì[‚³+1‚Æ‚·‚é
+-"XY"‚Ì[‚³‚ğX‚ÆY‚Ì[‚³‚ÌÅ‘å’l‚Æ‚·‚é
+-•¶š—ñs‚Ì[‚³‚ğ‹‚ß‚é
 
 */
 // END CUT HERE
