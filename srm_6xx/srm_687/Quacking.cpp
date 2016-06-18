285c6bff6d27d130016757b3ed896fffb873dec4
firewood <karamaki@gmail.com>
2016-06-19 00:35:53 +0900

    translate

--- srm_6xx/srm_687/Quacking.cpp
+++ srm_6xx/srm_687/Quacking.cpp
@@ -2,92 +2,10 @@
 /*
 SRM 687 Div2 Medium (500)
 
-PROBLEM STATEMENT
-
-Ducks have started mysteriously appearing in your room.
-All ducks make the same sound: "quack".
-Each duck makes the sound one or more times, one after another.
-For example, valid sounds for a single duck are "quack", "quackquackquackquack", "quackquack", etc.
-
-You have lost count of how many ducks are in your room.
-The ducks are quacking, and the sounds of their quacks are getting mixed up.
-You have recorded the sounds, obtaining a string of characters.
-When you later listened to the recording, you realized that the quacking of each individual duck forms a (not necessarily contiguous) subsequence of the recording.
-You also noticed that each letter in the recording belongs to exactly one duck.
-For example, if there were two ducks, you may have recorded "quqacukqauackck".
-
-You are given a string s that contains an arbitrary recording.
-Find and return the smallest number of ducks that could have produced the recording.
-Note that it is possible that the given recording is not a valid recording of quacking ducks.
-In such case, return -1.
-
-
-DEFINITION
-Class:Quacking
-Method:quack
-Parameters:string
-Returns:int
-Method signature:int quack(string s)
-
-
-CONSTRAINTS
--s will have between 5 and 2,500 characters, inclusive.
--Each character of s will be 'q', 'u', 'a', 'c', or 'k'.
-
-
-EXAMPLES
-
-0)
-"quqacukqauackck"
-
-Returns: 2
-
-This is the same as the one from the problem statement.
-
-
-1)
-"kcauq"
-
-Returns: -1
-
-A backward duck is not a real duck.
-
-
-2)
-"quackquackquackquackquackquackquackquackquackquack"
-
-Returns: 1
-
-A single duck can make arbitrarily many quack sounds.
-
-
-3)
-"qqqqqqqqqquuuuuuuuuuaaaaaaaaaacccccccccckkkkkkkkkk"
-
-Returns: 10
-
-
-4)
-"quqaquuacakcqckkuaquckqauckack"
-
-Returns: 3
-
-This is one possible solution with 3 ducks.
-
-Mixed: quqaquuacakcqckkuaquckqauckack
-Duck1: ____q_u__a___ck_______________
-Duck2: __q__u_ac_k_q___ua__ckq_u__ack
-Duck3: qu_a_______c___k__qu___a_ck___
-
-Here, we can verify that each letter comes from exactly one duck.
-
-
-5)
-"quackqauckquack"
-
-Returns: -1
-
-Note that the second quack is misspelled.
+問題
+-アヒルは"quack"と鳴く
+-何匹かの鳴き声が混ざった文字列が与えられる
+-最低何匹いるかを求める
 
 */
 // END CUT HERE
