f279c4343c39831d7020eea3201b0fe6ae00a953
firewood <karamaki@gmail.com>
2016-07-02 01:27:17 +0900

    translate

--- srm_6xx/srm_693/BiconnectedDiv1.cpp
+++ srm_6xx/srm_693/BiconnectedDiv1.cpp
@@ -2,76 +2,12 @@
 /*
 SRM 693 Div1 Easy (250)
 
-PROBLEM STATEMENT
-A 2-edge-connected graph is a connected undirected graph with the following property:
-for any two distinct vertices u,v and for any edge e there exists a path from u to v that does not contain e.
-
-You are given a graph G with n vertices numbered 0 through n-1, where n is not less than 3.
-The graph has a very specific structure.
-For each i between 0 and n-2, inclusive, vertices i and i+1 are connected by an edge with weight w1[i].
-Additionally, for each i between 0 and n-3, inclusive, vertices i and i+2 are connected by an edge with weight w2[i].
-It is easy to verify that this graph is 2-edge-connected.
-
-Note that some of the edge weights may be zeros.
-An edge with weight zero is still present in the graph, just like any other edge.
-
-You may erase some edges of the graph G.
-If you do, you must do it in such a way that the graph remains 2-edge-connected.
-Your goal is to minimize the sum of weights of edges that remain in G.
-
-You are given the vector <int>s w1 and w2 that describe the graph G.
-Compute and return the smallest possible sum of weights of a graph that can be produced in the way described above.
-
-
-DEFINITION
-Class:BiconnectedDiv1
-Method:minimize
-Parameters:vector <int>, vector <int>
-Returns:int
-Method signature:int minimize(vector <int> w1, vector <int> w2)
-
-
-CONSTRAINTS
--n will be between 3 and 100, inclusive.
--w1 will contain exactly n-1 elements.
--w2 will contain exactly n-2 elements.
--Each element of w1 and w2 will be between 0 and 10,000, inclusive.
-
-
-EXAMPLES
-
-0)
-{1,2}
-{3}
-
-Returns: 6
-
-There are three vertices and three edges (0,1), (1,2), (2,0). If you erase edge (0,1), then any path from 0 to 1 has to go through edge (0,2). And if you erase two or more edges, the graph will become disconnected. So you cannot erase anything.
-
-
-1)
-{3,0,4}
-{1,2}
-
-Returns: 10
-
-An optimal solution is to keep all the edges.
-
-
-2)
-{3,3,3,3}
-{3,6,3}
-
-Returns: 18
-
-An optimal solution is to erase edge (1,3).
-
-
-3)
-{7243,7525,8467,6351,9453,8456,8175,5874,6869,7400,6438,8926,6876}
-{2642,1743,3546,4100,2788,3019,2678,1935,1790,2674,3775,1920}
-
-Returns: 46729
+問題
+-任意の頂点u,vについて、任意の辺eを取り除いてもuからvに到達可能なときを、2辺連結グラフと呼ぶ
+-N個の頂点が与えられる
+-頂点iとi+1には重みw1[i]の辺がある
+-頂点iとi+2には重みw1[i]の辺がある
+-2辺連結グラフを維持しながら、辺を除去するとき、重みの合計の最小値を求める
 
 */
 // END CUT HERE
