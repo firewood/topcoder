9e751f60941e9df6391858b1b560728023eca5cd
firewood <karamaki@gmail.com>
2016-07-23 00:10:47 +0900

    translate

--- srm_6xx/srm_695/BearNSWE.cpp
+++ srm_6xx/srm_695/BearNSWE.cpp
@@ -2,93 +2,10 @@
 /*
 SRM 695 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Bear Limak is going to walk to the store.
-On his way to the store he likes to visit his friends and stop at various other interesting places.
-On his way back he is carrying heavy bags and therefore he walks straight home.
-
-Limak's walk from his home to the store consists of M parts.
-In the i-th part (0-based index) Limak will walk a[i] meters in the direction indicated by the character dir[i].
-That character is one of 'N', 'S', 'W', 'E', denoting North, South, West, and East, respectively.
-
-On his way back home, Limak follows a straight line from the store to his home.
-
-You are given the description of Limak's way to the store:
-the vector <int> a with M integers and the string dir with M characters.
-Compute and return the total distance (in meters) Limak will walk on his way to the store and back.
-Note that the correct answer may be non-integer (see the first example below).
-
-
-DEFINITION
-Class:BearNSWE
-Method:totalDistance
-Parameters:vector <int>, string
-Returns:double
-Method signature:double totalDistance(vector <int> a, string dir)
-
-
-NOTES
--Your return value must have relative error smaller than 1e-6. In other words, your returned value x will be accepted only if abs(x - ans) ? ans * 10-6, where ans denotes the exact correct answer.
-
-
-CONSTRAINTS
--M will be between 1 and 50, inclusive.
--a will have exactly M elements.
--dir will have exactly M characters.
--Each element in a will be between 1 and 50, inclusive.
--Each character in dir will be one of 'N', 'S', 'W', 'E'.
-
-
-EXAMPLES
-
-0)
-{1,3,3}
-"NES"
-
-Returns: 10.60555127546399
-
-In total, Limak's walk consists of four parts:
-
-He goes 1 meter North.
-3 meters East.
-3 meters South.
-He goes straight to his home.
-The distance is sqrt(2*2+3*3) = sqrt(13) = 3.6055512755.
-
-The total distance is 1 + 3 + 3 + 3.6055512755 = 10.6055512755.
-
-
-1)
-{10,10,10,10}
-"NWSE"
-
-Returns: 40.0
-
-In this test case the store is located at the same place as Limak's home.
-Thus, the length of Limak's walk from the store back home is 0 meters.
-
-
-2)
-{10,10,10,15,8,20,5}
-"NEWEWWE"
-
-Returns: 90.8062484748657
-
-It's possible that Limak visits some places more than once.
-
-
-3)
-{42}
-"E"
-
-Returns: 84.0
-
-
-4)
-{10,40,40}
-"NSE"
-
-Returns: 140.0
+問題
+-四方向いずれかの移動方向と、移動距離の配列が与えられる
+-与えられた方向に移動したのち、まっすぐ最初の位置に戻る
+-総移動距離を求める
 
 */
 // END CUT HERE
