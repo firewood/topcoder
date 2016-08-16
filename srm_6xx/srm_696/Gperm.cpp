239e963c4f982432e6b8d3777c1c9667566c7850
firewood <karamaki@gmail.com>
2016-08-17 00:37:59 +0900

    translate

--- srm_6xx/srm_696/Gperm.cpp
+++ srm_6xx/srm_696/Gperm.cpp
@@ -2,90 +2,12 @@
 /*
 SRM 696 Div1 Easy (300)
 
-PROBLEM STATEMENT
-Hero has an undirected multigraph.
-(Each pair of vertices can be connected by arbitrarily many undirected edges. There may also be arbitrarily many self-loops at each vertex.)
-Hero's graph has exactly 50 vertices, labeled 0 through 49.
-
-The graph has at most 20 edges.
-You are given the list of those edges: the vector <int>s x and y.
-For each valid i, there is an edge that connects the vertices x[i] and y[i].
-(Self-loops have x[i] = y[i].)
-
-Hero needs to paint all vertices red.
-He can paint the vertices in any order he likes, one vertex at a time.
-Each time he paints a vertex he has to pay a fee.
-The fee is computed as the number of edges that already have both endpoints red.
-
-Hero wants to minimize the sum of all fees he has to pay.
-Find the order in which he should paint the vertices, and return the corresponding total fee.
-
-
-DEFINITION
-Class:Gperm
-Method:countfee
-Parameters:vector <int>, vector <int>
-Returns:int
-Method signature:int countfee(vector <int> x, vector <int> y)
-
-
-CONSTRAINTS
--x will contain between 1 and 20 elements, inclusive.
--x and y will contain the same number of elements.
--Each element in x and y will be between 0 and 49, inclusive.
-
-
-EXAMPLES
-
-0)
-{0}
-{1}
-
-Returns: 1
-
-One optimal order of painting the vertices is 49, 48, 47, ..., 3, 2, 1, 0.
-The first 49 times the fee is 0.
-After Hero paints the last vertex, the last fee is 1.
-
-
-1)
-{0,1}
-{1,2}
-
-Returns: 2
-
-One optimal order of painting these vertices is 49, 48, ..., 5, 4, 3, 0, 2, 1. The last three fees will be 0, 0, and 2.
-
-
-2)
-{4,7,7}
-{7,4,4}
-
-Returns: 3
-
-
-3)
-{0,0,1}
-{1,2,2}
-
-Returns: 4
-
-Hero should first paint the 47 isolated vertices and then the remaining three vertices in any order.
-Regardless of the order in which Hero paints the last three vertices, the last three fees will be 0, 1, and 3.
-
-
-4)
-{7,8,9}
-{4,5,6}
-
-Returns: 6
-
-
-5)
-{45,28,42,5,27,27,42,36,14,27,19,42,24,27,8,31,24,27,14,28}
-{45,27,45,8,34,34,28,0,11,42,24,19,14,31,45,42,14,24,28,27}
-
-Returns: 53
+問題
+-50個の頂点がある
+-1ターンに1つずつ塗っていく
+-20本以内で辺が与えられる
+-辺の両辺が塗られているとき、1ターンごとにコストが1発生する
+-コストの総量の最小値を求める
 
 */
 // END CUT HERE
