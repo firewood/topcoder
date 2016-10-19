a480a34a83f503b50fce89a0e113909f06191c39
firewood <karamaki@gmail.com>
2016-10-20 00:18:09 +0900

    testing

---
+++ hackerrank/30days/27.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    cout << "5" << endl;
+    cout << "4 3" << endl;
+    cout << "-1 0 4 2" << endl;
+    cout << "5 2" << endl;
+    cout << "0 -1 2 1 4" << endl;
+    cout << "7 6" << endl;
+    cout << "2 0 -1 1 1 1 1" << endl;
+    cout << "3 1" << endl;
+    cout << "-1 0 4" << endl;
+    cout << "6 4" << endl;
+    cout << "0 -1 1 4 5 6" << endl;
+    return 0;
+}
