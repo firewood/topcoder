e161703377934db3bd5bf0ae14d88c71f8e3d8d1
firewood <karamaki@gmail.com>
2016-11-24 01:00:06 +0900

    translate

--- srm_7xx/srm_702/GridSortMax.cpp
+++ srm_7xx/srm_702/GridSortMax.cpp
@@ -2,126 +2,13 @@
 /*
 SRM 702 Div1 Easy (300)
 
-PROBLEM STATEMENT
-
-Charlie has a grid of n rows by m columns.
-The rows are numbered 0 through n-1 from top to bottom.
-The columns are numbered 0 through m-1 from left to right.
-
-Each cell of the grid contains a positive integer.
-The integers in Charlie's grid are a permutation of the numbers 1 through n*m.
-(I.e., each of these numbers occurs in the grid exactly once.)
-
-Given a grid, its value list is a sequence constructed by listing all values in the grid in row major order.
-That is, we first list the values in row 0 from left to right, then the values in row 1 from left to right, and so on.
-
-You are given the ints n and m: the dimensions of Charlie's grid.
-You are also given a vector <int> grid: the value list for Charlie's grid.
-(Formally, grid[i*m+j] is the value stored in row i, column j of the grid.)
-
-In Charlie's eyes, the most beautiful of all grids is the sorted grid: the grid whose value list is the ordered ordered sequence {1,2,3,...,n*m}.
-
-Given a grid, its similarity string is a string of zeroes and ones that describes the similarity between that particular grid and the sorted grid. More precisely:
-
-The similarity string is a string of length n*m.
-For each i, character i of the similarity string is '1' if both grids have the same i-th element in their value lists, and it is '0' if those values differ. (All indices in the previous sentence are 0-based.)
-
-For example, suppose n=2 and m=3. The sorted grid has the value list {1,2,3,4,5,6}, and its similarity string is "111111". Another possible grid with these dimensions has the value list {1,5,2,4,3,6}. The similarity string for this grid is "100101".
-
-Charlie can modify his grid in two ways: He may swap any two rows, and he may swap any two columns.
-He wants to use these operations to obtain a grid with the lexicographically largest possible similarity string.
-Find and return that string.
-
-
-DEFINITION
-Class:GridSortMax
-Method:findMax
-Parameters:int, int, vector <int>
-Returns:string
-Method signature:string findMax(int n, int m, vector <int> grid)
-
-
-CONSTRAINTS
--n,m will be between 1 and 50, inclusive.
--grid will be a permutation of [1,...,n*m]
-
-
-EXAMPLES
-
-0)
-2
-2
-{
- 1,2,
- 3,4
-}
-
-Returns: "1111"
-
-Charlie's grid is the sorted grid.
-Its similarity string is therefore "1111".
-Charlie should make no changes to the grid, as this is already the lexicographically largest of all possible strings.
-
-
-1)
-2
-2
-{
- 2,1,
- 3,4
-}
-
-Returns: "1100"
-
-The similarity string for Charlie's current grid is "0011", as the values 3 and 4 are in the same places as in the sorted grid.
-Charlie should swap the two columns.
-That swap will produce a grid with the similarity string "1100": now the values 1 and 2 are in their correct places but the values 3 and 4 aren't.
-
-
-2)
-2
-2
-{
- 4,2,
- 3,1
-}
-
-Returns: "1001"
-
-
-3)
-1
-10
-{10,6,2,3,5,7,1,9,4,8}
-
-Returns: "1111111111"
-
-
-4)
-3
-5
-{
- 5,2,10,7,9,
- 3,4,14,11,1,
- 15,12,6,8,13
-}
-
-Returns: "101100100100000"
-
-
-5)
-6
-2
-{
- 3,9,
- 5,1,
- 10,6,
- 2,7,
- 8,11,
- 12,4
-}
-
-Returns: "100000101010"
+問題
+-N行M列の升目がある
+-1行目の1列目に1,2,3,...、2行目にM+1,M+2,M+3,...と番号をつける
+-N行M列に並んだ数値が与えられる
+-各升目の数値と番号が一致するときを1、そうでないときを0とする
+-それら一致したかどうかの値をすべて結合したものを類似性文字列とする
+-任意の行または任意の列が交換できるとき、辞書順最大の類似性文字列を求める
 
 */
 // END CUT HERE
