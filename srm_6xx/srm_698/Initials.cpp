a9b6d0f8cf7cebfb7f95c572139452d62ac74956
firewood <karamaki@gmail.com>
2016-11-26 00:27:14 +0900

    translate

--- srm_6xx/srm_698/Initials.cpp
+++ srm_6xx/srm_698/Initials.cpp
@@ -2,68 +2,9 @@
 /*
 SRM 698 Div2 Easy (250)
 
-PROBLEM STATEMENT
-When dealing with long names and phrases, we sometimes use the initial letters of words to form its acronym.
-For example, we use "JFK" instead of "John Fitzgerald Kennedy", "lgtm" instead of "looks good to me", and so on.
-
-You are given a string name.
-This string contains a phrase: one or more words separated by single spaces.
-Please compute and return the acronym of this phrase.
-(As above, the acronym should consist of the first letter of each word, in order.)
-
-
-DEFINITION
-Class:Initials
-Method:getInitials
-Parameters:string
-Returns:string
-Method signature:string getInitials(string name)
-
-
-CONSTRAINTS
--name will contain between 1 and 50 characters, inclusive.
--Each character in s will be a space or a lowercase English letter ('a' - 'z').
--The first and last character in s will not be a space.
--No two continuous spaces can appear in s.
-
-
-EXAMPLES
-
-0)
-"john fitzgerald kennedy"
-
-Returns: "jfk"
-
-There are three words: "john", "fitzgerald" and "kennedy".
-Their first letters are 'j', 'f' and 'k'.
-The correct return value is their concatenation: the string "jfk".
-Note that all letters in our problem are in lowercase.
-
-
-1)
-"single"
-
-Returns: "s"
-
-There is only one word: "single". Its acronym has a single letter.
-
-
-2)
-"looks good to me"
-
-Returns: "lgtm"
-
-
-3)
-"single round match"
-
-Returns: "srm"
-
-
-4)
-"a aa aaa aa a bbb bb b bb bbb"
-
-Returns: "aaaaabbbbb"
+問題
+-単語を空白で区切った文字列が与えられる
+-各単語の頭文字を結合したものを求める
 
 */
 // END CUT HERE
