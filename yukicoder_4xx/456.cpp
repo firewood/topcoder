f587d55d4046b4e3cf03b8c87f87e2c2fb64b1fb
firewood <karamaki@gmail.com>
2016-12-09 00:31:27 +0900

    yukicoder #456

--- yukicoder_4xx/456.cpp
+++ yukicoder_4xx/456.cpp
@@ -6,17 +6,19 @@
 using namespace std;
 
 double binary_search(int a, int b, double t) {
-	double left = 1.0e-10, right = t, prev = 1.0e+10;
-	if (b >= 1) {
-		left = 1.0;
-	}
-	if (b == 0) {
-		left = exp(log(t) / a);
-	} else if (a == 0 && b == 1) {
-		left = exp(t);
+	double ans;
+	if (a == 0) {
+		ans = exp(pow(t, 1.0 / b));
+	} else if (b == 0) {
+		ans = pow(t, 1.0 / a);
 	} else {
-		right = max(2.0, t * 100);
+		for (int i = 2; i <= 10; ++i) {
+			while (a % i == 0 && b % i == 0) {
+				a /= i, b /= i, t = pow(t, 1.0 / i);
+			}
+		}
 
+		double left = 1.0, right = max(2.0, t * 100), prev = 1.0e+10;
 		for (int j = 0; j < 100; ++j) {
 			double mid = (left + right) / 2;
 			double x = 1, y = 1, z = log(mid);
@@ -51,18 +53,19 @@ double binary_search(int a, int b, double t) {
 				right = mid;
 			}
 			if (n > prev) {
-				if (n - prev < 1.0e-10) {
+				if (n - prev < 2.0e-11) {
 					break;
 				}
 			} else {
-				if (prev - n < 1.0e-10) {
+				if (prev - n < 2.0e-11) {
 					break;
 				}
 			}
 			prev = n;
 		}
+		ans = left;
 	}
-	return left;
+	return ans;
 }
 
 int main(int argc, char *argv[]) {
@@ -71,7 +74,7 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < m; ++i) {
 		int a, b;
 		double t;
-		cin >> a >> b >> t;
+		scanf("%d %d %lf", &a, &b, &t);
 		printf("%.10f\n", binary_search(a, b, t));
 	}
 	return 0;
