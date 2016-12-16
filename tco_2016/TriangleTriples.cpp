5d46ad668a7a88f8dcf1583ddddd1828e6a68474
firewood <karamaki@gmail.com>
2016-12-17 02:05:44 +0900

    translate

--- tco_2016/TriangleTriples.cpp
+++ tco_2016/TriangleTriples.cpp
@@ -2,74 +2,9 @@
 /*
 TCO 2016 Round 2B Easy (300)
 
-PROBLEM STATEMENT
-An ordered triple (a, b, c) is called a triangle triple if a, b, c are positive integers such that there is a triangle with side lengths a, b, c and a positive area.
-
-For example, (4, 5, 6) and (5, 4, 6) are two different triangle triples.
-On the other hand, (1, 2, 3) and (47, 5, 5) are not triangle triples.
-
-You are given the ints A, B, and C.
-Let X be the number of triangle triples (a, b, c) such that 1 <= a <= A, 1 <= b <= B, and 1 <= c <= C.
-Compute and return the value (X modulo 1,000,000,007).
-
-DEFINITION
-Class:TriangleTriples
-Method:count
-Parameters:int, int, int
-Returns:int
-Method signature:int count(int A, int B, int C)
-
-
-CONSTRAINTS
--A will be between 1 and 1,000,000,000, inclusive.
--B will be between 1 and 1,000,000,000, inclusive.
--C will be between 1 and 1,000,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-1
-10
-20
-
-Returns: 10
-
-We have 10 such triples: (1,1,1), (1,2,2), (1,3,3), ..., (1,10,10).
-
-
-1)
-2
-2
-1000000000
-
-Returns: 6
-
-This time we have: (1,1,1), (1,2,2), (2,1,2), (2,2,1), (2,2,2) and (2,2,3).
-
-
-2)
-10
-10
-10
-
-Returns: 505
-
-
-3)
-1
-1
-1
-
-Returns: 1
-
-
-4)
-123456789
-987654321
-555555555
-
-Returns: 64296241
+問題
+-三角形の辺の長さになりうる3つの数を三角形数とする
+-1以上A以下のa、1以上B以下のb、1以上C以下のcの三つの組み合わせの三角形数の総数を求める
 
 */
 // END CUT HERE
