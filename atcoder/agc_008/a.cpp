b3ce623867ab415aa1aa7afb2764e6eb4fbafc3f
firewood <karamaki@gmail.com>
2016-12-26 00:59:56 +0900

    AGC #008 A

---
+++ atcoder/agc_008/a.cpp
@@ -0,0 +1,27 @@
+// A. 
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	cin >> x >> y;
+	int ans = 1 << 30;
+	for (int a = -1; a <= 1; a += 2) {
+		for (int b = -1; b <= 1; b += 2) {
+			int cnt = (a < 0) + (b < 0);
+			int p = x * a, q = y * b;
+			if (p <= q) {
+				ans = min(ans, cnt + q - p);
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
