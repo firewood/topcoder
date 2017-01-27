ada7282d33b7d8b2bf41c52e4ebd343c5e263c73
firewood <karamaki@gmail.com>
2017-01-27 22:25:22 +0900

    Translate

--- srm_7xx/srm_703/AlternatingString.cpp
+++ srm_7xx/srm_703/AlternatingString.cpp
@@ -2,65 +2,10 @@
 /*
 SRM 703 Div2 Easy (250)
 
-PROBLEM STATEMENT
-A string of zeros and ones is called an alternating string if no two adjacent characters are the same.
-Examples of alternating strings: "1", "10101", "0101010101".
-
-You are given a string s.
-Each character of s is a '0' or a '1'.
-Please find the longest contiguous substring of s that is an alternating string.
-Return the length of that substring.
-
-
-DEFINITION
-Class:AlternatingString
-Method:maxLength
-Parameters:string
-Returns:int
-Method signature:int maxLength(string s)
-
-
-CONSTRAINTS
--s will contain between 1 and 50 characters, inclusive.
--Each character in s will be '0' or '1'.
-
-
-EXAMPLES
-
-0)
-"111101111"
-
-Returns: 3
-
-Among all substrings, there are 5 different alternating strings: "1", "0", "10", "01", "101". The one with maximal length is "101" and the length is 3.
-
-
-1)
-"1010101"
-
-Returns: 7
-
-The string s itself is an alternating string.
-
-
-2)
-"000011110000"
-
-Returns: 2
-
-Note that a substring must be contiguous. The longest alternating substrings of this s are "01" and "10". The string "010" is not a substring of this s.
-
-
-3)
-"1011011110101010010101"
-
-Returns: 8
-
-
-4)
-"0"
-
-Returns: 1
+問題
+-隣り合う2文字が全て異なる文字列を交互文字列とする
+-0と1からなる文字列sが与えられる
+-sの部分文字列のうち交互文字列であるものの最大の長さを求める
 
 */
 // END CUT HERE
