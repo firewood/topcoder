587c30fc0c82de1242b676ecd8291de2f9bc5bb3
firewood <karamaki@gmail.com>
2017-02-05 01:32:36 +0900

    AGC #010 B

---
+++ atcoder/agc_010/b.cpp
@@ -0,0 +1,41 @@
+// A. 
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+typedef long long LL;
+
+int main(int argc, char *argv[])
+{
+	LL N, a[100000], sum = 0;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> a[i];
+		sum += a[i];
+	}
+	LL x = (N * (N + 1)) / 2, y = sum / x;
+	bool ans = (sum % x) == 0;
+	if (ans) {
+		int m;
+		for (int i = 0; i < N; ++i) {
+			if (a[i] < y || a[i] > N * y) {
+				ans = false;
+				break;
+			}
+			LL b = a[i] + (N - i) * y;
+			if (i > 0 && m != (b % N)) {
+				ans = false;
+				break;
+			} else {
+				m = b % N;
+			}
+		}
+	}
+	cout << (ans ? "YES" : "NO") << endl;
+	return 0;
+}
