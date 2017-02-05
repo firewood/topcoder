58a684efb63ea11cdaf09a38f412ab834b1d992d
firewood <karamaki@gmail.com>
2017-02-06 00:25:47 +0900

    Translate

--- srm_7xx/srm_707/Cross.cpp
+++ srm_7xx/srm_707/Cross.cpp
@@ -2,94 +2,9 @@
 /*
 SRM 707 Div2 Easy (250)
 
-PROBLEM STATEMENT
-There is a rectangular board that is divided into n rows by m columns of cells.
-Each cell is either black or white.
-You are given the description of the board in the vector <string> board.
-Each character in board represents one cell.
-More precisely, the character board[i][j] represents the cell at coordinates (row i, column j).
-The character '#' represents a black cell, the character '.' is a white cell.
-
-You want to find a cross on this board.
-A cross is a configuration of 5 black cells placed like this:
-
- #
-###
- #
-
-Formally, there is a cross centered at (x, y) if the following five cells are all black: (x, y), (x+1, y), (x-1, y), (x, y-1), and (x, y+1).
-Note that other cells adjacent to the cross may also be black, it is still a valid cross.
-
-Return "Exists" if there is at least one cross on the given board.
-Otherwise, return "Does not exist".
-Note that the return value is case-sensitive.
-
-
-DEFINITION
-Class:Cross
-Method:exist
-Parameters:vector <string>
-Returns:string
-Method signature:string exist(vector <string> board)
-
-
-CONSTRAINTS
--n, m will be between 3 and 50, inclusive.
--board will contain exactly n elements.
--Each element in board will contain exactly m characters.
--Each character in board will be either '#' or '.'.
-
-
-EXAMPLES
-
-0)
-{".##",
- "###",
- "##."}
-
-Returns: "Exist"
-
-There is a cross centered at (1,1).
-
-
-1)
-{".##",
- "###",
- "#.."}
-
-Returns: "Does not exist"
-
-This time there is no cross.
-
-
-2)
-{"######",
- "######",
- "######",
- "######",
- "######",
- "######",
- "######"}
-
-Returns: "Exist"
-
-
-3)
-{".#.#",
- "#.#.",
- ".#.#",
- "#.#."}
-
-Returns: "Does not exist"
-
-
-4)
-{".#.#",
- "###.",
- ".###",
- "#.#."}
-
-Returns: "Exist"
+問題
+-白か黒が書かれたマス目がある
+-黒で十字をなす部分があるかどうかを求める
 
 */
 // END CUT HERE
