b902247636531b0cea07e97bdaec1b4200e7434a
firewood <karamaki@gmail.com>
2017-03-09 00:21:25 +0900

    translate

--- srm_5xx/srm_582/SemiPerfectSquare.cpp
+++ srm_5xx/srm_582/SemiPerfectSquare.cpp
@@ -2,72 +2,8 @@
 /*
 SRM 582 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Magical Girl Iris loves perfect squares.
-A positive integer n is a perfect square if and only if there is a positive integer b >= 1 such that b*b = n.
-For example, 1 (=1*1), 16 (=4*4), and 169 (=13*13) are perfect squares, while 2, 54, and 48 are not.
-
-Iris also likes semi-squares.
-A positive integer n is called a semi-square if and only if there are positive integers a >= 1 and b > 1 such that a < b and a*b*b = n.
-For example, 81 (=1*9*9) and 48 (=3*4*4) are semi-squares, while 24, 63, and 125 are not.
-(Note that we require that a < b.
-Even though 24 can be written as 6*2*2, that does not make it a semi-square.)
-
-You are given a int N.
-Return "Yes" (quotes for clarity) if N is a semi-square number.
-Otherwise, return "No".
-
-
-DEFINITION
-Class:SemiPerfectSquare
-Method:check
-Parameters:int
-Returns:string
-Method signature:string check(int N)
-
-
-NOTES
--The return value is case-sensitive. Make sure that you return the exact strings "Yes" and "No".
-
-
-CONSTRAINTS
--N will be between 2 and 1000, inclusive.
-
-
-EXAMPLES
-
-0)
-48
-
-Returns: "Yes"
-
-48 can be expressed as 3 * 4 * 4. Therefore, 48 is a semi-square.
-
-
-1)
-1000
-
-Returns: "No"
-
-1000 can be represented as 10 * 10 * 10, but it doesn't match the definition of semi-perfect squares.
-
-
-2)
-25
-
-Returns: "Yes"
-
-
-3)
-47
-
-Returns: "No"
-
-
-4)
-847
-
-Returns: "Yes"
+問題
+-Nがa×b×bかつa＜bを満たすかどうかを求める
 
 */
 // END CUT HERE
@@ -79,27 +15,6 @@ Returns: "Yes"
 #include <cstring>
 
 using namespace std;
-
-typedef long long LL;
-
-// BEGIN CUT HERE
-/*
-typedef pair<int, int> II;
-typedef vector<int> IntVec;
-typedef vector<LL> LLVec;
-typedef vector<II> IIVec;
-typedef vector<string> StrVec;
-typedef set<int> IntSet;
-typedef set<LL> LLSet;
-typedef set<II> IISet;
-typedef set<string> StrSet;
-
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
-#define MOD 1000000007LL
-#define MOD 1000000009LL
-*/
-// END CUT HERE
 
 class SemiPerfectSquare {
 public:
