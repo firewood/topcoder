6ea0abfce830509bbcdf1dde1dbc6eca931f01cb
firewood <karamaki@gmail.com>
2017-03-24 01:01:51 +0900

    translate

--- srm_6xx/srm_699/LastDigit.cpp
+++ srm_6xx/srm_699/LastDigit.cpp
@@ -2,77 +2,9 @@
 /*
 SRM 699 Div2 Medium (500)
 
-PROBLEM STATEMENT
-Limak chose a positive integer X and wrote it on a blackboard.
-After that, every day he erased the last digit of the current number.
-He stopped when he erased all digits.
-
-After the process was over, Limak computed the sum of all numbers that appeared on the blackboard, including the original number X.
-
-For example, if the original number was 509, the numbers that appeared on the blackboard were the numbers 509, 50, and 5.
-Their sum is 564.
-
-You are given a long long S.
-Limak wants you to find a number X such that the above process produces the sum S.
-It can be shown that for any positive S there is at most one valid X.
-If there is a valid X, find and return it.
-Otherwise, return -1.
-
-
-DEFINITION
-Class:LastDigit
-Method:findX
-Parameters:long long
-Returns:long long
-Method signature:long long findX(long long S)
-
-
-CONSTRAINTS
--S will be between 1 and 10^18, inclusive.
-
-
-EXAMPLES
-
-0)
-564
-
-Returns: 509
-
-This is the example from the problem statement. The value X = 509 produces the sum 564, as shown above.
-
-
-1)
-565
-
-Returns: -1
-
-There is no X for which the sum would be 565.
-
-
-2)
-3000
-
-Returns: 2701
-
-You can check that 2701 + 270 + 27 + 2 = 3000.
-
-
-3)
-137174210616796
-
-Returns: 123456789555123
-
-
-4)
-837592744927492746
-
-Returns: -1
-
-
-5)
-999999999999999999
-
-Returns: 900000000000000000
+問題
+-黒板に数Xを書き、最下位の桁をひとつずつ消す
+-黒板に現れた全ての数の合計がSのとき、Xを求める
 
 */
 // END CUT HERE
