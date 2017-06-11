be502158606fe1b41de0897f58fc8ab98282258f
Kenichi Aramaki <karamaki@gmail.com>
2017-06-11 21:00:33 +0900

    fix

--- yukicoder_4xx/482.cpp
+++ yukicoder_4xx/482.cpp
@@ -3,42 +3,23 @@
 
 using namespace std;
 
-class BIT {
-	std::vector<int> bit;
-	int size;
-public:
-	BIT() { }
-	BIT(int sz) { init(sz); }
-	void init(int sz) {
-		bit = std::vector<int>((size = sz) + 1);
-	}
-	int sum(int i) {
-		int s = 0;
-		while (i > 0) {
-			s += bit[i];
-			i -= i & -i;
-		}
-		return s;
-	}
-	void add(int i, int x) {
-		while (i <= size) {
-			bit[i] += x;
-			i += i & -i;
-		}
-	}
-};
-
 int main(int argc, char *argv[]) {
-	int n, k, c = 0;
+	long long n, k, c = 0;
 	cin >> n >> k;
-	BIT b(200001);
 	int d[200001];
+	int r[200001];
 	for (int i = 1; i <= n; ++i) {
 		cin >> d[i];
+		r[d[i]] = i;
 	}
-	for (int i = n; i > 0; --i) {
-		c += b.sum(d[i]);
-		b.add(d[i], 1);
+	for (int i = 1; i <= n; ++i) {
+		if (d[i] != i) {
+			++c;
+			int pos = r[i];
+			swap(d[i], d[pos]);
+			r[i] = i;
+			r[d[pos]] = pos;
+		}
 	}
 	bool ans = c <= k && ((k - c) % 2) == 0;
 	cout << (ans ? "YES" : "NO") << endl;
