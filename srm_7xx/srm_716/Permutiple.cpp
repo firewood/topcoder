552b633a6a56fd0d82ced67f9f60ed2b37c4a214
firewood <karamaki@gmail.com>
2017-07-12 23:27:17 +0900

    translate

--- srm_7xx/srm_716/Permutiple.cpp
+++ srm_7xx/srm_716/Permutiple.cpp
@@ -2,72 +2,8 @@
 /*
 SRM 716 Div2 Easy (250)
 
-PROBLEM STATEMENT
-You are given a positive integer x.
-Please check whether we can rearrange the digits of x (in base 10, without leading zeros) to produce a different number that is a multiple of x.
-
-Return "Possible" if this can be done and "Impossible" otherwise.
-Note that the return value is case-sensitive.
-
-
-DEFINITION
-Class:Permutiple
-Method:isPossible
-Parameters:int
-Returns:string
-Method signature:string isPossible(int x)
-
-
-CONSTRAINTS
--x will be between 1 and 1,000,000, inclusive.
-
-
-EXAMPLES
-
-0)
-142857
-
-Returns: "Possible"
-
-One valid way of rearranging the digits of 142857 is to form the number 285714. This new number is a multiple of x: we have 285714 = 2 * 142857.
-
-
-Another valid way is to form the number 857142 (which is equal to 6 * 142857).
-
-
-1)
-14
-
-Returns: "Impossible"
-
-The only other number we can get by rearranging the digits of the number 14 is the number 41.
-As 41 is not a multiple of 14, there is no solution.
-
-
-2)
-1035
-
-Returns: "Possible"
-
-We can get 3105 = 3 * 1035.
-
-
-3)
-1000000
-
-Returns: "Impossible"
-
-
-4)
-100035
-
-Returns: "Possible"
-
-
-5)
-4
-
-Returns: "Impossible"
+問題
+-数値xの各桁の数値を入れ替えて、x以外のxの倍数にできるかどうかを求める
 
 */
 // END CUT HERE
