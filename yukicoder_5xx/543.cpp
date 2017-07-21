b23fa80139178c0772e362cd8ed1d15a820ba968
firewood <karamaki@gmail.com>
2017-07-21 22:58:55 +0900

    yukicoder #543

---
+++ yukicoder_5xx/543.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string a, b;
+	cin >> a >> b;
+	cout << b << " " << a << endl;
+	return 0;
+}
