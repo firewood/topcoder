16d717656b62cad7e3e8a8788b8533777b307f60
firewood <karamaki@gmail.com>
2017-08-07 21:36:36 +0900

    eertree

--- hackerearth/jpcc2/a.cpp
+++ hackerearth/jpcc2/a.cpp
@@ -5,10 +5,72 @@
 #include <sstream>
 #include <cstdio>
 #include <cstring>
-#include <set>
 #include <map>
+#include <vector>
 
 using namespace std;
+
+struct EERTREE {
+	struct Node {
+		map<char, int> link;
+		int suffix_link;
+		int length;
+		int count;
+		Node() : suffix_link(0), length(0), count(0) { }
+	};
+	vector<Node> c;
+	string s;
+	int active_index;
+
+	Node *create_node() {
+		c.emplace_back();
+		return &c.back();
+	}
+
+	int find_previous_index(int id) {
+		const int pos = int(s.size()) - 1;
+		while (true) {
+			const int opposite_index = pos - 1 - c[id].length;
+			if (opposite_index >= 0 && s[opposite_index] == s.back()) {
+				break;
+			}
+			id = c[id].suffix_link;
+		}
+		return id;
+	}
+
+public:
+	EERTREE() {
+		Node *m1 = create_node();
+		m1->length = -1;
+		Node *m0 = create_node();
+		active_index = 0;
+	}
+
+	void add(char ch) {
+		s.push_back(ch);
+		const int a = find_previous_index(active_index);
+		const auto inserted_result = c[a].link.insert(make_pair(ch, int(c.size())));
+		active_index = inserted_result.first->second;
+		if (!inserted_result.second) {
+			c[active_index].count++;
+			return;
+		}
+		Node *node = create_node();
+		node->count = 1;
+		node->length = c[a].length + 2;
+		if (node->length == 1) {
+			node->suffix_link = 1;
+		} else {
+			const int b = find_previous_index(c[a].suffix_link);
+			node->suffix_link = c[b].link[ch];
+		}
+	}
+
+	int size() const {
+		return int(c.size()) - 2;
+	}
+};
 
 int main(int argc, char *argv[])
 {
@@ -16,75 +78,13 @@ int main(int argc, char *argv[])
 	cin >> n;
 	string s;
 	cin >> s;
-//	n = 131072, s = string(n, 'a');
+	n = 131072, s = string(n, 'a');
 
-	int top[131072] = {}, len[131072] = {};
-	int prev_top = 0, prev_len = 0;
-	char prev_char = 0;
-	int ans = 0;
-	{
-		map<int, int> m;
-		for (int i = 0; i < n; ++i) {
-			if (prev_char != s[i]) {
-				if (prev_char) {
-					m[prev_char] = max(m[prev_char], prev_len);
-				}
-				len[prev_top] = prev_len;
-				prev_len = 0;
-				prev_top = i;
-				prev_char = s[i];
-			}
-			top[i] = prev_top;
-			++prev_len;
-		}
-		m[prev_char] = max(m[prev_char], prev_len);
-		len[prev_top] = prev_len;
-		for (auto &kv : m) {
-			ans += kv.second;
-		}
+	EERTREE eertree;
+	for (auto c : s) {
+		eertree.add(c);
 	}
+	cout << eertree.size() << endl;
 
-	{
-		set<string> m;
-		for (int i = 0; i < n; ++i) {
-			int st = top[i], ed = st + len[st] - 1;
-			int skip_length = min(i - st, ed - i);
-			int j = 1 + skip_length;
-			if (i - j >= 0 && i + j < n && s[i - j] == s[i + j]) {
-				string x = s.substr(i, skip_length + 1);
-				for (; i - j >= 0 && i + j < n; ++j) {
-					if (s[i - j] != s[i + j]) {
-						break;
-					}
-					x += s[i + j];
-					m.insert(x);
-				}
-			}
-		}
-		ans += m.size();
-	}
-
-	{
-		set<string> m;
-		for (int i = 1; i < n; ++i) {
-			if (s[i - 1] == s[i]) {
-				int st = top[i], ed = st + len[st] - 1;
-				int skip_length = min(i - st - 1, ed - i);
-				int j = 1 + skip_length;
-				if (i - j >= 1 && i + j < n && s[i - j - 1] == s[i + j]) {
-					string x = s.substr(i, skip_length + 1);
-					for (; i - j >= 1 && i + j < n; ++j) {
-						if (s[i - j - 1] != s[i + j]) {
-							break;
-						}
-						x += s[i + j];
-						m.insert(x);
-					}
-				}
-			}
-		}
-		ans += m.size();
-	}
-	cout << ans << endl;
 	return 0;
 }
