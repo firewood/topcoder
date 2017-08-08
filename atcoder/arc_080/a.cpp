527ee1b8e1e68b51faca1ff2e358379ecebd32dd
firewood <karamaki@gmail.com>
2017-08-08 23:02:01 +0900

    ABC #069 A

---
+++ atcoder/arc_080/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n, m;
+	cin >> n >> m;
+	cout << (n - 1) * (m - 1) << endl;
+	return 0;
+}
