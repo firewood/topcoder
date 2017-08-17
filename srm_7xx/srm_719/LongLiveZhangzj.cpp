bf1e03ad4697022bb15b239fb323a11f15c8bcfe
firewood <karamaki@gmail.com>
2017-08-17 21:43:24 +0900

    Translate

--- srm_7xx/srm_719/LongLiveZhangzj.cpp
+++ srm_7xx/srm_719/LongLiveZhangzj.cpp
@@ -2,67 +2,9 @@
 /*
 SRM 719 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Zhangzj is the emperor of Yali empire and is esteemed by his people.
-
-"Long live Zhangzj" is heard across the country. Needless to say, people in the empire hope their leader will live forever. However, everybody will die, unless others donate their lives to the person.
-
-In the empire, there's a special way to donate life. There are some fixed words that are called exciting. Whenever someone speaks an exciting word, he automatically donates one second of his life to Zhangzj. For example, "toad" and "river" are exciting. Then, if somebody says the sequence of words "a", "toad", "jumps", "into", "the", "toad", "river", they donate three seconds to Zhangzj.
-
-Given a vector <string> speech containing all the words someone says, and a vector <string> words containing all exciting words, return the number of seconds the speaker will donate to Zhangzj.
-
-A word may appear multiple times in speech, but not in words.
-
-
-DEFINITION
-Class:LongLiveZhangzj
-Method:donate
-Parameters:vector <string>, vector <string>
-Returns:int
-Method signature:int donate(vector <string> speech, vector <string> words)
-
-
-CONSTRAINTS
--Both speech and words will contain between 1 and 50 elements, inclusive.
--All pairs of elements in words will be distinct.
--Each element of speech and words will consist only of lowercase English letters.
--The length of each element of speech and words will be between 1 and 50, inclusive.
-
-
-EXAMPLES
-
-0)
-{"make", "topcoder", "great", "again"}
-{"make", "america", "great", "again"}
-
-Returns: 3
-
-"make", "great" and "again" appear in speech and are exciting. Thus, the answer is 3.
-
-
-1)
-{"toads"}
-{"toad"}
-
-Returns: 0
-
-The strings "toad" and "toads" are not identical, therefore this speaker does not donate any seconds to Zhangzj.
-
-
-2)
-{"a", "a"}
-{"a"}
-
-Returns: 2
-
-Two elements of speech can be the same.
-
-
-3)
-{"je", "le", "ai", "deja", "vu", "et", "je", "le", "veux", "encore"}
-{"i", "am", "having", "deja", "vu", "please", "stop", "the", "encore"}
-
-Returns: 3
+問題
+-良い単語wordsが与えられる
+-文speechにwordsの単語が何個含まれているかを求める
 
 */
 // END CUT HERE
