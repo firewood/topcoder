e81436f428ee108b99b57920f917d0f2ff2ee631
firewood <karamaki@gmail.com>
2017-09-16 23:34:23 +0900

    ABC 074 A

---
+++ atcoder/arc_083/a.cpp
@@ -0,0 +1,13 @@
+// A.
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	long long n, a, ans;
+	cin >> n >> a;
+	ans = (n * n - a);
+	cout << ans << endl;
+	return 0;
+}
