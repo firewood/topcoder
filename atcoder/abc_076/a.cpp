b883a303d8c11caf17ac81404a6a4f97d1d080c0
firewood <karamaki@gmail.com>
2017-10-29 23:08:43 +0900

    ABC #076 A

---
+++ atcoder/abc_076/a.cpp
@@ -0,0 +1,15 @@
+// A. 
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int r, g;
+	cin >> r >> g;
+	int ans = g * 2 - r;
+	cout << ans << endl;
+	return 0;
+}
