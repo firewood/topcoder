ce0d674eb00a1005e4b1d38daa86d87d57db4547
firewood <karamaki@gmail.com>
2017-11-06 22:17:54 +0900

    ABC #077 B

---
+++ atcoder/arc_084/b.cpp
@@ -0,0 +1,15 @@
+// B.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	cin >> n;
+	int r = sqrt(n);
+	int ans = r * r;
+	cout << ans << endl;
+	return 0;
+}
