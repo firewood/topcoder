4c19f235236ccb09a785263cf347bd6724175e47
firewood <karamaki@gmail.com>
2017-11-09 22:09:36 +0900

    ABC #053 A

---
+++ atcoder/arc_068/a.cpp
@@ -0,0 +1,14 @@
+// A.
+ 
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+ 
+int main(int argc, char *argv[])
+{
+	int x;
+	cin >> x;
+	cout << (x >= 1200 ? "ARC" : "ABC") << endl;
+	return 0;
+}
