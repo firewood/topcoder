15279ed24d7b6e2e7a9774107cc3cf8d04519973
firewood <karamaki@gmail.com>
2017-11-10 22:40:25 +0900

    simplified

--- atcoder/arc_084/d.cpp
+++ atcoder/arc_084/d.cpp
@@ -11,22 +11,26 @@ using namespace std;
 
 typedef pair<int, int> II;
 
-int main(int argc, char *argv[])
-{
-	int k;
-	cin >> k;
+int solve(int k) {
 	deque<II> q;
 	q.push_back(II(1, 1));
-	vector<int> vis(k + 1, 1 << 30);
+	vector<int> vis(k + 1, -1);
 	while (!q.empty()) {
 		II top = q.front();
 		q.pop_front();
 		int x = top.first, y = top.second;
-		if (y < vis[x]) {
+		if (vis[x] < 0) {
 			vis[x] = y;
 			q.push_front(II((x * 10) % k, y));
 			q.push_back(II((x + 1) % k, y + 1));
 		}
 	}
-	cout << vis[0] << endl;
+	return vis[0];
+}
+
+int main(int argc, char *argv[])
+{
+	int k;
+	cin >> k;
+	cout << solve(k) << endl;
 }
