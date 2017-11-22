d3a647e52493a5bedee0195ac06a86d54c57e394
firewood <karamaki@gmail.com>
2017-11-22 22:51:20 +0900

    ABC #034 A

---
+++ atcoder/abc_034/a.cpp
@@ -0,0 +1,14 @@
+// A. 
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	cin >> x >> y;
+	cout << (x < y ? "Better" : "Worse") << endl;
+	return 0;
+}
