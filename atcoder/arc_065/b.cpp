ea8f8b434b1cfc9f7244a313a7fadc10fcbb275f
firewood <karamaki@gmail.com>
2017-11-26 20:48:51 +0900

    ABC #049 B

---
+++ atcoder/arc_065/b.cpp
@@ -0,0 +1,19 @@
+// B.
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int h, w;
+	cin >> h >> w;
+	for (int i = 0; i < h; ++i) {
+		string s;
+		cin >> s;
+		cout << s << endl;
+		cout << s << endl;
+	}
+	return 0;
+}
