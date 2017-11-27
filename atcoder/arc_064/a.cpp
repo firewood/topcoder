bd2d71086df8ee01be283d329354ef13c082006c
firewood <karamaki@gmail.com>
2017-11-27 23:13:29 +0900

    ABC #048 A

---
+++ atcoder/arc_064/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string a, b, c;
+	cin >> a >> b >> c;
+	cout << a[0] << b[0] << c[0] << endl;
+	return 0;
+}
