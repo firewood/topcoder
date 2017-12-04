8ba926a62aba0ecd12f1592032e3ac93ee543fbe
firewood <karamaki@gmail.com>
2017-12-04 22:09:05 +0900

    ABC #045 A

---
+++ atcoder/arc_061/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int a, b, h;
+	cin >> a >> b >> h;
+	int ans = ((a + b) * h) / 2;
+	cout << ans << endl;
+	return 0;
+}
