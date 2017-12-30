a311cb1db96aa94bfb739b6111bcfe8babce643e
firewood <karamaki@gmail.com>
2017-12-30 21:57:29 +0900

    ABC #084 A

---
+++ atcoder/abc_084/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int m;
+	cin >> m;
+	cout << (48 - m) << endl;
+	return 0;
+}
