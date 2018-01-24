fda033774a8df80cc4201e54482662571cf30569
firewood <karamaki@gmail.com>
2018-01-24 22:09:57 +0900

    translate

--- srm_7xx/srm_726/StringHalving.cpp
+++ srm_7xx/srm_726/StringHalving.cpp
@@ -2,69 +2,10 @@
 /*
 SRM 726 Div2 Easy (250)
 
-PROBLEM STATEMENT
-Hero has a string of lowercase English letters.
-Each letter that appears in the string appears exactly twice.
-
-Hero now wants to remove one half of his string.
-More precisely, he wants to remove one of the two copies of each letter in his string.
-The order of the letters he does not remove will remain unchanged.
-
-If there are multiple ways to remove the letters, Hero prefers the ones where the resulting string begins with a smaller letter (i.e., a letter that is earlier in the alphabet).
-
-You are given the string s containing Hero's string.
-Find the smallest letter that can appear at the beginning of the string after Hero removes half of the letters.
-Return a string with a single character: that letter.
-
-
-DEFINITION
-Class:StringHalving
-Method:lexsmallest
-Parameters:string
-Returns:string
-Method signature:string lexsmallest(string s)
-
-
-CONSTRAINTS
--s will contain between 2 and 50 characters, inclusive.
--Each character in s will be between 'a' and 'z', inclusive.
--Each letter from 'a' to 'z' will have either zero or two occurrences in s.
-
-
-EXAMPLES
-
-0)
-"baba"
-
-Returns: "a"
-
-Hero can remove the first 'b' and the second 'a' to obtain the string "ab".
-
-
-1)
-"bbaa"
-
-Returns: "b"
-
-Regardless of which 'a' and which 'b' Hero removes, the resulting string will always be "ba".
-
-
-2)
-"zyiggiyssz"
-
-Returns: "g"
-
-
-3)
-"topcodertpcder"
-
-Returns: "c"
-
-
-4)
-"rvofqorvfq"
-
-Returns: "f"
+問題
+-同じ文字が2回だけ出現する文字列がある
+-それぞれの文字を1つ削除し、半分の長さにする
+-辞書順最小の文字列を作るときの先頭の文字を求めよ
 
 */
 // END CUT HERE
