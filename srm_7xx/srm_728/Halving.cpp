f64d5ecd37cd2ca2a04f4438e71d530aa437f9f7
firewood <karamaki@gmail.com>
2018-02-04 21:49:40 +0900

    translate

--- srm_7xx/srm_728/Halving.cpp
+++ srm_7xx/srm_728/Halving.cpp
@@ -2,76 +2,12 @@
 /*
 SRM 728 Div1 Easy (250)
 
-PROBLEM STATEMENT
-
-You have a collection of sticks.
-The length of each stick is a positive integer.
-
-You want to have a collection of sticks in which all the sticks have the same length.
-You may alter your current collection by performing zero or more steps.
-Each step must look as follows:
-
-You choose one of your sticks. The chosen stick must have length at least 2.
-Let L be the length of the chosen stick.
-If L is even, cut the stick into two sticks of length L/2 each. Otherwise, cut it into sticks of lengths (L-1)/2 and (L+1)/2.
-Keep one of the two new sticks and throw away the other one.
-
-It can be proved that any collection of sticks can be turned into a collection of sticks that all have the same length.
-You are given the current lengths of your sticks in the vector <int> a.
-Compute and return the smallest number of steps needed to reach your goal.
-
-
-DEFINITION
-Class:Halving
-Method:minSteps
-Parameters:vector <int>
-Returns:int
-Method signature:int minSteps(vector <int> a)
-
-
-CONSTRAINTS
--a will contain between 2 and 50 elements, inclusive.
--Each element of a will be between 1 and 10**9, inclusive.
-
-
-EXAMPLES
-
-0)
-{11, 4}
-
-Returns: 3
-
-One optimal solution is:
-
- Pick the stick of length 11, cut it into sticks of lengths 5 and 6 and keep the part of length 5. 
- Pick the stick of length 4, cut it into two sticks of length 2 and keep the part of length 2. 
- Pick the stick of length 5, cut it into sticks of lengths 2 and 3 and keep the part of length 2. 
-
-In the end, you'll have two sticks of length 2.
-
-1)
-{1000000000, 1000000000, 1000000000, 1000000000}
-
-Returns: 0
-
-All your sticks have the same length, no steps are needed.
-
-2)
-{1, 2, 3, 4, 5, 6, 7}
-
-Returns: 10
-
-
-3)
-{13, 13, 7, 11, 13, 11}
-
-Returns: 11
-
-
-4)
-{1, 1}
-
-Returns: 0
+問題
+-N本かの棒がある
+-それぞれの棒について、長さが2以上のときは分割操作ができる
+-長さがLのとき、Lが偶数ならL/2に、Lが奇数なら(L-1)/2と(L+1)/2に分割できる
+-分割したあと、片方だけを残す
+-全ての棒を同じ長さにするのに必要なステップ数の最小値を求める
 
 */
 // END CUT HERE
