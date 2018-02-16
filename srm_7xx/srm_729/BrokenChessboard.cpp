3d449c06d06689ecc6dcf0bff7465f0e5cf25528
firewood <karamaki@gmail.com>
2018-02-16 23:30:28 +0900

    translate

--- srm_7xx/srm_729/BrokenChessboard.cpp
+++ srm_7xx/srm_729/BrokenChessboard.cpp
@@ -1,91 +1,11 @@
 // BEGIN CUT HERE
 /*
-SRM 610 Div1 Easy (250)
-SRM 610 Div1 Medium (500)
+SRM 729 Div2 Easy (250)
 
-PROBLEM STATEMENT
-A chessboard consists of alternating black and white squares: every square differs in color from
-the squares that are adjacent to it vertically and horizontally.
-
-You are given a rectangular board of black and white squares in a vector <string> board.
-However, the coloring of the squares might not match the chessboard coloring scheme. Each string in
-board represents a row of squares of the board, and each character (all either 'B' or 'W') represents
-a square in that row. Return the minimum number of squares that would need their color swapped for
-the given board to become a valid chessboard.
-
-
-DEFINITION
-Class:BrokenChessboard
-Method:minimumFixes
-Parameters:vector <string>
-Returns:int
-Method signature:int minimumFixes(vector <string> board)
-
-
-CONSTRAINTS
--board will contain between 1 and 50 elements, inclusive.
--Each element of board will contain between 1 and 50 characters, inclusive.
--Each element of board will be the same length.
--Each character of each element of board will be either 'B' or 'W'.
-
-
-EXAMPLES
-
-0)
-{"BWB",
- "BBW",
- "BWW"}
-
-Returns: 2
-
-There are two ways to fix this board:
-
-BWB  WBW
-WBW  BWB
-BWB  WBW
-
-
-The result on the left would require changing 2 squares (center-left and bottom-right).
-The result on the right would mean changing 7 squares, so the former is a better choice.
-
-
-1)
-{"WW",
- "WW"}
-
-Returns: 2
-
-Either of the two valid boards we could make would take exactly 2 changes.
-
-
-2)
-{"BWB",
- "WBW",
- "BWB"}
-
-Returns: 0
-
-This board is already valid, so no fixes are needed.
-
-
-3)
-{"B",
- "W",
- "B",
- "B"}
-
-Returns: 1
-
-The board is not square, but anyways it only needs 1 square changed.
-
-
-4)
-{"BWBBB",
- "WWBBW",
- "BBBBW",
- "WBWBB"}
-
-Returns: 7
+問題
+-チェス盤は白と黒のマス目からなる。隣り合うマス目の色は異なる。
+-盤面と色の状態が与えられる。
+-隣り合うマス目が異なるようにひっくり返すとき、少なくとも何個をひっくり返す必要があるかを求めよ。
 
 */
 // END CUT HERE
