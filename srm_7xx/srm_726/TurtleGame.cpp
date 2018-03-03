2983d935725c888824a353581c59db5e3d9a6e33
firewood <karamaki@gmail.com>
2018-03-03 21:49:47 +0900

    bug2

--- srm_7xx/srm_726/TurtleGame.cpp
+++ srm_7xx/srm_726/TurtleGame.cpp
@@ -2,78 +2,196 @@
 /*
 SRM 726 Div2 Medium (500)
 
-問題
--盤面が与えられる
--左下からスタートして、右下がゴール
--右か下だけ進める
--二人で交互にひとつずつ升目を通れなくしていく
--ゴールに到達できない状態になった方が負け
--先手が勝つかどうかを求めよ
+PROBLEM STATEMENT
+Hero and Jack have a rectangular board that is divided into a grid of unit square cells.
+Some of the cells may already be missing from the board.
+
+Turtles like to walk across their board.
+Each turtle will always enter the top left cell of the board and then it will walk to the bottom right cell.
+During the walk the turtle moves in steps, and each step must take the turtle either one cell down or one cell to the right.
+Obviously, a turtle cannot visit a cell that is already missing from the board.
+The sequence of cells visited by a turtle along its walk will be called a turtle path.
+
+We say that a board is turtle-friendly if a turtle can still complete its walk on the board.
+In other words, a turtle-friendly board is a board that contains at least one turtle path.
+In particular, note that a turtle-friendly board must contain both the top left cell and the bottom right cell.
+
+Hero and Jack are now going to play a game with their board.
+They will take alternating turns, with Hero being the first one to play.
+In each turn, the active player has to remove one cell from the board.
+The game continues as long as the board remains turtle-friendly.
+The player who causes the board to stop being turtle-friendly loses the game.
+
+You are given the vector <string> board that represents the initial state of the board:
+the character '.' represents a cell that is still present, the character '#' is a cell that has already been removed.
+It is guaranteed that board represents a turtle-friendly board.
+
+Return "Win" if Hero can win the game regardless of how Jack plays.
+Otherwise, return "Lose".
+
+DEFINITION
+Class:TurtleGame
+Method:getwinner
+Parameters:vector <string>
+Returns:string
+Method signature:string getwinner(vector <string> board)
+
+
+CONSTRAINTS
+-board will contain between 2 and 20 elements, inclusive.
+-All elements in board will contain the same sumber of characters.
+-Each element in board will contain between 2 and 20 characters, inclusive.
+-Each character in board will be either '.' or '#'.
+-It is guaranteed that there is at least one undestroyed turtle path.
+
+
+EXAMPLES
+
+0)
+{"..",
+ ".."}
+
+Returns: "Win"
+
+This board contains two different turtle paths.
+Hero has two moves that don't lose him the game: he can remove either the top right or the bottom left cell.
+Each of these moves will leave only one of the two turtle paths, and Jack will have no valid moves left.
+Jack's move will create a board that is not turtle-friendly, which means that Hero will win the game.
+
+
+1)
+{"...",
+ ".#.",
+ ".#.",
+ "..."}
+
+Returns: "Lose"
+
+Again there are only two turtle paths.
+However, now they are longer.
+Regardless of which cell Hero removes (other than the top left and the bottom right cell), his move will create three cells that can be destroyed "for free".
+Since 3 is an odd number, Jack will be the one to remove the last of these three cells. In the following turn Hero will lose the game.
+
+
+2)
+{".##",
+ "..#",
+ "#.."}
+
+Returns: "Lose"
+
+
+3)
+{"....",
+ "...#",
+ "#...",
+ "...."}
+
+Returns: "Win"
+
+
+4)
+{"...#.",
+ "....#",
+ "#....",
+ "..##."}
+
+Returns: "Win"
+
+
+5)
+{"..........",
+ "........#."}
+
+Returns: "Lose"
 
 */
 // END CUT HERE
 #include <algorithm>
+#include <cmath>
+#include <numeric>
 #include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
 #include <vector>
 #include <iostream>
 #include <sstream>
 #include <cstring>
+#include <bitset>
 
 using namespace std;
 
+typedef bitset<400> Bits;
+
 class TurtleGame {
-	bool win(vector <string> &board) {
-		int f[20][20] = {};
-		f[0][0] = 1;
-		int i, j;
-		for (i = 0; i != board.size(); ++i) {
-			for (j = 0; j != board[0].length(); ++j) {
-				if (board[i][j] == '.') {
-					if ((j > 0 && f[i][j - 1]) || (i > 0 && f[i - 1][j])) {
-						f[i][j] = 1;
+	int h;
+	int w;
+	unordered_map<Bits, int> cmemo;
+	unordered_map<Bits, int> memo;
+	Bits corner;
+
+	bool check(const Bits &b) {
+		if (cmemo[b]) {
+			return cmemo[b] - 1 != 0;
+		}
+		Bits c;
+		c[0] = 1;
+		for (int i = 0; i < h; ++i) {
+			for (int j = 0; j < w; ++j) {
+				int k = i * w + j;
+				if ((i || j) && !b[k]) {
+					if ((i && c[k - w]) || (j && c[k - 1])) {
+						c[k] = 1;
 					}
 				}
 			}
 		}
-		i = board.size() - 1, j = board[0].length() - 1;
-		if (!f[i][j]) {
-			return true;
+		cmemo[b] = c[h * w - 1] + 1;
+		return cmemo[b] - 1 != 0;
+	}
+
+	bool win(Bits &b) {
+		if (memo[b]) {
+			return memo[b] - 1 != 0;
 		}
-		while (i > 0 || j > 0) {
-			f[i][j] = 2;
-			if (i > 0 && f[i - 1][j]) {
-				--i;
-			} else {
-				--j;
-			}
-		}
-		for (i = 0; i != board.size(); ++i) {
-			for (j = 0; j != board[0].length(); ++j) {
-				if (board[i][j] == '.' && f[i][j] != 2) {
-					board[i][j] = '#';
-					return !win(board);
+		for (int i = 0; i < h; ++i) {
+			for (int j = 0; j < w; ++j) {
+				int k = i * w + j;
+				if (!corner[k] && !b[k]) {
+					b[k] = 1;
+					if (check(b)) {
+						if (!win(b)) {
+							b[k] = 0;
+							memo[b] = 2;
+							return true;
+						}
+					}
+					b[k] = 0;
 				}
 			}
 		}
+		memo[b] = 1;
 		return false;
 	}
 
 public:
 	string getwinner(vector <string> board) {
-		board[0][0] = 'S';
-		board[board.size() - 1][board[0].length()] = 'G';
-		for (int i = 0; i != board.size(); ++i) {
-			for (int j = 0; j != board[0].length(); ++j) {
-				if (board[i][j] == '.') {
-					vector <string> b = board;
-					b[i][j] = '#';
-					if (!win(b)) {
-						return "Win";
-					}
+		h = (int)board.size();
+		w = (int)board[0].length();
+		cmemo.clear();
+		memo.clear();
+		corner = Bits();
+		corner[0] = 1, corner[h * w - 1] = 1;
+		Bits b;
+		for (int i = 0; i < h; ++i) {
+			for (int j = 0; j < w; ++j) {
+				if (board[i][j] == '#') {
+					b[i * w + j] = 1;
 				}
 			}
 		}
-		return "Lose";
+		return win(b) ? "Win" : "Lose";
 	}
 
 // BEGIN CUT HERE
@@ -159,6 +277,24 @@ public:
 		}
 		n++;
 
+
+
+		if ((Case == -1) || (Case == n)) {
+			string Arr0[] = {
+				"..........#.......",
+				"..........#....#..",
+				"...........#...#..",
+				".....#..#........#",
+				".........##....#..",
+				".....#............",
+				".............#....",
+				"...#...........#.." };
+			string Arg1 = "Lose";
+
+			vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0])));
+			verify_case(n, Arg1, getwinner(Arg0));
+		}
+		n++;
 	}
 
 // END CUT HERE
@@ -168,6 +304,7 @@ public:
 // BEGIN CUT HERE
 int main() {
 	TurtleGame ___test;
+	___test.run_test(1);
 	___test.run_test(-1);
 }
 // END CUT HERE
