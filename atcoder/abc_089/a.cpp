f856b4ec1ebf7bc785bcb28d4f3e3943bb98c00b
firewood <karamaki@gmail.com>
2018-03-04 22:49:28 +0900

    ABC #089 A

---
+++ atcoder/abc_089/a.cpp
@@ -0,0 +1,16 @@
+// A. 
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int n;
+	cin >> n;
+	int ans = n / 3;
+	cout << ans << endl;
+	return 0;
+}
