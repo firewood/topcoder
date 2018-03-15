aa4107eafeb40174572ca9a3c272e645237a98fe
firewood <karamaki@gmail.com>
2018-03-15 22:30:14 +0900

    ABC #090 A

---
+++ atcoder/arc_091/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string s[3];
+	cin >> s[0] >> s[1] >> s[2];
+	cout << s[0][0] << s[1][1] << s[2][2] << endl;
+	return 0;
+}
