7fca5164032c5d416c2ab109f4e10df969dab61d
firewood <karamaki@gmail.com>
2018-03-20 23:15:16 +0900

    ABC #091 A

---
+++ atcoder/arc_092/a.cpp
@@ -0,0 +1,13 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << ((a + b) >= c ? "Yes" : "No") << endl;
+	return 0;
+}
