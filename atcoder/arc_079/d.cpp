679c2c96c0afa3d77aefd709dc3aae58cbc854e9
firewood <karamaki@gmail.com>
2018-03-29 23:18:25 +0900

    fix

--- atcoder/arc_079/d.cpp
+++ atcoder/arc_079/d.cpp
@@ -1,24 +1,38 @@
 // D.
 
 #include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long LL;
 
 int main(int argc, char *argv[]) {
+	const LL n = 50;
 	LL k;
 	cin >> k;
-	LL a[2] = { k / 4 + 1, k / 4 + 1 };
-	k %= 4;
-	for (LL i = 0; i < k; ++i) {
-		int t = i % 2;
-		a[t] += 2;
-		a[!t] -= 1;
+
+	LL a[n];
+	fill(a, a + n, k / n + n - 1);
+	for (LL i = 0; i < k % n; ++i) {
+		int t = i % n;
+		for (int j = 0; j < n; ++j) {
+			if (t == j) {
+				a[j] += n;
+			} else {
+				a[j] -= 1;
+			}
+		}
 	}
-	cout << 2 << " " << a[0] << " " << a[1] << endl;
+
+	cout << n << endl;
+	for (int i = 0; i < n; ++i) {
+		if (i) {
+			cout << " ";
+		}
+		cout << a[i];
+	}
+	cout << endl;
+
 	return 0;
 }
