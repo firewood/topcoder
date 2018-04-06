54db68cc1532baee89822c6673f814dca40fd002
firewood <karamaki@gmail.com>
2018-04-06 23:52:46 +0900

    translate

--- srm_7xx/srm_731/TreesAndBrackets.cpp
+++ srm_7xx/srm_731/TreesAndBrackets.cpp
@@ -2,80 +2,10 @@
 /*
 SRM 731 Div1 Easy (250)
 
-PROBLEM STATEMENT
-This problem is about rooted trees in which the order of children matters.
-This type of trees is easily encoded using correct bracket sequences.
-The code of a tree rooted at r is constructed as follows:
-
-Write down the opening bracket '('.
-For each child c of r, in order, write down the code of the subtree rooted at c.
-Write down the closing bracket ')'.
-
-For example, the code of a three-vertex tree in which the root has two children is "(()())".
-
-You are given the strings t1 and t2 that represent two rooted trees using the encoding defined above.
-You want to transform t1 into t2.
-
-The only operation you are allowed to perform is to remove a leaf from t1.
-(A leaf is a vertex with no children.)
-Note that removing the child of a parent does not change the relative order of the other children of that same parent.
-
-Return "Possible" if there is a sequence of zero or more operations that transforms t1 into t2.
-Otherwise, return "Impossible".
-
-
-DEFINITION
-Class:TreesAndBrackets
-Method:check
-Parameters:string, string
-Returns:string
-Method signature:string check(string t1, string t2)
-
-
-CONSTRAINTS
--t1 and t2 will contain between 2 and 100 characters, inclusive.
--Each character in t1 and t2 will be either '(' or ')'.
--Both t1 and t2 will represent a correct tree.
-
-
-EXAMPLES
-
-0)
-"(())"
-"()"
-
-Returns: "Possible"
-
-
-1)
-"()"
-"()"
-
-Returns: "Possible"
-
-
-2)
-"(()()())"
-"((()))"
-
-Returns: "Impossible"
-
-Currently t1 is a tree of depth 2 in which the root has three children, while t2 is a tree of depth 3.
-Clearly, you cannot increase the depth of a tree by removing some of its vertices.
-
-
-3)
-"((())((())())())"
-"(()(())())"
-
-Returns: "Possible"
-
-
-4)
-"((())((())())())"
-"((()()()()()))"
-
-Returns: "Impossible"
+問題
+-木の親子関係を括弧で表す
+-各頂点は'('ではじまり、子の全ての頂点を記述し、')'で閉じる
+-1回の操作で葉だけを取り除くことができるとき、木表現t1をt2にできるかどうかを求めよ。
 
 */
 // END CUT HERE
