11af45bf281a7dc181f656feb4344fbaf4ebcbb4
firewood <karamaki@gmail.com>
2018-05-31 23:24:17 +0900

    ABC #098 A

---
+++ atcoder/arc_098/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	cin >> a >> b;
+	int ans = max({ a + b, a - b, a * b });
+	cout << ans << endl;
+	return 0;
+}
