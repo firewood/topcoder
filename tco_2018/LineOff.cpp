7709e066e766a8a530df23fb29015d36b86546e2
Kenichi Aramaki <karamaki@gmail.com>
2018-06-07 01:24:45 -0700

    translate

--- tco_2018/LineOff.cpp
+++ tco_2018/LineOff.cpp
@@ -2,71 +2,11 @@
 /*
 TCO18 R1B Easy (250)
 
-PROBLEM STATEMENT
-
-You are given a set of colored points on a straight line.
-
-You play a game with these points.
-The game is a sequence of moves.
-In each move you have to erase two points that are adjacent and share the same color.
-(Two points are adjacent if there is no other point between them. Distances don't matter.)
-
-You are given the string points. Each character of points describes the color of one point, in the order in which they are arranged on the line at the beginning of the game.
-(Different letters represent different colors.)
-Compute and return the maximum number of moves you can make.
-
-
-DEFINITION
-Class:LineOff
-Method:movesToDo
-Parameters:string
-Returns:int
-Method signature:int movesToDo(string points)
-
-
-CONSTRAINTS
--points will contain between 1 and 50 characters, inclusive.
--Each character in points will be a lowercase English letter ('a'-'z').
-
-
-EXAMPLES
-
-0)
-"abba"
-
-Returns: 2
-
-The only valid first move is to erase the two points of color 'b'. After that move the points of color 'a' are now adjacent and they can be removed in our second move.
-
-
-1)
-"zwwwzffw"
-
-Returns: 2
-
-You can remove two 'w' points and two 'f' points with your first two moves. After that the remaining points on the line will be arranged as follows: "zwzw". In this situation you don't have any adjacent points that share the same color, so there are no more moves.
-
-
-2)
-"rrrrrrr"
-
-Returns: 3
-
-With an odd number of points, at least one of them will have no pair.
-
-
-3)
-"dfghj"
-
-Returns: 0
-
-All colors are different so no points can be removed.
-
-
-4)
-"wasitacarooracatisaw"
-
-Returns: 10
+問題
+-直線上に、彩色された点がいくつかある
+-同じ色の隣接する2つの点を消すと1点入る
+-各文字が色を表した文字列pointsが与えられる
+-最大の点数を求めよ
 
 */
 // END CUT HERE
