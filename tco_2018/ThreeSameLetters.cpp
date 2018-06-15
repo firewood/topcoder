0b3372cede92c564cc0a9f6b8063daa6d0edcc23
firewood <karamaki@gmail.com>
2018-06-15 22:22:07 +0900

    translate

--- tco_2018/ThreeSameLetters.cpp
+++ tco_2018/ThreeSameLetters.cpp
@@ -2,72 +2,9 @@
 /*
 TCO18 R1B Hard (1000)
 
-PROBLEM STATEMENT
-
-A string is called a block if it consists of exactly three equal letters.
-
-A string is called a one-block string if it contains exactly one contiguous substring that is a block.
-
-For example:
-
-"aaa" and "abcccdd" are one-block strings
-"noblocks" and "ababab" are not one-block strings, as they do not contain any blocks
-"xxxyyyzzz" and "baaaad" are not one-block strings, as each of them contains at least two blocks
-
-Note that the two blocks in "baaaad" overlap partially.
-
-You are given the ints L and S.
-If we can only use the first S lowercase letters of the English alphabet, how many different one-block strings of length L are there?
-Return their count modulo 10^9 + 7.
-
-
-DEFINITION
-Class:ThreeSameLetters
-Method:countStrings
-Parameters:int, int
-Returns:int
-Method signature:int countStrings(int L, int S)
-
-
-CONSTRAINTS
--L will be between 1 and 26, inclusive.
--S will be between 1 and 26, inclusive.
-
-
-EXAMPLES
-
-0)
-3
-7
-
-Returns: 7
-
-The seven one-block strings are "aaa", "bbb", "ccc", ..., "ggg".
-
-
-1)
-4
-2
-
-Returns: 4
-
-The four one-block strings are "aaab", "abbb", "baaa", and "bbba".
-
-
-2)
-2
-17
-
-Returns: 0
-
-
-3)
-10
-11
-
-Returns: 410199993
-
-Don't forget to do the calculations modulo 10^9 + 7.
+問題
+-同じ文字が3つ続く部分をブロックとする
+-S種類の文字からなる長さLの文字列のうち、ブロックを一つだけ含むものがいくつあるかを求めよ
 
 */
 // END CUT HERE
