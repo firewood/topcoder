365f7b269c06fee408a17115f41c9cefd9dee4cc
firewood <karamaki@gmail.com>
2018-06-30 22:21:05 +0900

    translate

--- srm_7xx/srm_735/BinaryCalculator.cpp
+++ srm_7xx/srm_735/BinaryCalculator.cpp
@@ -2,67 +2,9 @@
 /*
 SRM 735 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-You have a very simple calculator that has a display and two buttons.
-The buttons are labelled "-2" and "+3".
-Whenever you press a button, the corresponding operation is applied to the number shown on the display.
-For example, if the display shows the number 10, pressing the "-2" button changes it to 10-2 = 8, while pressing the "+3" button changes it to 10+3 = 13.
-
-You are given the ints a and b.
-The display currently shows the integer a.
-You would like to change the displayed number to b.
-Calculate and return the smallest total number of buttons you need to press.
-
-
-DEFINITION
-Class:BinaryCalculator
-Method:minimumSteps
-Parameters:int, int
-Returns:int
-Method signature:int minimumSteps(int a, int b)
-
-
-CONSTRAINTS
--a will be between 1 and 100, inclusive.
--b will be between 1 and 100, inclusive.
-
-
-EXAMPLES
-
-0)
-10
-14
-
-Returns: 3
-
-One solution is to press the button +3 to get 13, then -2 to get 11 and finally +3 to get 14.
-There are other ways of obtaining the same result, but none of them is shorter than 3 presses.
-
-
-1)
-23
-23
-
-Returns: 0
-
-As a = b, you don't need to press any buttons.
-
-
-2)
-18
-12
-
-Returns: 3
-
-The only optimal solution is to press "-2" three times in a row.
-
-
-3)
-23
-62
-
-Returns: 13
+問題
+-マイナス2とプラス3だけが実行できる電卓がある
+-この電卓で値aからbにする手数の最小値を求めよ
 
 */
 // END CUT HERE
