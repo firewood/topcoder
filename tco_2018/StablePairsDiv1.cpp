28b3a100a85a70efd9a6ee41538f4c841d842912
firewood <karamaki@gmail.com>
2018-08-15 23:00:13 +0900

    translate

--- tco_2018/StablePairsDiv1.cpp
+++ tco_2018/StablePairsDiv1.cpp
@@ -2,116 +2,13 @@
 /*
 TCO18 R1B Medium (600)
 
-PROBLEM STATEMENT
-
-Consider the set of integers between 1 and n, inclusive, and two positive integers c and k.
-You want to build an ordered list of k pairs 
-(x1, y1),
-(x2, y2),
-...
-(xk, yk)
-such that the following conditions are met.
-
- 1 ? xi < yi ? n for all i between 1 and k, inclusive.
- yi < xi+1 for all i between 1 and k-1, inclusive.
- (xi+1 + yi+1) - (xi + yi) = c for all i between 1 and k-1, inclusive.
-
-If a list of pairs satisfies all of the above conditions, the list is said to be stable.
-For any fixed n, c, and k, a stable list of k pairs is said to be maximal
-if its sum of elements (the sum of all 2k integers it contains) is the largest among all stable lists 
-of k pairs.
-
-For instance, consider n=5, c=4, and k=2.
-There are two stable lists of pairs: one is [(1, 2), (3, 4)] and the other is [(2, 3), (4, 5)].
-The latter is the only maximal stable list of pairs in this example as its sum is (2+3+4+5) = 14.
-
-Given n, c, and k, find one maximal stable list of pairs, 
-and return a vector <int> that describes the list.
-Specifically, if 
-(x1, y1),
-(x2, y2),
-...,
-(xk, yk)
-is your maximal stable list of pairs, you must return 
-{
-x1, y1,
-x2, y2,
-...,
-xk, yk
-}.
-
-If there are many maximal stable lists of pairs, you may return any one of them. 
-If no stable list of pairs with the desired properties exists, you must return an empty vector <int>.
-
-
-DEFINITION
-Class:StablePairsDiv1
-Method:findMaxStablePairs
-Parameters:int, int, int
-Returns:vector <int>
-Method signature:vector <int> findMaxStablePairs(int n, int c, int k)
-
-
-CONSTRAINTS
--n will be between 1 and 100, inclusive.
--c will be between 1 and 100, inclusive.
--k will be between 1 and 100, inclusive.
-
-
-EXAMPLES
-
-0)
-5
-4
-2
-
-Returns: {2, 3, 4, 5 }
-
-This example was described in the problem statement.
-
-
-1)
-4
-4
-2
-
-Returns: {1, 2, 3, 4 }
-
-
-2)
-1
-100
-1
-
-Returns: { }
-
-When n=1, regardless of c and k, there is no way to build a pair. 
-
-
-3)
-3
-100
-1
-
-Returns: {2, 3 }
-
-With k=1 we are looking for stable lists that only contain a single pair of numbers. There are three stable lists: [(1, 2)], [(1, 3)], and [(2, 3)]. Obviously, the last one is the only maximal one in this case.
-
-
-4)
-10
-6
-3
-
-Returns: {2, 5, 6, 7, 9, 10 }
-
-
-5)
-12
-7
-3
-
-Returns: {4, 5, 6, 10, 11, 12 }
+問題
+-1からNまでの数がある
+-以下の条件でK個のペア(x1, y1), (x2, y2), ... (xk, yk)を作る
+-1 <= xi < yi <= n
+-yi < xi+1
+-(xi+1 + yi+1) - (xi + yi) = C
+-N, C, Kが与えられるとき、K個のペアを構築せよ
 
 */
 // END CUT HERE
