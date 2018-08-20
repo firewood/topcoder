3ebb9500bf6e75bd980171dcd36c5e9925e6fc6e
Kenichi Aramaki <karamaki@gmail.com>
2018-08-20 22:05:06 +1200

    ABC #106 A

---
+++ atcoder/abc_106/a.cpp
@@ -0,0 +1,17 @@
+// A. 
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	cin >> a >> b;
+	int ans = (a - 1) * (b - 1);
+	cout << ans << endl;
+	return 0;
+}
