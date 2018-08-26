f712994b8ad41481f279ddec915a0f98b10a5026
Kenichi Aramaki <karamaki@gmail.com>
2018-08-26 22:58:56 +1200

    simplified

--- atcoder/agc_024/b.cpp
+++ atcoder/agc_024/b.cpp
@@ -8,22 +8,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int n;
+	int n, mx = 0;
 	cin >> n;
-	vector<int> pos(n + 1);
+	vector<int> cnt(n + 1);
 	for (int i = 0; i < n; ++i) {
 		int a;
 		cin >> a;
-		pos[a] = i;
-	}
-	pos[0] = 1<<30;
-	int cnt = 0, mx = 0;
-	for (int i = 1; i <= n; ++i) {
-		if (pos[i - 1] > pos[i]) {
-			cnt = 0;
-		}
-		++cnt;
-		mx = max(mx, cnt);
+		cnt[a] = cnt[a - 1] + 1;
+		mx = max(mx, cnt[a]);
 	}
 	int ans = n - mx;
 	cout << ans << endl;
