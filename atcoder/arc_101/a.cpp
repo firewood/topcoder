68d8fe137a70ca52a836dbca31a2fb996e8e2db4
Kenichi Aramaki <karamaki@gmail.com>
2018-08-27 20:44:35 +0900

    ABC #107 A

---
+++ atcoder/arc_101/a.cpp
@@ -0,0 +1,16 @@
+// A. 
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int n, i;
+	cin >> n >> i;
+	int ans = n - i + 1;
+	cout << ans << endl;
+	return 0;
+}
