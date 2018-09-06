e3e60f27dbf0a995f4d3abc2dfadce5bdbf97d91
firewood <karamaki@gmail.com>
2018-09-07 00:23:11 +0900

    fix

--- atcoder/arc_102/c.cpp
+++ atcoder/arc_102/c.cpp
@@ -11,11 +11,11 @@ typedef long long LL;
 int main(int argc, char *argv[]) {
 	LL n, k, ans = 0;
 	cin >> n >> k;
-	for (LL i = 1; i <= n; ++i) {
-		if ((i * 2) % k == 0) {
-			LL a = n - (i % k);
-			LL b = a / k + (a % k ? 0 : 1);
-			ans += b * b;
+	for (LL a = 1; a <= n; ++a) {
+		if ((a * 2) % k == 0) {
+			LL b = k - (a % k);
+			LL c = (n - b + k) / k;
+			ans += c * c;
 		}
 	}
 	cout << ans << endl;
