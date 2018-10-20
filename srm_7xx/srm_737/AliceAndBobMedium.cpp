9c863ba262d90d67f743db124a696a2d419db995
firewood <karamaki@gmail.com>
2018-10-20 23:12:11 +0900

    translate

--- srm_7xx/srm_737/AliceAndBobMedium.cpp
+++ srm_7xx/srm_737/AliceAndBobMedium.cpp
@@ -2,80 +2,13 @@
 /*
 SRM 737 Div2 Medium (500)
 
-PROBLEM STATEMENT
-
-Alice and Bob are playing a game of NIM.
-
-The rules of NIM are as follows:
-There are several piles of stones.
-The players take alternating turns.
-In each turn, the current player selects one non-empty pile of stones and removes some stones from the pile.
-(The player must remove at least one stone. They can remove as many stones as they want, possibly all of them, but just from a single pile.)
-The game ends when all stones have been removed.
-The player who removed the last stone wins the game.
-Equivalently, the first player who is unable to make a valid move loses the game.
-
-The game of NIM has a very simple strategy based on binary representations of pile sizes.
-A position in the game of NIM is winning if the bitwise XOR of the sizes of all piles is non-zero, and losing if the bitwise XOR of the sizes of all piles is zero.
-It can be shown that:
-
-If it is your turn and you are in a losing position, you will lose the game if your opponent plays optimally.
-If it is your turn and you are in a winning position, you will win the game if you play optimally.
-
-Both Alice and Bob play the game of NIM optimally.
-It is now Alice's turn.
-You are given a description of the current state of the game: the vector <int> a.
-Each element of a represents the number of stones on one of the piles.
-Consider all possible first moves Alice can make in the given situation.
-For how many of them will Alice eventually win the game?
-Find and return that number.
-
-(Two moves are considered distinct if they produce a different new sequence of pile sizes.
-In other words, we can distinguish between different piles that have the same number of stones.
-See Example #1.)
-
-
-DEFINITION
-Class:AliceAndBobMedium
-Method:count
-Parameters:vector <int>
-Returns:int
-Method signature:int count(vector <int> a)
-
-
-CONSTRAINTS
--a will contain between 1 and 37 elements, inclusive.
--Each element of a will be between 1 and 737,373,737, inclusive.
-
-
-EXAMPLES
-
-0)
-{737}
-
-Returns: 1
-
-There is a single pile with 737 stones. In this situation Alice has exactly one winning move: she should remove all stones and win immediately. If she does anything else, Bob will remove all the remaining stones and win.
-
-
-1)
-{7,3,7}
-
-Returns: 3
-
-In this situation Alice has three different winning moves:
-
-Remove three stones from the first pile.
-Remove three stones from the second pile.
-Remove three stones from the third pile.
-
-
-2)
-{3,7,3,7}
-
-Returns: 0
-
-Regardless of how Alice plays from this position, Bob will eventually win the game.
+問題
+-二人でNIMゲームを行う
+-いくつかの石からなるいくつかの山がある
+-プレイヤーは一つの山を選び、一つ以上の石を取る
+-最後の石を取ったプレイヤーが勝つ
+-石の状態が与えられる
+-勝てる初手の総数を求めよ
 
 */
 // END CUT HERE
