38d1331fa3c69f4134df9a6838a2a77a14e324f3
firewood <karamaki@gmail.com>
2018-10-24 22:54:53 +0900

    translate

--- srm_7xx/srm_740/GetAccepted.cpp
+++ srm_7xx/srm_740/GetAccepted.cpp
@@ -2,77 +2,10 @@
 /*
 SRM 740 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-For this task we have created a simple automated system.
-It was supposed to ask you a question by giving you a string question.
-You would have returned a string with the answer, and that would be all.
-Here is the entire conversation the way we planned it:
-
-"Do you want to get accepted?"
-"True."
-
-Unluckily for you, a hacker got into our system and modified it by inserting negations into the sentence. Each negation completely changes the meaning of the question to the opposite one, which means that you need to give us the opposite answer.
-
-Write a program that will read the question and answer accordingly.
-More precisely, your program must return either the string "True." if the question you are given has the same meaning as the one shown above or the string "False." if the question has the opposite meaning.
-
-
-DEFINITION
-Class:GetAccepted
-Method:answer
-Parameters:string
-Returns:string
-Method signature:string answer(string question)
-
-
-NOTES
--All strings in this problem are case-sensitive. In particular, make sure the strings your program returns have correct uppercase and lowercase letters, as shown in the statement and examples.
-
-
-CONSTRAINTS
--question will always have the following form: "Do you " + X + "want to " + Y + "get " + Z + "accepted?", where each of X, Y and Z is the concatenation of zero or more copies of the string "not ".
--question will have at most 1000 characters.
-
-
-EXAMPLES
-
-0)
-"Do you want to get accepted?"
-
-Returns: "True."
-
-This is the original question, you should give the original answer.
-
-
-1)
-"Do you not want to get accepted?"
-
-Returns: "False."
-
-This question has the opposite meaning from the original, you should give the opposite answer.
-
-
-2)
-"Do you want to not get accepted?"
-
-Returns: "False."
-
-This is another possible negation of the original question.
-
-
-3)
-"Do you want to get not not accepted?"
-
-Returns: "True."
-
-Two negations cancel each other out. The meaning of this question is the same as the meaning of the original question, so you should answer "True.".
-
-
-4)
-"Do you not want to not get not not not accepted?"
-
-Returns: "False."
+問題
+-質問に対してTrueまたはFalseを返すシステムを作成している
+-肯定文や二重否定文に対してはTrue、否定文に対してはFalseを返したい
+-質問に対する適切な応答を作成せよ
 
 */
 // END CUT HERE
