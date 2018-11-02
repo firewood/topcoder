0a97e07e99040c7eeb7a2f9ae56b6443133656ab
firewood <karamaki@gmail.com>
2018-11-02 23:04:32 +0900

    translate

--- srm_7xx/srm_741/DigitStringDiv2.cpp
+++ srm_7xx/srm_741/DigitStringDiv2.cpp
@@ -2,65 +2,9 @@
 /*
 SRM 741 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-You are given a digit string S and a positive integer integer X.
-We want to select a non-empty contiguous substring of S such that:
-
-The substring does not begin with the digit '0'.
-When you convert it to a number, its value is strictly greater than X.
-
-Two ways of selecting a substring are different if they begin or end at a different index. 
-
-Compute and return the number of ways in which we can select a substring with the above properties.
-
-
-DEFINITION
-Class:DigitStringDiv2
-Method:count
-Parameters:string, int
-Returns:int
-Method signature:int count(string S, int X)
-
-
-CONSTRAINTS
--S will contain between 1 and 9 characters, inclusive.
--Each character of S will be a digit ('0'-'9').
--X will be between 0 and 777,444,111, inclusive.
-
-
-EXAMPLES
-
-0)
-"0"
-1
-
-Returns: 0
-
-
-1)
-"10"
-9
-
-Returns: 1
-
-
-2)
-"471"
-47
-
-Returns: 2
-
-We can select either the substring "471" or the substring "71".
-
-
-3)
-"2222"
-97
-
-Returns: 3
-
-We can select the entire string S ("2222"). We also have two different ways to select the string "222": either we choose the first three or the last three characters of S
+問題
+-文字列Sと数値Xが与えられる
+-Sの部分文字列のうち先頭が0でなくXより大きいものの個数を求めよ
 
 */
 // END CUT HERE
