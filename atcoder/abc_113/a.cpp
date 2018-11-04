8e62f10f05f9be6e2c2c30fa3a1823b33fa898c7
firewood <karamaki@gmail.com>
2018-11-04 23:32:11 +0900

    ABC #113 A

---
+++ atcoder/abc_113/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int x, y;
+	cin >> x >> y;
+	int ans = x + y / 2;
+	cout << ans << endl;
+	return 0;
+}
