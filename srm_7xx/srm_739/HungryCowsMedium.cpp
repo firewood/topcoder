34fd9ff916dfb7545fdcec316f361a21011b3673
firewood <karamaki@gmail.com>
2018-11-08 21:53:58 +0900

    Translate

--- srm_7xx/srm_739/HungryCowsMedium.cpp
+++ srm_7xx/srm_739/HungryCowsMedium.cpp
@@ -2,75 +2,13 @@
 /*
 SRM 739 Div1 Medium (500)
 
-PROBLEM STATEMENT
-
-There is a herd of hungry cows.
-You are given the vector <int> cowAppetites.
-Each element of cowAppetites is the number of units of food one of the cows needs.
-
-There is also a long straight road with a coordinate system.
-All the cows are currently standing at coordinate 0.
-There are some barns along the road.
-All barns are at distinct positive integer coordinates.
-You are given the positions of all barns in the vector <int> barnPositions.
-Each barn contains an unlimited supply of food.
-There is no other food anywhere else.
-
-During each unit of time, each cow can choose one of three actions:
-
-Do nothing.
-Move by one unit of distance in either direction.
-Eat one unit of food, if able.
-
-A cow can only eat when it is at the same coordinate as one of the barns.
-Additionally, during each unit of time each barn can only serve one cow, so if there are multiple cows at a barn at the same time, only one of them can choose to eat during the next unit of time.
-Each cow can combine movement and eating arbitrarily.
-For example, a cow can move to a barn, eat 2 units of food, move to another barn, and eat 3 units of food there.
-
-If all cows carefully make a plan, what is the earliest time in which all of them will finish eating?
-
-
-DEFINITION
-Class:HungryCowsMedium
-Method:getWellFedTime
-Parameters:vector <int>, vector <int>
-Returns:long long
-Method signature:long long getWellFedTime(vector <int> cowAppetites, vector <int> barnPositions)
-
-
-CONSTRAINTS
--cowAppetites will contain between 1 and 300 elements, inclusive.
--Each element of cowAppetites will be between 1 and 1,000,000,000, inclusive.
--barnPositions will contain between 1 and 300 elements, inclusive.
--Each element of barnPositions will be between 1 and 1,000,000,000, inclusive.
--All elements of barnPositions will be distinct.
-
-
-EXAMPLES
-
-0)
-{3}
-{5}
-
-Returns: 8
-
-A cow that needs 3 units of food and a barn at coordinate 5. The cow will need 5 units of time to reach the barn and then another 3 units of time to eat as much as it needs.
-
-
-1)
-{1,1,1,1,1}
-{2,3}
-
-Returns: 5
-
-
-2)
-{4,4,4}
-{4,2}
-
-Returns: 9
-
-The inputs are not necessarily sorted.
+問題
+-直線状の農場に牛がいる
+-牛が必要とする餌の量cowAppetitesと、餌場の座標barnPositionsで与えられる
+-それぞれの餌場の供給量は無尽蔵である
+-牛は単位時間に距離1を移動するか餌を1食べる
+-他の牛が食べている餌場では食べられない
+-全ての牛が餌を食べ終わるのに必要な最小時間を求めよ
 
 */
 // END CUT HERE
