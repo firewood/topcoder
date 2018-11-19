d7aefec8443e79900d0e0d9379e1b282976516a4
firewood <karamaki@gmail.com>
2018-11-19 23:57:22 +0900

    Fix

--- atcoder/arc_029/a.cpp
+++ atcoder/arc_029/a.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < n; ++i) {
 		cin >> m[i];
 	}
-	sort(m, m + n);
+	sort(m, m + 4);
 	int ans = min({
 		max(m[0] + m[1] + m[2], m[3]),
 		max(m[0] + m[3], m[1] + m[2]),
