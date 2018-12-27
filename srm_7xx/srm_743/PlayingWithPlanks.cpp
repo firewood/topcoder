93d2931931becd1ca07ddffc5ca7c47ee5c95e7a
firewood <karamaki@gmail.com>
2018-12-27 22:41:41 +0900

    translate

--- srm_7xx/srm_743/PlayingWithPlanks.cpp
+++ srm_7xx/srm_743/PlayingWithPlanks.cpp
@@ -2,76 +2,9 @@
 /*
 SRM 743 Div2 Easy (250)
 
-PROBLEM STATEMENT
-
-You are given a plank of length plankLength. You want to cut the plank into exactly pieces pieces such that the length of each piece is a (positive) integer and all the lengths are distinct.
-
-If it is possible to cut the plank in the described way, return "possible". Otherwise return "impossible".
-
-
-DEFINITION
-Class:PlayingWithPlanks
-Method:canItBeDone
-Parameters:int, int
-Returns:string
-Method signature:string canItBeDone(int plankLength, int pieces)
-
-
-NOTES
--Note that the return value is case-sensitive.
-
-
-CONSTRAINTS
--plankLength will be between 1 and 1,000,000, inclusive.
--pieces will be between 1 and 1,000, inclusive.
-
-
-EXAMPLES
-
-0)
-9
-3
-
-Returns: "possible"
-
-One possibility is to cut the plank into pieces of lengths 1, 3 and 5.
-
-
-1)
-12
-1
-
-Returns: "possible"
-
-
-2)
-9
-4
-
-Returns: "impossible"
-
-It can be shown that there is no way to cut a plank of length 9 into 4 pieces of different integer lengths.
-
-
-3)
-10
-4
-
-Returns: "possible"
-
-
-4)
-750932
-901
-
-Returns: "possible"
-
-
-5)
-54
-10
-
-Returns: "impossible"
+問題
+-長さがplankLengthの木材がある
+-全ての長さが整数かつ異なるように切るとき、pieces本に分割できるかどうかを求めよ
 
 */
 // END CUT HERE
