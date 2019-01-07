ac2a6f40a4424cb8fc07676fb3f339524884c65f
firewood <karamaki@gmail.com>
2019-01-07 23:22:29 +0900

    Educational DP A

---
+++ atcoder/dp/2018_a.cpp
@@ -0,0 +1,27 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	cin >> n;
+	vector<int> h(n), cost(n, 1 << 30);
+	for (int i = 0; i < n; ++i) {
+		cin >> h[i];
+	}
+	cost[0] = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 1; j <= 2; ++j) {
+			if (i + j < n) {
+				cost[i + j] = min(cost[i + j], cost[i] + abs(h[i + j] - h[i]));
+			}
+		}
+	}
+	cout << cost[n - 1] << endl;
+	return 0;
+}
