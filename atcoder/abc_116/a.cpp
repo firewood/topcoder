d5f325c78849648386029dbe85a43b47756f3859
firewood <karamaki@gmail.com>
2019-01-20 23:01:49 +0900

    ABC #116 A

---
+++ atcoder/abc_116/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, b, c;
+	cin >> a >> b >> c;
+	int ans = a * b / 2;
+	cout << ans << endl;
+	return 0;
+}
