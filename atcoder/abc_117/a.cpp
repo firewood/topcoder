a4e5dba2936bfd3163a08377e0952013c9aecb55
firewood <karamaki@gmail.com>
2019-02-03 22:43:45 +0900

    ABC #117 A

---
+++ atcoder/abc_117/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout.precision(10);
+	double t, x;
+	cin >> t >> x;
+	double ans = t / x;
+	cout << ans << endl;
+	return 0;
+}
