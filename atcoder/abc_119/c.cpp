2604fa23857c069fe981e4860d9b8f8d4818d1bc
firewood <karamaki@gmail.com>
2019-03-03 00:47:10 +0900

    brute-force

--- atcoder/abc_119/c.cpp
+++ atcoder/abc_119/c.cpp
@@ -8,55 +8,41 @@
 
 using namespace std;
 
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int n;
+int target[3];
+int fragment[8];
+
+int dfs(int i, int used) {
+	if (i >= 3) return 0;
+	int min_cost = 1 << 30;
+	int bm = 1 << n;
+	for (int t = 1; t < bm; ++t) {
+		if (used & t) continue;
+		int len = 0, cost = 0;
+		for (int i = 0; i < n; ++i) {
+			if ((1 << i) & t) {
+				if (len) {
+					cost += 10;
+				}
+				len += fragment[i];
+			}
+		}
+		cost += abs(target[i] - len);
+		cost += dfs(i + 1, used | t);
+		min_cost = min(min_cost, cost);
+	}
+	return min_cost;
+}
 
 int main(int argc, char *argv[]) {
-	int n, a, b, c, ans = 1 << 30;
-	cin >> n >> a >> b >> c;
-	vector<int> l(n);
+	cin >> n;
+	for (int i = 0; i < 3; ++i) {
+		cin >> target[i];
+	}
 	for (int i = 0; i < n; ++i) {
-		cin >> l[i];
+		cin >> fragment[i];
 	}
-	int bm = 1 << n;
-	for (int i = 1; i < bm; ++i) {
-		int acost = 0, alen = 0;
-		for (int x = 0; x < n; ++x) {
-			if (i & (1 << x)) {
-				if (alen > 0) {
-					acost += 10;
-				}
-				alen += l[x];
-			}
-		}
-		acost += abs(a - alen);
-		for (int j = 1; j < bm; ++j) {
-			if (i & j) continue;
-			int blen = 0, bcost = acost;
-			for (int x = 0; x < n; ++x) {
-				if (j & (1 << x)) {
-					if (blen > 0) {
-						bcost += 10;
-					}
-					blen += l[x];
-				}
-			}
-			bcost += abs(b - blen);
-			for (int k = 1; k < bm; ++k) {
-				if ((i & k) != 0 || (j & k) != 0) continue;
-				int clen = 0, ccost = bcost;
-				for (int x = 0; x < n; ++x) {
-					if (k & (1 << x)) {
-						if (clen > 0) {
-							ccost += 10;
-						}
-						clen += l[x];
-					}
-				}
-				ccost += abs(c - clen);
-				ans = min(ans, ccost);
-			}
-		}
-	}
+	int ans = dfs(0, 0);
 	cout << ans << endl;
 	return 0;
 }
