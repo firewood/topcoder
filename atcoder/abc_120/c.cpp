11df9df0c7be948e4462498f1c1f9867b3f5a946
firewood <karamaki@gmail.com>
2019-03-09 00:14:17 +0900

    using stack

--- atcoder/abc_120/c.cpp
+++ atcoder/abc_120/c.cpp
@@ -2,37 +2,22 @@
 
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <algorithm>
-#include <list>
+#include <queue>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	string s;
 	cin >> s;
-	int n = (int)s.length();
-	list<int> a;
-	for (int i = 0; i < n; ++i) {
-		a.push_back(s[i] != '0');
-	}
 	int ans = 0;
-	list<int>::iterator it = a.begin();
-	while (it != a.end()) {
-		list<int>::iterator next = it;
-		++next;
-		if (next == a.end()) {
-			break;
-		}
-		if (*it == *next) {
-			++it;
-		} else {
+	deque<char> q;
+	for (char c : s) {
+		q.push_front(c);
+		while (q.size() >= 2 && q[0] != q[1]) {
 			ans += 2;
-			it = a.erase(it);
-			it = a.erase(it);
-			if (it != a.begin()) {
-				--it;
-			}
+			q.pop_front();
+			q.pop_front();
 		}
 	}
 	cout << ans << endl;
