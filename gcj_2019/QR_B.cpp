f807279a4ad5f1a057b1eb037c191aa7e44df067
firewood <karamaki@gmail.com>
2019-04-11 00:50:12 +0900

    simplified

--- gcj_2019/QR_B.cpp
+++ gcj_2019/QR_B.cpp
@@ -9,37 +9,11 @@
 using namespace std;
 
 string solve(int n, string s) {
-	if (s[0] == 'E' && s.back() == 'S') {
-		return string(n - 1, 'S') + string(n - 1, 'E');
+	string ans;
+	for (char c : s) {
+		ans += c == 'E' ? 'S' : 'E';
 	}
-	if (s[0] == 'S' && s.back() == 'E') {
-		return string(n - 1, 'E') + string(n - 1, 'S');
-	}
-	if (s[0] == 'E' && s.back() == 'E') {
-		int prev = 0, row = 0;
-		for (char c : s) {
-			if (c == 'S') {
-				++row;
-				if (prev == c) {
-					return string(row - 1, 'S') + string(n - 1, 'E') + string(n - row, 'S');
-				}
-			}
-			prev = c;
-		}
-	}
-	if (s[0] == 'S' && s.back() == 'S') {
-		int prev = 0, col = 0;
-		for (char c : s) {
-			if (c == 'E') {
-				++col;
-				if (prev == c) {
-					return string(col - 1, 'E') + string(n - 1, 'S') + string(n - col, 'E');
-				}
-			}
-			prev = c;
-		}
-	}
-	return "";
+	return ans;
 }
 
 int main(int argc, char *argv[]) {
