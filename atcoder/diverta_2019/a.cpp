c5a315d5023cfa35f9bb0a55afdc09df32698a86
Kenichi Aramaki <karamaki@gmail.com>
2019-05-11 08:45:33 -0700

    Diverta 2019 A

---
+++ atcoder/diverta_2019/a.cpp
@@ -0,0 +1,17 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int n, k;
+  cin >> n >> k;
+  int ans = n - k + 1;
+	cout << ans << endl;
+	return 0;
+}
