0e934735def7a271b1fb6b3f008f528c17b5a69f
firewood <karamaki@gmail.com>
2019-06-01 00:51:43 +0900

    M Solutions 2019 A

---
+++ atcoder/m_solutions_2019/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	cin >> n;
+	int ans = (n - 2) * 180;
+	cout << ans << endl;
+	return 0;
+}
