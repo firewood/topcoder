f3d5922f34219f7cc42f64b78458229329ecb39b
firewood <karamaki@gmail.com>
2019-06-10 00:41:44 +0900

    ABC #128 A

---
+++ atcoder/abc_128/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, p, ans;
+	cin >> a >> p;
+	ans = ((a * 3) + p) / 2;
+	cout << ans << endl;
+	return 0;
+}
