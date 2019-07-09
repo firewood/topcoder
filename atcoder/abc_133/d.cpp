96f7aa804431cb54b164b95cee77b3fe0e728e00
firewood <karamaki@gmail.com>
2019-07-10 01:28:10 +0900

    simplified

--- atcoder/abc_133/d.cpp
+++ atcoder/abc_133/d.cpp
@@ -13,39 +13,17 @@ int main(int argc, char *argv[]) {
 	int n;
 	cin >> n;
 	vector<LL> a(n), r(n);
+	LL all_sum = 0, odd_sum = 0;
 	for (int i = 0; i < n; ++i) {
 		cin >> a[i];
-	}
-	for (int i = 0; i < 2*n; ++i) {
-		LL& curr = a[i % n];
-		LL& next = a[(i + 1) % n];
-		LL diff = next - curr;
-		if (diff > 0) {
-			LL& nn = a[(i + 2) % n];
-			LL d = min(diff, nn);
-			r[(i + 2) % n] += 2 * d;
-			next -= d;
-			nn -= d;
-		} else {
-			LL& prev = a[(i - 1 + n) % n];
-			LL d = min(-diff, prev);
-			r[i % n] += 2 * d;
-			curr -= d;
-			prev -= d;
+		all_sum += a[i];
+		if (i % 2) {
+			odd_sum += a[i];
 		}
 	}
-	LL mn = *min_element(a.begin(), a.end());
-	for (int i = 0; i < n; ++i) {
-		a[i] -= mn;
-		r[i] += mn;
-	}
-	for (int i = 0; i < 2 * n; ++i) {
-		LL& curr = a[i % n];
-		LL& next = a[(i + 1) % n];
-		LL d = min(curr, next);
-		r[(i + 1) % n] += 2 * d;
-		curr -= d;
-		next -= d;
+	r[0] = all_sum - 2 * odd_sum;
+	for (int i = 1; i < n; ++i) {
+		r[i] = 2 * a[i - 1] - r[i - 1];
 	}
 	for (int i = 0; i < n; ++i) {
 		if (i)cout << " ";
