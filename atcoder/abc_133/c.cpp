29b28b2a561bb35fce518dfef02db68882dbce85
firewood <karamaki@gmail.com>
2019-07-14 00:00:06 +0900

    simplified

--- atcoder/abc_133/c.cpp
+++ atcoder/abc_133/c.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
@@ -11,9 +10,12 @@ typedef long long LL;
 
 LL calc(LL a, LL b) {
 	LL r = 1LL << 32;
-	for (LL i = a; i < min(a + 2020, b); ++i) {
-		for (LL j = a + 1; j <= min(a + 2022, b); ++j) {
+	for (LL i = a; i < b; ++i) {
+		for (LL j = a + 1; j <= b; ++j) {
 			r = min(r, (i * j) % 2019);
+			if (!r) {
+				return r;
+			}
 		}
 	}
 	return r;
@@ -23,7 +25,6 @@ int main(int argc, char *argv[]) {
 	LL l, r;
 	cin >> l >> r;
 	LL ans = calc(l, r);
-	ans = min(ans, calc(max(l, r - 2020), r));
 	cout << ans << endl;
 	return 0;
 }
