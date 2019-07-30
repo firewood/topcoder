18b10a578a613bb40d404f7ecc71a1a260abd51a
firewood <karamaki@gmail.com>
2019-07-31 00:21:06 +0900

    ABC #130 A

---
+++ atcoder/abc_130/a.cpp
@@ -0,0 +1,14 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int x, a;
+	cin >> x >> a;
+	cout << (x < a ? 0 : 10) << endl;
+	return 0;
+}
