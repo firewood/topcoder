045ba443237d10dfbea3a9f7c82cae3401d45b25
Kenichi Aramaki <karamaki@gmail.com>
2019-08-23 12:58:06 +0900

    ABC 134 A

---
+++ atcoder/abc_134/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int r;
+  cin >> r;
+  int ans = 3 * r * r;
+  cout << ans << endl;
+  return 0;
+}
