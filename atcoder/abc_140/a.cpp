88d8eae0a3edeec6412224150229ae733f271df0
firewood <karamaki@gmail.com>
2019-09-08 00:32:07 -0700

    ABC #140 A

---
+++ atcoder/abc_140/a.cpp
@@ -0,0 +1,18 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int n;
+	cin >> n;
+	int ans = n * n * n;
+	cout << ans << endl;
+	return 0;
+}
