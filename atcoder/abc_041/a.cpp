83bb489870919116ae9d2456c0a37df64fb03207
firewood <karamaki@gmail.com>
2019-11-16 00:47:10 +0900

    ABC #041 A

---
+++ atcoder/abc_041/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string s;
+	int i;
+	cin >> s >> i;
+	cout << s[i - 1] << endl;
+	return 0;
+}
