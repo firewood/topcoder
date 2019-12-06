3f4af0f66657fecf3d402ecf60ff2b3f59601452
firewood <karamaki@gmail.com>
2019-12-07 01:00:36 +0900

    Sumitomo Trust Bank 2019 A

---
+++ atcoder/sumitrust_2019/a.cpp
@@ -0,0 +1,16 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int ans = a != c;
+	cout << ans << endl;
+	return 0;
+}
