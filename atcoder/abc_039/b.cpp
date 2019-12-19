533c757fdc9d172c342f8426aedd033ca59278a7
firewood <karamaki@gmail.com>
2019-12-20 00:15:21 +0900

    ABC #039 B

---
+++ atcoder/abc_039/b.cpp
@@ -0,0 +1,17 @@
+// B.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	cin >> n;
+	int ans = sqrt(sqrt(n));
+	cout << ans << endl;
+	return 0;
+}
