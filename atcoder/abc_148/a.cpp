d1fa3e0b7b4002d2e9aa92a67f5bdff033eaf7e9
firewood <karamaki@gmail.com>
2020-01-02 01:08:55 +0900

    ABC #148 A

---
+++ atcoder/abc_148/a.cpp
@@ -0,0 +1,16 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int a, b;
+	cin >> a >> b;
+	int ans = 6 - a - b;
+	cout << ans << endl;
+	return 0;
+}
