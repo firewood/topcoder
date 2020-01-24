4172c37a03536f95e0623f0238f6acb2c00888cc
firewood <karamaki@gmail.com>
2020-01-25 00:06:22 +0900

    ABC #152 A

---
+++ atcoder/abc_152/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int n, m;
+	cin >> n >> m;
+	cout << (m >= n ? "Yes" : "No") << endl;
+	return 0;
+}
