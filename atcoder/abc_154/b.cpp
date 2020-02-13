fe3ca4121efd3efdf9d14469b86341e09e910b16
firewood <karamaki@gmail.com>
2020-02-14 00:07:05 +0900

    ABC #154 B

---
+++ atcoder/abc_154/b.cpp
@@ -0,0 +1,17 @@
+// B.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string s;
+	cin >> s;
+	int len = s.length();
+	string ans(len, 'x');
+	cout << ans << endl;
+	return 0;
+}
