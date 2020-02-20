01885c00f1a224e2e147f6b0f51516a44c7d5772
firewood <karamaki@gmail.com>
2020-02-21 02:20:51 +0900

    ABC #151 A

---
+++ atcoder/abc_151/a.cpp
@@ -0,0 +1,16 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string x;
+	cin >> x;
+	x[0] += 1;
+	cout << x << endl;
+	return 0;
+}
