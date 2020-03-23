891677a98bfa1cf762c36140d12015048260dc51
firewood <karamaki@gmail.com>
2020-03-24 00:22:18 +0900

    Codefesta 2014 Qual B D

--- atcoder/codefesta_2014/qb_d.cpp
+++ atcoder/codefesta_2014/qb_d.cpp
@@ -3,51 +3,38 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <cmath>
+#include <set>
+#include <map>
 #include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int N;
-	string s;
-	{
-		getline(cin, s);
-		stringstream ss(s);
-		ss >> N;
+	int n = -1, h;
+	cin >> n;
+	map<int, vector<int>> m;
+	for (int i = 0; i < n; ++i) {
+		cin >> h;
+		m[h].push_back(i);
 	}
-	static int v[1 << 18];
-	for (int i = 0; i < N; ++i) {
-		getline(cin, s);
-		stringstream ss(s);
-		ss >> v[i];
-	}
-
-	static int cnt[1 << 18];
-	for (int i = 0; i < N; ++i) {
-		int h = v[i];
-		for (int j = i - 1; j >= 0; --j) {
-			if (v[j] >= h) {
-				++cnt[j];
-			}
-			h = max(h, v[j]);
+	set<int> s;
+	s.insert(-1);
+	s.insert(n);
+	vector<int> ans(n);
+	for (auto it = m.rbegin(); it != m.rend(); ++it) {
+		for (auto i : it->second) {
+			auto ed = s.lower_bound(i);
+			auto st = ed;
+			--st;
+			ans[i] = *ed - *st - 2;
 		}
-		h = v[i];
-		for (int j = i + 1; j < N; ++j) {
-			if (v[j] >= h) {
-				++cnt[j];
-			}
-			h = max(h, v[j]);
+		for (auto i : it->second) {
+			s.insert(i);
 		}
 	}
-
-	for (int i = 0; i < N; ++i) {
-		cout << cnt[i] << endl;
+	for (int i = 0; i < n; ++i) {
+		cout << ans[i] << endl;
 	}
-
 	return 0;
 }
