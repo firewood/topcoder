625d6013c587d4d3b7dcf885ae552f5592e8c6ad
firewood <karamaki@gmail.com>
2020-04-02 01:13:31 +0900

    ABC #027 A

---
+++ atcoder/abc_027/a.cpp
@@ -0,0 +1,18 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a[3];
+	cin >> a[0] >> a[1] >> a[2];
+	sort(a, a + 3);
+	int ans = a[0] == a[1] ? a[2] : a[0];
+	cout << ans << endl;
+	return 0;
+}
