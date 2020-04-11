29f2ef9ccfc950be4e4aaa40383450c31353095d
firewood <karamaki@gmail.com>
2020-04-12 00:15:28 +0900

    large

--- gcj_2020/R1A_A.cpp
+++ gcj_2020/R1A_A.cpp
@@ -1,60 +1,45 @@
 // Google Code Jam 2020 Round 1A
-// Problem A.
+// Problem A. Pattern Matching
 
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <map>
-#include <set>
 #include <cstring>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
-typedef pair<int, int> II;
-
 string solve(int n, vector<string> w) {
-	vector<string> l(n), r(n);
-	vector<II> v, rv;
-	vector<int> pos(n);
+	string left, right;
+	vector<size_t> lpos(n), rpos(n);
 	for (int i = 0; i < n; ++i) {
-		int p;
-		while ((p = w[i].find("**")) > 0) {
-			w[i] = w[i].substr(0, p) + w[i].substr(p + 1);
+		lpos[i] = w[i].find('*');
+		string s = w[i].substr(0, lpos[i]);
+		size_t len = min(left.length(), s.length());
+		if (left.substr(0, len) != s.substr(0, len)) {
+			return "";
 		}
-		p = w[i].find("*");
-		l[i] = w[i].substr(0, p);
-		r[i] = w[i].substr(p + 1);
-		v.push_back(II(l[i].length(), i));
-		rv.push_back(II(r[i].length(), i));
-	}
-	string ans;
-	sort(v.rbegin(), v.rend());
-	sort(rv.rbegin(), rv.rend());
-	string x = l[v[0].second];
-	string y = r[rv[0].second];
-	for (int j = 0; j <= (int)y.length(); ++j) {
-		string z = x + y.substr(y.length() - j);
-		bool ok = true;
-		for (int i = 0; i < n; ++i) {
-			if (z.length() < l[i].length() + r[i].length()) {
-				ok = false;
-				break;
-			}
-			if (z.substr(0, l[i].length()) == l[i] && z.substr(z.length() - r[i].length()) == r[i]) {
-				;
-			} else {
-				ok = false;
-				break;
-			}
+		if (s.length() > left.length()) {
+			left = s;
 		}
-		if (ok) {
-			ans = z;
-			break;
+		rpos[i] = w[i].rfind('*') + 1;
+		s = w[i].substr(rpos[i]);
+		len = min(right.length(), s.length());
+		if (right.substr(right.length() - len) != s.substr(s.length() - len)) {
+			return "";
+		}
+		if (s.length() > right.length()) {
+			right = s;
 		}
 	}
-	return ans;
+	for (int i = 0; i < n; ++i) {
+		for (size_t j = lpos[i]; j < rpos[i]; ++j) {
+			if (w[i][j] != '*') {
+				left += w[i][j];
+			}
+		}
+	}
+	return left + right;
 }
 
 int main(int argc, char* argv[]) {
