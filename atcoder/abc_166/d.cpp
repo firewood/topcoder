325cab214c48821b1a523e07c00eeb4da207499f
firewood <karamaki@gmail.com>
2020-05-06 00:53:53 +0900

    simplified

--- atcoder/abc_166/d.cpp
+++ atcoder/abc_166/d.cpp
@@ -3,32 +3,20 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
 
 using namespace std;
 typedef long long LL;
-typedef pair<LL, LL> LLLL;
-
-LLLL solve(LL x) {
-	map<LL, LL> m;
-	for (LL i = -2000; i < 2000; ++i) {
-		m[i * i * i * i * i] = i;
-	}
-	for (auto kv : m) {
-		LL y = kv.first - x;
-		if (m.find(y) != m.end()) {
-			return LLLL(-m[y], -kv.second);
-		}
-	}
-	return { 0,0 };
-}
 
 int main(int argc, char* argv[]) {
-	LL x = -1;
+	LL x;
 	cin >> x;
-	LLLL ans = solve(x);
-	cout << ans.first << " " << ans.second << endl;
+	for (LL a = -500; a <= 500; ++a) {
+		for (LL b = -500; b <= 500; ++b) {
+			if (a * a * a * a * a - b * b * b * b * b == x) {
+				cout << a << " " << b << endl;
+				return 0;
+			}
+		}
+	}
 	return 0;
 }
