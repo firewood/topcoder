284bb5282a7f05ff391d37459436676f661db43f
firewood <karamaki@gmail.com>
2020-05-05 11:22:32 +0900

    tuple

--- atcoder/abc_165/c.cpp
+++ atcoder/abc_165/c.cpp
@@ -2,49 +2,46 @@
 
 #include <iostream>
 #include <sstream>
+#include <tuple>
 #include <algorithm>
 #include <numeric>
 #include <vector>
 
 using namespace std;
 
-typedef pair<int, int> II;
-typedef pair<II, II> IIII;
 typedef long long LL;
 
-int main(int argc, char* argv[]) {
-	int n, m, q;
-	cin >> n >> m >> q;
-	vector<IIII> v(q);
-	for (int i = 0; i < q; ++i) {
-		cin >> v[i].first.first >> v[i].first.second >> v[i].second.first >> v[i].second.second;
-		v[i].first.first -= 1;
-		v[i].first.second -= 1;
-	}
-	vector<vector<int>> seq;
-	for (int i = 1; i <= m; ++i) {
-		seq.emplace_back(vector<int>(1, i));
-	}
-	for (int i = 2; i <= n; ++i) {
-		vector<vector<int>> next;
-		for (auto v : seq) {
-			for (int d = v.back(); d <= m; ++d) {
-				vector<int> w = v;
-				w.emplace_back(d);
-				next.emplace_back(w);
-			}
+int n, m, q;
+vector<tuple<int, int, int, int>> v;
+LL ans;
+
+void dfs(int c, vector<int> &seq) {
+	if (c < n) {
+		for (int d = seq[c - 1]; d <= m; ++d) {
+			seq[c] = d;
+			dfs(c + 1, seq);
 		}
-		seq = next;
-	}
-	LL ans = 0;
-	for (auto a : seq) {
+	} else {
 		LL score = 0;
 		for (int i = 0; i < q; ++i) {
-			if (a[v[i].first.second] - a[v[i].first.first] == v[i].second.first) {
-				score += v[i].second.second;
+			if (seq[get<1>(v[i])] - seq[get<0>(v[i])] == get<2>(v[i])) {
+				score += get<3>(v[i]);
 			}
 		}
 		ans = max(ans, score);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	cin >> n >> m >> q;
+	for (int i = 0; i < q; ++i) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		v.emplace_back(a - 1, b - 1, c, d);
+	}
+	for (int i = 1; i <= m; ++i) {
+		vector<int> seq(n, i);
+		dfs(1, seq);
 	}
 	cout << ans << endl;
 	return 0;
