1aa695136e00118277093cee128dc8bfde21b6a3
firewood <karamaki@gmail.com>
2020-05-05 13:27:31 +0900

    recursive lambda

--- atcoder/abc_165/c.cpp
+++ atcoder/abc_165/c.cpp
@@ -9,40 +9,51 @@
 
 using namespace std;
 
-typedef long long LL;
+template<typename F> class [[nodiscard]] FixPoint : private F {
+public:
+	explicit constexpr FixPoint(F && f) noexcept : F(std::forward<F>(f)) {}
 
-int n, m, q;
-vector<tuple<int, int, int, int>> v;
-LL ans;
-
-void dfs(int c, vector<int> &seq) {
-	if (c < n) {
-		for (int d = seq[c - 1]; d <= m; ++d) {
-			seq[c] = d;
-			dfs(c + 1, seq);
-		}
-	} else {
-		LL score = 0;
-		for (int i = 0; i < q; ++i) {
-			if (seq[get<1>(v[i])] - seq[get<0>(v[i])] == get<2>(v[i])) {
-				score += get<3>(v[i]);
-			}
-		}
-		ans = max(ans, score);
+	template<typename... Args> constexpr decltype(auto)
+	operator()(Args &&... args) const noexcept(noexcept(F::operator()(std::declval<FixPoint>(), std::declval<Args>()...))) {
+		return F::operator()(*this, std::forward<Args>(args)...);
 	}
+};
+
+template<typename F> static inline constexpr decltype(auto)
+makeFixPoint(F&& f) noexcept {
+	return FixPoint<F>{ std::forward<F>(f) };
 }
 
-int main(int argc, char* argv[]) {
+int main() {
+	int n, m, q, a, b, c, d;
+	vector<tuple<int, int, int, int>> v;
+
 	cin >> n >> m >> q;
 	for (int i = 0; i < q; ++i) {
-		int a, b, c, d;
 		cin >> a >> b >> c >> d;
-		v.emplace_back(a - 1, b - 1, c, d);
+		v.emplace_back(a, b, c, d);
 	}
-	for (int i = 1; i <= m; ++i) {
-		vector<int> seq(n, i);
-		dfs(1, seq);
-	}
+
+	auto push_back = [](vector<int> seq, int next) {
+		vector<int> v = seq;
+		seq.emplace_back(next);
+		return seq;
+	};
+	auto ans = makeFixPoint([&](auto dfs, vector<int> seq) -> int {
+		int r = 0;
+		if ((int)seq.size() <= n) {
+			for (int d = seq.back(); d <= m; ++d) {
+				r = max(r, dfs(push_back(seq, d)));
+			}
+		} else {
+			for (int i = 0; i < q; ++i) {
+				if (seq[get<1>(v[i])] - seq[get<0>(v[i])] == get<2>(v[i])) {
+					r += get<3>(v[i]);
+				}
+			}
+		}
+		return r;
+	})(push_back({}, 1));
 	cout << ans << endl;
 	return 0;
 }
