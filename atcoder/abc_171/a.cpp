8698016f08daa7f81d63e9a9002d5ce4ab0e42ac
firewood <karamaki@gmail.com>
2020-07-04 00:10:27 +0900

    ABC #171 A

---
+++ atcoder/abc_171/a.cpp
@@ -0,0 +1,15 @@
+// A.
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	char c;
+	cin >> c;
+	cout << (c < 'a' ? "A" : "a") << endl;
+	return 0;
+}
