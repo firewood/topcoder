2e5c24b68284b0390125e2328b54c53a1395519c
firewood <karamaki@gmail.com>
2020-08-12 01:30:10 +0900

    simplified

--- atcoder/agc_047/b.cpp
+++ atcoder/agc_047/b.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
@@ -14,54 +13,43 @@ struct Trie {
 	Trie* next[26];
 	int cnt[26];
 	Trie() {
-		memset(next, 0, sizeof(next));
-		memset(cnt, 0, sizeof(cnt));
+		fill_n(next, 26, (Trie *)NULL);
+		fill_n(cnt, 26, 0);
 	}
 };
 
 int main(int argc, char *argv[]) {
 	vector<Trie> pool(1000002);
-	int pi = 1;
-	LL n, ans = 0;
+	LL n, ans = 0, pi = 0;
 	cin >> n;
 	vector<string> s(n);
 	for (int i = 0; i < n; ++i) {
 		cin >> s[i];
 		int len = s[i].length();
-		vector<int> cf(len + 2);
+		vector<int> cf(len + 1);
 		for (int j = 0; j < len; ++j) {
-			cf[j + 2] = cf[j + 1] | (1 << (s[i][j] - 'a'));
+			cf[j + 1] = cf[j] | (1 << (s[i][j] - 'a'));
 		}
-		Trie* p = &(pool[0]);
-
-		for (int k = 0; k < 26; ++k) {
-			if (cf[len - 1 + 2] & (1 << k)) {
-				p->cnt[k] += 1;
-			}
-		}
-
+		Trie* node = &(pool[0]);
 		for (int j = len - 1; j >= 0; --j) {
-			char c = s[i][j] - 'a';
-			if (p->next[c] == 0) {
-				p->next[c] = &(pool[pi]);
-				++pi;
-			}
-			p = p->next[c];
 			for (int k = 0; k < 26; ++k) {
 				if (cf[j + 1] & (1 << k)) {
-					p->cnt[k] += 1;
+					node->cnt[k] += 1;
 				}
 			}
+			char c = s[i][j] - 'a';
+			if (node->next[c] == NULL) {
+				node->next[c] = &(pool[++pi]);
+			}
+			node = node->next[c];
 		}
 	}
 	for (int i = 0; i < n; ++i) {
-		int len = s[i].length();
-		Trie* p = &(pool[0]);
-		for (int j = len - 1; j > 0; --j) {
-			char c = s[i][j] - 'a';
-			p = p->next[c];
+		Trie* node = &(pool[0]);
+		for (int j = s[i].length() - 1; j > 0; --j) {
+			node = node->next[s[i][j] - 'a'];
 		}
-		ans += p->cnt[s[i][0] - 'a'] - 1;
+		ans += node->cnt[s[i][0] - 'a'] - 1;
 	}
 	cout << ans << endl;
 	return 0;
