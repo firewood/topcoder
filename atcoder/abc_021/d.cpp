6024d7ec3e86998ac5422767455a35186bd7cc3b
firewood <karamaki@gmail.com>
2020-10-05 00:13:43 +0900

    ABC #021 D

---
+++ atcoder/abc_021/d.cpp
@@ -0,0 +1,80 @@
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-result"
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+
+static const long long MOD = 1000000007;
+static const size_t MAX_N = 200001;
+
+struct modll {
+	long long x;
+	modll() { }
+	modll(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modll operator+(int y) { return (x + y + MOD) % MOD; }
+	modll operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modll operator-(int y) { return (x - y + MOD) % MOD; }
+	modll operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modll operator*(int y) { return (x * y) % MOD; }
+	modll operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modll operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modll operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modll modinv(int a) { return modpow(a, MOD - 2); }
+	static modll modpow(int a, int b) {
+		modll x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+modll combination(LL n, LL r) {
+	static modll fact[MAX_N + 1], inv[MAX_N + 1];
+	if (!fact[0]) {
+		fact[0] = 1;
+		for (int i = 1; i <= MAX_N; ++i) {
+			fact[i] = fact[i - 1] * i;
+		}
+		inv[MAX_N] = modll::modinv(fact[MAX_N]);
+		for (int i = MAX_N; i >= 1; --i) {
+			inv[i - 1] = inv[i] * i;
+		}
+	}
+	if (r > n) {
+		return 0;
+	}
+	return (fact[n] * inv[r]) * inv[n - r];
+}
+
+// èdï°ëgçáÇπ
+modll nHr(LL n, LL r) {
+	return combination(n + r - 1, r);
+}
+
+void solve(long long n, long long k) {
+	modll ans = nHr(n, k);
+	cout << ans << endl;
+}
+
+int main() {
+	long long n, k;
+	std::cin >> n >> k;
+	solve(n, k);
+	return 0;
+}
