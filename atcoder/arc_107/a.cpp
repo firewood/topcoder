357fa4e8759f510dce97194fde770a204d862a5c
firewood <karamaki@gmail.com>
2020-11-01 01:55:18 +0900

    ARC #107 A

---
+++ atcoder/arc_107/a.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+typedef long long LL;
+
+static const long long MOD = 998244353;
+
+struct modll {
+	long long x;
+	modll() { }
+	modll(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modll operator+(int y) { return (x + y + MOD) % MOD; }
+	modll operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modll operator-(int y) { return (x - y + MOD) % MOD; }
+	modll operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modll operator*(int y) { return (x * y) % MOD; }
+	modll operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modll operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modll operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modll modinv(int a) { return modpow(a, MOD - 2); }
+	static modll modpow(int a, int b) {
+		modll x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+void solve(int A, int B, int C) {
+	modll ans = A;
+	ans *= A + 1;
+	ans *= B;
+	ans *= B + 1;
+	ans *= C;
+	ans *= C + 1;
+	ans /= 8;
+	cout << ans << endl;
+}
+
+int main() {
+	int A, B, C;
+	std::cin >> A >> B >> C;
+	solve(A, B, C);
+	return 0;
+}
