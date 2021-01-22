1cd31f20b4255dc1e516b0797f8f31475b15113c
firewood <karamaki@gmail.com>
2021-01-23 01:02:51 +0900

    ABC #188 D

--- atcoder/abc_188/d.cpp
+++ atcoder/abc_188/d.cpp
@@ -13,59 +13,42 @@
 using namespace std;
 
 typedef long long LL;
-typedef long double LD;
-typedef pair<LL, LL> II;
-typedef pair<II, LL> III;
-typedef priority_queue<II, vector<II>, greater<>> Queue;
-static const LL INF = 1LL << 60;
 
 void solve(long long N, long long C, std::vector<long long> &a, std::vector<long long> &b, std::vector<long long> &c) {
-	LL ans = 0, sum = 0, last = 0;
+	vector<int> aseq(N), bseq(N);
+	iota(aseq.begin(), aseq.end(), 0);
+	iota(bseq.begin(), bseq.end(), 0);
+	sort(aseq.begin(), aseq.end(), [&](int lhs, int rhs) {
+		return a[lhs] < a[rhs];
+	});
+	sort(bseq.begin(), bseq.end(), [&](int lhs, int rhs) {
+		return b[lhs] < b[rhs];
+	});
 	set<LL> days;
-	vector<III> v;
 	for (int i = 0; i < N; i++) {
-		v.emplace_back(III(II(a[i], b[i]), i));
 		days.insert(a[i]);
-		days.insert(b[i] + 1);
+		days.insert(++b[i]);
 	}
-	sort(v.begin(), v.end());
-	Queue q;
-	int index = 0;
-	last = *(days.begin());
+	LL ans = 0, sum = 0, last_day = 0, ai = 0, bi = 0;
 	for (LL day : days) {
-		LL cost = min(sum, C);
-		ans += cost * (day - last);
-		last = day;
-		while (!q.empty() && q.top().first == day) {
-			LL i = q.top().second;
-			sum -= c[i];
-			q.pop();
+		ans += min(sum, C) * (day - last_day);
+		last_day = day;
+		while (ai < N && a[aseq[ai]] == day) {
+			sum += c[aseq[ai++]];
 		}
-		while (index < N) {
-			int i = v[index].second;
-			if (a[i] > day) {
-				break;
-			}
-			sum += c[i];
-			q.emplace(II(b[i] + 1, i));
-			++index;
+		while (bi < N && b[bseq[bi]] == day) {
+			sum -= c[bseq[bi++]];
 		}
 	}
 	cout << ans << endl;
 }
 
 int main() {
-    long long N;
-	std::cin >> N;
-	long long C;
-	std::cin >> C;
-	std::vector<long long> a(N);
-	std::vector<long long> b(N);
-	std::vector<long long> c(N);
+	long long N, C;
+	std::cin >> N >> C;
+	std::vector<long long> a(N), b(N), c(N);
 	for (int i = 0; i < N; i++) {
-		std::cin >> a[i];
-		std::cin >> b[i];
-		std::cin >> c[i];
+		std::cin >> a[i] >> b[i] >> c[i];
 	}
 	solve(N, C, a, b, c);
 	return 0;
