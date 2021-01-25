6564c64404e379477e6a6fb927a0501cd842cad0
firewood <karamaki@gmail.com>
2021-01-26 00:24:19 +0900

    AGC #051 B

---
+++ atcoder/agc_051/b.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	cout << 10*10*10 << endl;
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			for (int k = 0; k < 10; ++k) {
+				cout << (i + 100 * k) << " " << (i + 1000 * j) << endl;
+			}
+		}
+	}
+	return 0;
+}
