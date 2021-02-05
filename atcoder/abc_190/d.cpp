bbfe9e6f70621948fab8c7bf78df68333dd14e7c
firewood <karamaki@gmail.com>
2021-02-05 22:35:12 +0900

    use int128_t

--- atcoder/abc_190/d.cpp
+++ atcoder/abc_190/d.cpp
@@ -13,8 +13,15 @@
 using namespace std;
 
 typedef long long LL;
+#ifdef __GNUC__
+#define int128_t __int128
+#else
+#include <boost/multiprecision/cpp_int.hpp>
+typedef boost::multiprecision::int128_t int128_t;
+#endif
 
 LL solve(long long N) {
+#if 1
 	LL ans = 0;
 	for (LL i = 1; i * i <= N; ++i) {
 		LL j = N / i;
@@ -24,6 +31,20 @@ LL solve(long long N) {
 		}
 	}
 	return ans * 2;
+#else
+	LL N2 = N * 2;
+	LL ans = 0;
+	for (LL i = 1; i * i <= N2; ++i) {
+		LL j = N2 / i;
+		if (i * j == N2) {
+			ans += (N - (i * int128_t(i + 1)) / 2) % i == 0;
+			if (i != j) {
+				ans += (N - (j * int128_t(j + 1)) / 2) % j == 0;
+			}
+		}
+	}
+	return ans;
+#endif
 }
 
 int main() {
