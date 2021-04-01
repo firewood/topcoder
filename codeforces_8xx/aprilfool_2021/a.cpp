5c8fa5d730aa1fa3fcea275ca60bf84b94963997
firewood <karamaki@gmail.com>
2021-04-02 01:23:48 +0900

    Aprilfool 2021 A

---
+++ codeforces_8xx/aprilfool_2021/a.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		string S;
+		cin >> S;
+		if (S.empty()) break;
+		cout << "NO" << endl;
+	}
+	return 0;
+}
