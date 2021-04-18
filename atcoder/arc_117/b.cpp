79019eb313dd7dac72c853bb5048acbc0f730162
firewood <karamaki@gmail.com>
2021-04-19 01:06:50 +0900

    ARC #117 B

---
+++ atcoder/arc_117/b.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const long long MOD = 1000000007;
+
+struct modll {
+	long long x;
+	modll() { }
+	modll(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modll operator+(int y) { return (x + y + MOD) % MOD; }
+	modll operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modll operator-(int y) { return (x - y + MOD) % MOD; }
+	modll operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modll operator*(int y) { return (x * y) % MOD; }
+	modll operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modll operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modll operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modll modinv(int a) { return modpow(a, MOD - 2); }
+	static modll modpow(int a, int b) {
+		modll x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+int solve(int N, std::vector<int> &A) {
+	sort(A.begin(), A.end());
+	modll ans = 1;
+	for (int i = 0; i < N; ++i) {
+		ans *= (A[i] - (i ? A[i - 1] : 0) + 1);
+	}
+	return ans;
+}
+
+int main() {
+    int N;
+	std::cin >> N;
+	std::vector<int> A(N);
+	for (int i = 0; i < N; i++) {
+		std::cin >> A[i];
+	}
+	cout << solve(N, A) << endl;
+	return 0;
+}
