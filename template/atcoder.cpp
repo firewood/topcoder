38f1e82e5911c153cb074a8c21267cb74f9a7340
firewood <karamaki@gmail.com>
2021-06-07 00:31:23 +0900

    modint

--- template/atcoder.cpp
+++ template/atcoder.cpp
@@ -12,45 +12,44 @@
 
 using namespace std;
 
-typedef long long LL;
 typedef long double LD;
-typedef pair<LL, LL> II;
-typedef pair<LL, II> III;
+typedef pair<int64_t, int64_t> II;
+typedef pair<int64_t, II> III;
 
-static const LL INF = 1LL << 60;
+static const int64_t INF = 1LL << 60;
 {% if mod %}
-static const long long MOD = {{ mod }};
+static const int64_t MOD = {{ mod }};
 static const size_t MAX_N = 1000001;
 
-struct modll {
-	long long x;
-	modll() { }
-	modll(int _x) : x(_x) { }
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
 	operator int() const { return (int)x; }
-	modll operator+(int y) { return (x + y + MOD) % MOD; }
-	modll operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
-	modll operator-(int y) { return (x - y + MOD) % MOD; }
-	modll operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
-	modll operator*(int y) { return (x * y) % MOD; }
-	modll operator*=(int y) { x = (x * y) % MOD; return *this; }
-	modll operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
-	modll operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
-	static modll modinv(int a) { return modpow(a, MOD - 2); }
-	static modll modpow(int a, int b) {
-		modll x = a, r = 1;
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
 		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
 		return r;
 	}
 };
 
-modll combination(LL n, LL r) {
-	static modll fact[MAX_N + 1], inv[MAX_N + 1];
+modint combination(int64_t n, int64_t r) {
+	static modint fact[MAX_N + 1], inv[MAX_N + 1];
 	if (!fact[0]) {
 		fact[0] = 1;
 		for (int i = 1; i <= MAX_N; ++i) {
 			fact[i] = fact[i - 1] * i;
 		}
-		inv[MAX_N] = modll::modinv(fact[MAX_N]);
+		inv[MAX_N] = modint::modinv(fact[MAX_N]);
 		for (int i = MAX_N; i >= 1; --i) {
 			inv[i - 1] = inv[i] * i;
 		}
@@ -67,11 +66,11 @@ modll combination(LL n, LL r) {
 bool solve({{ formal_arguments }}) {
 	bool ans = false;
 {% else %}
-LL solve({{ formal_arguments }}) {
+int64_t solve({{ formal_arguments }}) {
 	{% if mod %}
-	modll ans = 0;
+	modint ans = 0;
 	{% else %}
-	LL ans = 0;
+	int64_t ans = 0;
 	{% endif %}
 {% endif %}
 {% if hw %}
@@ -96,19 +95,23 @@ LL solve({{ formal_arguments }}) {
 {% endif %}
 
 int main() {
+#if DEBUG || _DEBUG
+	freopen("in.txt", "r", stdin);
+//	freopen("in_1.txt", "r", stdin);
+#endif
 {% if prediction_success %}
-    {{input_part}}
+	{{input_part}}
 {% if yes_str %}
 	cout << (solve({{ actual_arguments }}) ? "{{ yes_str }}" : "{{ no_str }}") << endl;
 {% else %}
 	cout << solve({{ actual_arguments }}) << endl;
 {% endif %}
 {% else %}
-	LL N, M, ans = 0;
+	int64_t N, M, ans = 0;
 
 	cin >> N >> M;
 
-	std::vector<long long> X(N), Y(N);
+	std::vector<int64_t> X(N), Y(N);
 	for (int i = 0; i < N; i++) {
 		std::cin >> X[i] >> Y[i];
 	}
