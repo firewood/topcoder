343999b40ae97ee1941683b174e44a7d80a334dc
firewood <karamaki@gmail.com>
2021-06-15 00:14:10 +0900

    CP typical 90 #015

---
+++ atcoder/typical_90/015.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+static const int64_t MOD = 1000000007;
+static const size_t MAX_N = 200001;
+
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+modint combination(int n, int r) {
+	static modint fact[MAX_N + 1], inv[MAX_N + 1];
+	if (!fact[0]) {
+		fact[0] = 1;
+		for (int i = 1; i <= MAX_N; ++i) {
+			fact[i] = fact[i - 1] * i;
+		}
+		inv[MAX_N] = modint::modinv(fact[MAX_N]);
+		for (int i = MAX_N; i >= 1; --i) {
+			inv[i - 1] = inv[i] * i;
+		}
+	}
+	if (r > n) {
+		return 0;
+	}
+	return (fact[n] * inv[r]) * inv[n - r];
+}
+
+void solve(int N) {
+	for (int k = 1; k <= N; ++k) {
+		modint ans = 0;
+		for (int a = 1; a + (k - 1) * (a - 1) <= N; ++a) {
+			ans += combination(N - (k - 1) * (a - 1), a);
+		}
+		cout << ans << endl;
+	}
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+	solve(N);
+	return 0;
+}
