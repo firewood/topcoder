9fc43adeedf5312b1a84ee39eb4f26bf3cc6e8c3
firewood <karamaki@gmail.com>
2021-07-31 00:49:58 +0900

    ABC #211 C

---
+++ atcoder/abc_211/c.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+static const int64_t MOD = 1000000007;
+
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+modint solve(std::string S) {
+	string ch = "chokudai";
+	vector<modint> dp(9, 0);
+	dp[0] = 1;
+	for (int i = 0; i < S.length(); ++i) {
+		vector<modint> next = dp;
+		for (int j = 0; j < 8; ++j) {
+			if (S[i] == ch[j]) {
+				next[j + 1] += dp[j];
+			}
+		}
+		dp = next;
+	}
+	return dp[8];
+}
+
+int main() {
+	std::string S;
+	std::cin >> S;
+	cout << solve(S) << endl;
+	return 0;
+}
