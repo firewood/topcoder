f3fc11d229514614474d2c21fc60172c376dfe19
firewood <karamaki@gmail.com>
2021-09-14 00:41:27 +0900

    Facebook Hacker Cup 2021 Round 1 A

---
+++ fhc_2021/r1_a_2.cpp
@@ -0,0 +1,85 @@
+// Facebook Hacker Cup 2021 Round 1
+// Problem A2: Weak Typing - Chapter 2
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+const int64_t MOD = 1000000007;
+
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
+		for (; b; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+int solve1(int N, const string& s) {
+	int ans = 0;
+	int last_x = -1, last_o = -1;
+	for (int i = 0; i < N; ++i) {
+		char c = s[i];
+		int diff = -1;
+		if (c == 'X') {
+			ans += last_o > last_x;
+			last_x = i;
+		} else if (c == 'O') {
+			ans += last_x > last_o;
+			last_o = i;
+		}
+	}
+	return ans;
+}
+
+int solve2(int N, const string &s) {
+	modint ans = 0;
+	int last_x = -1, last_o = -1;
+	for (int i = 0; i < N; ++i) {
+		char c = s[i];
+		int diff = -1;
+		if (c == 'X') {
+			if (last_o > last_x) {
+				diff = last_o;
+			}
+			last_x = i;
+		} else if (c == 'O') {
+			if (last_x > last_o) {
+				diff = last_x;
+			}
+			last_o = i;
+		}
+		if (diff >= 0) {
+			ans += int(int64_t(diff + 1) * (N - i) % MOD);
+		}
+	}
+	return ans;
+}
+
+int main(int argc, char* argv[]) {
+	int T, N;
+	cin >> T;
+	for (int t = 1; t <= T; ++t) {
+		cin >> N;
+		string s;
+		cin >> s;
+		cout << "Case #" << t << ": " << solve2(N, s) << endl;
+	}
+	return 0;
+}
