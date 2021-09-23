36919828491150a10aa2ebf2c51513aff6f46491
firewood <karamaki@gmail.com>
2021-09-24 01:27:25 +0900

    CODEFESTA 2016 Qual B D

---
+++ atcoder/codefesta_2016/qb_d.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int64_t solve(int N, std::vector<int> A) {
+	int64_t ans = A[0] - 1, x = 2;
+	for (int i = 1; i < N; ++i) {
+		if (A[i] == x) {
+			++x;
+		} else {
+			ans += (A[i] - 1) / x;
+		}
+	}
+	return ans;
+}
+
+int main() {
+#if DEBUG || _DEBUG
+	freopen("in.txt", "r", stdin);
+//	freopen("in_1.txt", "r", stdin);
+#endif
+	int N;
+	std::cin >> N;
+	std::vector<int> A(N);
+	for (int i = 0; i < N; i++) {
+		std::cin >> A[i];
+	}
+	cout << solve(N, A) << endl;
+	return 0;
+}
