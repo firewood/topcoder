1aeb36301586294f4234ffeb854f15e751a1e620
firewood <karamaki@gmail.com>
2021-11-04 00:34:39 +0900

    ABC #150 E

---
+++ atcoder/abc_150/e.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+static const int64_t MOD = 1000000007;
+
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
+		for (; b > 0; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+modint solve(int N, std::vector<int> C) {
+	modint ans = 0;
+	sort(C.begin(), C.end());
+	for (int i = 0; i < N; ++i) {
+		ans += modint(N - i + 1) * C[i];
+	}
+	return ans * modint::modpow(2, N * 2 - 2);
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+	std::vector<int> C(N);
+	for (int i = 0; i < N; i++) {
+		std::cin >> C[i];
+	}
+	cout << solve(N, C) << endl;
+	return 0;
+}
