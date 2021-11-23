1707b48c81fa0b675ad738adc7eba3653f1c9d8d
firewood <karamaki@gmail.com>
2021-11-24 02:18:26 +0900

    ABC #228 E

---
+++ atcoder/abc_228/e.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+static const int64_t MOD = 998244353;
+
+struct modint {
+	int64_t x;
+	modint() { }
+	modint(int _x) : x(_x) { }
+	operator int() const { return (int)x; }
+	modint operator+(int y) { return (x + y + MOD) % MOD; }
+	modint operator+=(int y) { x = (x + y + MOD) % MOD; return *this; }
+	modint operator-(int y) { return (x - y + MOD) % MOD; }
+	modint operator-=(int y) { x = (x - y + MOD) % MOD; return *this; }
+	modint operator*(int y) { return (x * y) % MOD; }
+	modint operator*=(int y) { x = (x * y) % MOD; return *this; }
+	modint operator/(int y) { return (x * modpow(y, MOD - 2)) % MOD; }
+	modint operator/=(int y) { x = (x * modpow(y, MOD - 2)) % MOD; return *this; }
+	static modint modinv(int a) { return modpow(a, MOD - 2); }
+	static modint modpow(int a, int b) {
+		modint x = a, r = 1;
+		for (; b > 0; b >>= 1, x *= x) if (b & 1) r *= x;
+		return r;
+	}
+};
+
+int64_t solve(int64_t N, int64_t K, int64_t M) {
+	if (M % MOD == 0) return 0;
+	int64_t x = K % (MOD - 1), a = 1, b = N;
+	for (; b > 0; b >>= 1, x = (x * x) % (MOD - 1)) {
+		if (b & 1) a = (a * x) % (MOD - 1);
+	}
+	return modint::modpow(M % MOD, a);
+}
+
+int main() {
+	int64_t N, K, M;
+	std::cin >> N >> K >> M;
+	cout << solve(N, K, M) << endl;
+	return 0;
+}
