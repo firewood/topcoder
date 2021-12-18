0ba8e269a41d13eb6a088a547a2031a10cadae5b
firewood <karamaki@gmail.com>
2021-12-18 18:22:21 +0900

    simplified

--- atcoder/arc_096/d.cpp
+++ atcoder/arc_096/d.cpp
@@ -1,61 +1,47 @@
-// D.
-
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
 #include <iostream>
 #include <sstream>
-#include <algorithm>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
-typedef long long LL;
-typedef pair<LL, LL> LLLL;
+int64_t solve2(int64_t N, int64_t C, std::vector<int64_t> x, std::vector<int64_t> v) {
+	int64_t ans = 0, sum = 0, rsum = 0;
+	vector<int64_t> dp(N + 1);
+	for (int i = N - 1; i > 0; --i) {
+		rsum += v[i];
+		dp[i] = max(rsum - (C - x[i]) * 2, dp[i + 1]);
+	}
+	for (int i = 0; i < N; ++i) {
+		sum += v[i];
+		ans = max(ans, sum - x[i] + dp[i + 1]);
+	}
+	return ans;
+}
 
-int main(int argc, char *argv[])
-{
-	int n;
-	LL C;
-	cin >> n >> C;
-	vector<LLLL> cd, dc;
-	vector<LL> b1(n+1), b2(n+1);
-	for (int i = 0; i < n; ++i) {
-		LL x, v;
-		cin >> x >> v;
-		cd.push_back(LLLL(x, v));
+int64_t solve(int64_t N, int64_t C, std::vector<int64_t> x, std::vector<int64_t> v) {
+	vector<int64_t> rx(N), rv = v;
+	for (int i = 0; i < N; ++i) {
+		rx[i] = C - x[N - 1 - i];
 	}
-	sort(cd.begin(), cd.end());
-	dc = cd;
-	for (int i = 0; i < n; ++i) {
-		dc[i].first = C - cd[i].first;
-	}
-	sort(dc.begin(), dc.end());
+	reverse(rv.begin(), rv.end());
+	return max(solve2(N, C, x, v), solve2(N, C, rx, rv));
+}
 
-	LL ans = 0, tot = 0;
-	for (int i = 0; i < n; ++i) {
-		tot += cd[i].second;
-		ans = max(ans, tot - cd[i].first);
-		b1[i+1] = ans;
+int main() {
+	int64_t N, C;
+	std::cin >> N >> C;
+	std::vector<int64_t> x(N), v(N);
+	for (int i = 0; i < N; i++) {
+		std::cin >> x[i] >> v[i];
 	}
-	ans = 0;
-	tot = 0;
-	for (int i = 0; i < n; ++i) {
-		tot += dc[i].second;
-		ans = max(ans, tot - dc[i].first);
-		b2[i+1] = ans;
-	}
-
-	tot = 0;
-	for (int i = 0; i < n; ++i) {
-		LL r = max(0LL, b2[n - (i + 1)] - cd[i].first);
-		tot += cd[i].second;
-		ans = max(ans, tot - cd[i].first + r);
-	}
-	tot = 0;
-	for (int i = 0; i < n; ++i) {
-		LL r = max(0LL, b1[n - (i + 1)] - dc[i].first);
-		tot += dc[i].second;
-		ans = max(ans, tot - dc[i].first + r);
-	}
-
-	cout << ans << endl;
+	cout << solve(N, C, x, v) << endl;
 	return 0;
 }
