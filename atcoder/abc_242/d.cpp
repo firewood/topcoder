db8eb620105502784d62334a9b87fcb72ebb734e
firewood <karamaki@gmail.com>
2022-03-07 01:09:15 +0900

    fix

--- atcoder/abc_242/d.cpp
+++ atcoder/abc_242/d.cpp
@@ -16,10 +16,13 @@ const string abc = "ABCABC";
 string S;
 
 char solve(int64_t t, int64_t k) {
+	if (k == 0) {
+		return abc[S[0] - 'A' + (t % 3)];
+	}
 	if (t == 0) {
 		return S[k];
 	}
-	return abc[(solve(t - 1, k / 2) - 'A') + 1 + (k % 2)];
+	return abc[solve(t - 1, k / 2) - 'A' + 1 + (k % 2)];
 }
 
 int main() {
@@ -28,7 +31,7 @@ int main() {
 	cin >> Q;
 	for (int tt = 0; tt < Q; ++tt) {
 		cin >> t >> k;
-		cout << solve(t - max(int64_t(0), (t / 60 - 1) * 60), k - 1) << endl;
+		cout << solve(t, k - 1) << endl;
 	}
 	return 0;
 }
