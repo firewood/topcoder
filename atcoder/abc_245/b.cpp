059189ccb5fe446d3e46c4ed9b10a89ee344e95f
firewood <karamaki@gmail.com>
2022-03-29 00:22:13 +0900

    ABC #245 B

---
+++ atcoder/abc_245/b.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int solve(int N, std::vector<int> A) {
+	set<int64_t> s(A.begin(), A.end());
+	int ans = 0;
+	while (s.find(ans) != s.end()) {
+		++ans;
+	}
+	return ans;
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+	std::vector<int> A(N);
+	for (int i = 0; i < N; i++) {
+		std::cin >> A[i];
+	}
+	cout << solve(N, A) << endl;
+	return 0;
+}
