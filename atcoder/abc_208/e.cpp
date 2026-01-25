bfc1b87d4610f39c6ae497e1b0956d96e0c62cf6
firewood <174658+firewood@users.noreply.github.com>
2026-01-25 16:57:57 +0900

    practice

--- atcoder/abc_208/e.cpp
+++ atcoder/abc_208/e.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 // 桁DP
-int64_t solve(int64_t N, int64_t K) {
+int64_t solve1(int64_t N, int64_t K) {
 	map<int64_t, int64_t> eq;		// Nと等しい
 	map<int64_t, int64_t> le;		// Nより小さい
 	string s = to_string(N);
@@ -59,6 +59,40 @@ int64_t solve(int64_t N, int64_t K) {
 			ans += kv.second;
 		}
 	}
+	return ans;
+}
+
+// 桁DP
+int64_t solve(int64_t N, int64_t K) {
+	int64_t ans = 0;
+	string s = to_string(N);
+	int64_t prod = 1;
+	map<int64_t, int64_t> dp;
+	for (int64_t i = 0; i < s.length(); i++) {
+		map<int64_t, int64_t> next;
+		int64_t d = s[i] - '0';
+		for (int64_t j = 0; j <= 9; ++j) {
+			for (auto kv : dp) {
+				next[min(kv.first * j, K + 1)] += kv.second;
+			}
+			if (i > 0 && j > 0) {
+				next[min(j, K + 1)] += 1;
+			}
+		}
+		for (int64_t j = 0; j < d; ++j) {
+			if (i > 0 || j > 0) {
+				next[min(prod * j, K + 1)] += 1;
+			}
+		}
+		prod = min(prod * d, K + 1);
+		dp = next;
+	}
+	for (const auto& kv : dp) {
+		if (kv.first <= K) {
+			ans += kv.second;
+		}
+	}
+	ans += prod <= K;
 	return ans;
 }
 
