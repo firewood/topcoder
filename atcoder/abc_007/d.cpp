855df25bb280d2aa5e82f05129227a4a5ea10086
firewood <174658+firewood@users.noreply.github.com>
2026-01-25 16:57:06 +0900

    practice

--- atcoder/abc_007/d.cpp
+++ atcoder/abc_007/d.cpp
@@ -12,10 +12,10 @@
 
 using namespace std;
 
-long long calc(long long x) {
+int64_t calc1(int64_t x) {
 	// 桁DP
 	string s = to_string(x);
-	long long dp[20][2][2] = { 1 };
+	int64_t dp[20][2][2] = { 1 };
 	for (size_t i = 0; i < s.length(); ++i) {
 		int d = s[i] - '0';
 		for (int j = 0; j < 2; ++j) {		// 未満フラグ
@@ -29,12 +29,39 @@ long long calc(long long x) {
 	return dp[s.size()][0][1] + dp[s.size()][1][1];
 }
 
-long long solve(long long A, long long B) {
+int64_t calc(int64_t x) {
+	// 桁DP
+	string s = to_string(x);
+	auto dp = vector(20, vector(10, int64_t(0)));
+	bool p = false;
+	for (size_t i = 0; i < s.length(); ++i) {
+		int d = s[i] - '0';
+		for (int k = 0; k < 2; ++k) {		// 禁止フラグ
+			for (int l = 0; l <= 9; ++l) {
+				dp[i + 1][k || l == 4 || l == 9] += dp[i][k];
+			}
+		}
+		if (i > 0) {
+			for (int l = 1; l <= 9; ++l) {
+				dp[i + 1][l == 4 || l == 9] += 1;
+			}
+		}
+		for (int l = 0; l < d; ++l) {
+			if (i > 0 || l > 0) {
+				dp[i + 1][p || l == 4 || l == 9] += 1;
+			}
+		}
+		p |= d == 4 || d == 9;
+	}
+	return dp[s.size()][1] + p;
+}
+
+int64_t solve(int64_t A, int64_t B) {
 	return calc(B) - calc(A - 1);
 }
 
 int main() {
-    long long A, B;
+	int64_t A, B;
 	std::cin >> A >> B;
 	cout << solve(A, B) << endl;
 	return 0;
