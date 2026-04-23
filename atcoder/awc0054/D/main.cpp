2a4f1f20481ebb2050ccab9f30fe624b0099a15a
firewood <174658+firewood@users.noreply.github.com>
2026-04-23 22:33:21 +0900

    fix

--- atcoder/awc0054/D/main.cpp
+++ atcoder/awc0054/D/main.cpp
@@ -21,28 +21,27 @@
 
 using namespace std;
 typedef pair<int64_t, int64_t> II;
-map<II, II> memo;
+const int64_t INF = 1LL << 60;
+vector<vector<II>> memo(3002, vector<II>(3002, { INF, INF }));
 vector<int64_t> a;
 
-II dfs(int left, int right) {
-	if (left >= right) {
-		return { 0, 0 };
+II &dfs(int left, int right) {
+	II &r = memo[left][right];
+	if (r.first == INF) {
+		if (left >= right) {
+			r = { 0, 0 };
+		} else if (left + 1 == right) {
+			r = { a[left], 0 };
+		} else {
+			II &p = dfs(left + 1, right), &q = dfs(left, right - 1);
+			II x(a[left] + p.second, p.first), y(a[right - 1] + q.second, q.first);
+			r = ((x.first - x.second) >= (y.first - y.second)) ? x : y;
+		}
 	}
-	if (left + 1 == right) {
-		return { a[left], 0 };
-	}
-	II key(left, right);
-	if (memo.find(key) != memo.end()) {
-		return memo[key];
-	}
-	II p = dfs(left + 1, right), q = dfs(left, right - 1);
-	II x(a[left] + p.second, p.first), y(a[right - 1] + q.second, q.first);
-	II r = ((x.first - x.second) > (y.first - y.second)) ? x : y;
-	memo[key] = r;
 	return r;
 }
 
-II solve(int N, std::vector<int64_t> A) {
+II solve(int N, std::vector<int64_t> &A) {
 	a = A;
 	return dfs(0, N);
 }
@@ -54,6 +53,6 @@ int main() {
 	for (int64_t i = 0; i < N; ++i) {
 		std::cin >> A[i];
 	}
-	auto ans = solve(N, std::move(A));
+	auto ans = solve(N, A);
 	cout << ans.first << " " << ans.second << endl;
 }
